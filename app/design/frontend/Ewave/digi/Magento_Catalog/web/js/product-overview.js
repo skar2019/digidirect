587e9928e9d7e99d427c8f70053153787d2f939f
Clint Mercado <dev1@digidirect.com.au>
2020-04-30 16:26:52 +0800

    Product overview open state as requested by Shant

--- app/design/frontend/Ewave/digi/Magento_Catalog/web/js/product-overview.js
+++ app/design/frontend/Ewave/digi/Magento_Catalog/web/js/product-overview.js
@@ -18,9 +18,9 @@ define([
         },
         exit: function () {
             $("#productOverview").tabs({
-                active: 0,
+                active: 1,
                 openedState: "active",
-                collapsible: false
+                collapsible: true
             })
         },
     });
