4b92178b3963ada5d9e8909420c3234c2477551f
Clint Mercado <dev1@digidirect.com.au>
2020-06-29 16:06:43 +0800

    Workaround for staging checkout issue # Conflicts: #	app/design/frontend/Ewave/digi/Magento_Checkout/web/js/order-button-disabled.js

--- app/design/frontend/Ewave/digi/Magento_Checkout/web/js/order-button-disabled.js
+++ app/design/frontend/Ewave/digi/Magento_Checkout/web/js/order-button-disabled.js
@@ -16,6 +16,7 @@ define([
         $("#co-payment-form").trigger("reset");
         $("#co-transparent-form-braintree").trigger("reset");
         $(".payment-method-braintree").removeClass('_active');
+
         if ($('#braintree_paypal').is(':checked')) {
             $('#payment-method-braintree-paypal').removeClass('_active');
         }
