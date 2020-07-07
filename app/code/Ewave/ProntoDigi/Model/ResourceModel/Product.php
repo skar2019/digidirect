e6eb04eb0bac2abb7df1bab3b720f0c8ade5abd8
Clint Mercado <dev1@digidirect.com.au>
2020-07-07 16:50:17 +0800

    Removed objectmanager instance for m2e class

--- app/code/Ewave/ProntoDigi/Model/ResourceModel/Product.php
+++ app/code/Ewave/ProntoDigi/Model/ResourceModel/Product.php
@@ -225,7 +225,7 @@ class Product extends \Magento\Catalog\Model\ResourceModel\Product
     public function updateProductAttributes($products, $attributes = [])
     {
 
-        $model = $this->_objectManager->create(M2e\Ebay-Amazon-Magento2\PublicServices\Product\SqlChange::class);
+        //$model = $this->_objectManager->create(M2e\Ebay-Amazon-Magento2\PublicServices\Product\SqlChange::class);
         $linkFiled = $this->getLinkField();
         $productCollection = $this->collectionFactory->create();
         foreach ($attributes as $code) {
@@ -253,9 +253,9 @@ class Product extends \Magento\Catalog\Model\ResourceModel\Product
                         ];
                     }
                     
-                    $model->markQtyWasChanged($products[$item['id']]);
-                    $model->markPriceWasChanged($products[$item['id']]);
-                    $model->applyChanges();
+//                    $model->markQtyWasChanged($products[$item['id']]);
+//                    $model->markPriceWasChanged($products[$item['id']]);
+//                    $model->applyChanges();
                 }
             }
             foreach ($dataToUpdate as $tableName => $data) {
