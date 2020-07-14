4c283fadd630bfbb5af8bd526f76e43a6ef2c55c
Clint Mercado <dev1@digidirect.com.au>
2020-07-14 20:49:23 +0800

    Added braintree_googlepay on code checking to add payment type to transaction

--- app/code/Ewave/ProntoDigi/ProntoApi/Orders/Post/MapperHelper.php
+++ app/code/Ewave/ProntoDigi/ProntoApi/Orders/Post/MapperHelper.php
@@ -258,7 +258,7 @@ class MapperHelper
         $method = $payment->getMethodInstance();
 
         $code = $method->getCode();
-        if ($code == 'm2epropayment') {
+        if ($code == 'm2epropayment' || $code == 'braintree_googlepay' ) {
             $componentMode = $payment->getAdditionalInformation('component_mode');
             $paymentMethod = $payment->getAdditionalInformation('payment_method');
             $code = $paymentMethod ? $componentMode . '-' . $paymentMethod : $componentMode;
