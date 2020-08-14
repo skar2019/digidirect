421015ab427a67509d9f090315585e77d8cf7786
Clint Mercado <dev1@digidirect.com.au>
2020-08-14 10:36:52 +0800

    Removed alerts

--- app/design/frontend/Ewave/digi/Magento_Catalog/web/js/product/price/cashback-label.js
+++ app/design/frontend/Ewave/digi/Magento_Catalog/web/js/product/price/cashback-label.js
@@ -5,10 +5,8 @@ define([
     var productInfoPriceBlock = $('.product-info-price');
     if(cashbackLabel){
         productInfoPriceBlock.find('.price-box.price-final_price').addClass('cashback-active');
-        alert($('.price-box.price-final_price.cashback-active').find('.old-price-label').length);
         if($('.price-box.price-final_price.cashback-active').find('.old-price-label').length === 0)
         {
-            alert("no old price");
             productInfoPriceBlock.find('.price-box.price-final_price').addClass('pad-top');
         }
         
