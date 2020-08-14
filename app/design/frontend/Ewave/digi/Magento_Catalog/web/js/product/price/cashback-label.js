99852755e66eaf89c9e6d0c936b2bebf5888bec4
Clint Mercado <dev1@digidirect.com.au>
2020-08-14 10:09:20 +0800

    Fixed pdp cashback

--- app/design/frontend/Ewave/digi/Magento_Catalog/web/js/product/price/cashback-label.js
+++ app/design/frontend/Ewave/digi/Magento_Catalog/web/js/product/price/cashback-label.js
@@ -5,6 +5,13 @@ define([
     var productInfoPriceBlock = $('.product-info-price');
     if(cashbackLabel){
         productInfoPriceBlock.find('.price-box.price-final_price').addClass('cashback-active');
+        alert($('.price-box.price-final_price.cashback-active').find('.old-price-label').length);
+        if($('.price-box.price-final_price.cashback-active').find('.old-price-label').length === 0)
+        {
+            alert("no old price");
+            productInfoPriceBlock.find('.price-box.price-final_price').addClass('pad-top');
+        }
+        
         productInfoPriceBlock.prepend($('.block-extendedrule'));
     }
 });
