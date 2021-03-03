9d5e068458a15b02b60b87ba58e12ae3e89d0643
Dev <dev4@digidirect.com.au>
2021-03-03 16:58:07 +0800

    Removed Navigation and AbstractAttributesNavigation

--- app/code/Digidirect/Navigation/Constants/Acl.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Digidirect\Navigation\Constants;
-
-class Acl
-{
-    const ACL_DIGIDIRECT_NAVIGATION_MENU_SETS = 'Digidirect_Navigation::navigation_menu_sets';
-    const ACL_DIGIDIRECT_NAVIGATION_MENU_ITEM_SAVE = 'Digidirect_Navigation::navigation_menu_items_save';
-}
