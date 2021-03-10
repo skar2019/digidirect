d327bed5785133fec201581e6b1cac6d5e37bb75
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-03-10 13:44:14 +0800

    Fixed Layout of Product Listing

---
+++ app/design/frontend/Digidirect/digi/requirejs-config.js
@@ -0,0 +1,18 @@
+var config = {
+    map: {
+        '*': {
+            'slickInit': 'js/widgets/slick-init',
+            'customScrollbarInit': 'js/scrollbar-init',
+            'mousewheel': 'js/vendor/jquery.mousewheel.min',
+            'mCustomScrollbar': 'js/vendor/jquery.mCustomScrollbar.min',
+            'selectric' : 'js/vendor/jquery.selectric.min',
+            'customSelectInit' : 'js/selectric-init',
+            'truncateDotdotdot' : 'js/dist/widgets/truncate-dotdotdot',
+            'truncateCollection' : 'js/dist/widgets/truncate-collection',
+            'productTile' : 'js/dist/widgets/product-tile',
+            'secondaryNavigation': 'js/widgets/secondary-navigation',
+            'Magento_Search/form-mini': 'Magento_Search/js/form-mini', //Hack for js error in vendor/klevu/module-productsearch/view/frontend/web/js/disableautosuggest.js
+            'eventTransporter' : 'js/dist/widgets/event-transporter'
+        }
+    }
+};
