396dc46a8eaa44dd454303b837a36940f6e7eed3
Dev <dev4@digidirect.com.au>
2021-03-12 18:54:12 +0800

    Commit checkout related and renamed

---
+++ app/code/Digidirect/Checkout/view/frontend/web/js/abstract-total-mixin.js
@@ -0,0 +1,17 @@
+define([], function () {
+    "use strict";
+
+    return function (target) {
+        return target.extend({
+            /**
+             * @return {*}
+             */
+            isFullMode: function () {
+                if (!this.getTotals()) {
+                    return false;
+                }
+                return true;
+            }
+        });
+    }
+});
