964ad5244b6d356f953ecf338ecd09a62dd13b45
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-03-15 18:55:32 +0800

    Added Product Overlay

---
+++ app/code/Digidirect/ProductOverlay/Ui/Component/Listing/Column/Overlay/Store/Options.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Digidirect\ProductOverlay\Ui\Component\Listing\Column\Overlay\Store;
+
+use Magento\Store\Ui\Component\Listing\Column\Store\Options as StoreOptions;
+
+/**
+ * Store Options for product overlays
+ */
+class Options extends StoreOptions
+{
+    /**
+     * All Store Views value
+     */
+    const ALL_STORE_VIEWS = '0';
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if ($this->options !== null) {
+            return $this->options;
+        }
+
+        $this->currentOptions['All Store Views']['label'] = __('All Store Views');
+        $this->currentOptions['All Store Views']['value'] = self::ALL_STORE_VIEWS;
+
+        $this->generateCurrentOptions();
+
+        $this->options = array_values($this->currentOptions);
+
+        return $this->options;
+    }
+}
