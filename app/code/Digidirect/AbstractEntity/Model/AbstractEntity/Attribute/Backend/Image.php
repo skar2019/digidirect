07ac5da1d7fb78d205ca0994e4d5eee84a1e88a5
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-03-17 17:02:42 +0800

    Removed Abstract Entity

--- app/code/Digidirect/AbstractEntity/Model/AbstractEntity/Attribute/Backend/Image.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Digidirect\AbstractEntity\Model\AbstractEntity\Attribute\Backend;
-
-class Image extends \Magento\Catalog\Model\Category\Attribute\Backend\Image
-{
-    /** Image backend model can be extended */
-}
--- app/code/Digidirect/AbstractEntity/Model/AbstractEntity/Attribute/Source/AddToSitemap.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Digidirect\AbstractEntity\Model\AbstractEntity\Attribute\Source;
-
-class AddToSitemap
-{
-    const ADD_TO_SITEMAP_ENABLED = 1;
-
-    const ADD_TO_SITEMAP_DISABLED = 0;
-}
