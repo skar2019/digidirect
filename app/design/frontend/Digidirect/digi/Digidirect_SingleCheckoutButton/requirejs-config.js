5ff69ea2a6bbfdd09c7d226d6e5d07a34ec15358
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-04-22 21:41:02 +0800

    Added checkout module pre-requisite

---
+++ app/design/frontend/Digidirect/digi/Digidirect_SingleCheckoutButton/requirejs-config.js
@@ -0,0 +1,10 @@
+/* eslint no-unused-vars: [1] */
+var config = {
+    config: {
+        mixins: {
+            'Digidirect_SingleCheckoutButton/js/view/single-button': {
+                'Digidirect_SingleCheckoutButton/js/view/single-button-mixin': true
+            }
+        }
+    }
+};
