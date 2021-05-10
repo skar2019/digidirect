7b6dd8585ea681357fb8c0cfa6d05c0837a6f990
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-05-11 00:25:53 +0800

    For testing

--- app/code/Digidirect/InfiniteScroll/Model/Catalog/Processor.php
+++ app/code/Digidirect/InfiniteScroll/Model/Catalog/Processor.php
@@ -90,28 +90,28 @@ class Processor implements ProcessorInterface
 //                }
 //            }
             
-            $dom = new \Zend_Dom_Query($html);
-            $results = $dom->query('.product-items');
-            
-            $ctr = 0;
-            $resultHtml = "";
-            
-            foreach($results as $node){
-                $childNodes = $node->childNodes;
-                $resultHtml .= $childNodes->C14N();
-            }
-            
 //            $dom = new \Zend_Dom_Query($html);
-//            $results = $dom->query(".product-items");
-//
-//            $count = count($results); // get number of matches: 4
+//            $results = $dom->query('.product-items');
+//            
+//            $ctr = 0;
 //            $resultHtml = "";
 //            
-//            if($count > 0){
-//                foreach ($results as $result) {
-//                    $resultHtml .= $result->nodeValue;
-//                }
+//            foreach($results as $node){
+//                $childNodes = $node->childNodes;
+//                $resultHtml .= $childNodes->C14N();
 //            }
+            
+            $dom = new \Zend_Dom_Query($html);
+            $results = $dom->query(".product-items");
+
+            $count = count($results); // get number of matches: 4
+            $resultHtml = "";
+            
+            if($count > 0){
+                foreach ($results as $result) {
+                    $resultHtml .= $result->ownerDocument->saveHTML($result);
+                }
+            }
             
         }
 
