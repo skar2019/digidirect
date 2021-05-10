5bc2bbc2ef68bde5d33e78cd7bda450ad81775c6
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-05-11 01:07:50 +0800

    For testing

--- app/code/Digidirect/InfiniteScroll/Model/Catalog/Processor.php
+++ app/code/Digidirect/InfiniteScroll/Model/Catalog/Processor.php
@@ -100,6 +100,7 @@ class Processor implements ProcessorInterface
             if($count > 0){
                 foreach ($results as $result) {
                     $resultNode .= $result->nodeValue;
+                    $resultHtml .= $result->C14N();
                 }
             }
             
