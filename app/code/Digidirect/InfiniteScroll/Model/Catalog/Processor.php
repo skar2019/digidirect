87a72ba2d884ed01f4afc95202c5bb82e47250fe
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-05-11 10:55:55 +0800

    For testing

--- app/code/Digidirect/InfiniteScroll/Model/Catalog/Processor.php
+++ app/code/Digidirect/InfiniteScroll/Model/Catalog/Processor.php
@@ -91,7 +91,7 @@ class Processor implements ProcessorInterface
 //            }
             
             $dom = new \Zend_Dom_Query($html);
-            $results = $dom->query(".product-items");
+            $results = $dom->query(".product-items li");
 
             $count = count($results); // get number of matches: 4
             $resultHtml = "";
