57c93b4ea8f625543af34c0d44d9f64518105b20
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-06-01 13:12:45 +0800

    Removed payment info

--- app/code/Digidirect/Vii/view/frontend/web/js/view/payment/gift-cards.js
+++ app/code/Digidirect/Vii/view/frontend/web/js/view/payment/gift-cards.js
@@ -34,6 +34,9 @@ define([
 
             if (data && data.extension_attributes) {
                 cardsData = JSON.parse(data.extension_attributes.gift_cards);
+                
+                console.log(cardsData);
+                
                 if (cardsData.length > this.cardsData().length) {
                     this.addCardItems(cardsData);
                 } else {
