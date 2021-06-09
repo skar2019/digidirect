40c353822a6f845f4b4488cbac19bb95c95599e0
clintdigi <dev1@digidirect.com.au>
2021-06-09 18:12:17 +0800

    Pronto Module

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -48,32 +48,24 @@ class Order extends AbstractHelper
             $counter++;
             //var_dump($order);
             /* @var $order \Magento\Sales\Model\Order */
-//            foreach ($order->getAllVisibleItems() as $item) {
-//                /* @var $item \Magento\Sales\Model\Order\Item */
-//                $sku = $item->getSku();
-//                $name = $item->getName();
-//                $price = (double) $item->getBasePriceInclTax();
-//                $qty = (double) $item->getQtyOrdered();
-//                if (!isset($piwikItems[$sku])) {
-//                    $piwikItems[$sku] = [$sku, $name, $price * $qty, $qty];
-//                } else {
-//                    // Aggregate row total instead of unit price in case there
-//                    // are different prices for the same SKU.
-//                    $piwikItems[$sku][2] += $price * $qty;
-//                    $piwikItems[$sku][3] += $qty;
-//                }
-//            }
+            
+            if ($order->getState() == 'canceled') {
+                continue;
+            }
             
             $orderId = $order->getIncrementId();
-            //if($orderId == '000001174')
-            //{
-                
             
-            echo "<br />orderId ".$orderId ."<br />";
+            echo "<br />orderId ".$orderId;
             $accountname = $order->getCustomerFirstname().$order->getCustomerLastname();
             $contactname = $order->getCustomerFirstname()." ".$order->getCustomerLastname();
             //check pronto if customer has an account.
             //if not, create customer account to pronto
+            
+            if(empty($accountname))
+            {
+                echo "<br> empty accountname <br>";
+                continue;
+            }
             $data['sales-order']['header']['accountname'] = $accountname;
             $data['sales-order']['header']['account'] = $order->getCustomerId();
             
@@ -92,17 +84,7 @@ class Order extends AbstractHelper
             $tax = (double) $order->getBaseTaxAmount();
             $shipping = (double) $order->getBaseShippingInclTax();
             $discount = abs((double) $order->getBaseDiscountAmount());
-            //below not yet used 
-            if (empty($piwikOrder)) {
-                $piwikOrder = [$orderId, $grandTotal, $subTotal, $tax, $shipping, $discount];
-            } else {
-                $piwikOrder[0] .= ', ' . $orderId;
-                $piwikOrder[1] += $grandTotal;
-                $piwikOrder[2] += $subTotal;
-                $piwikOrder[3] += $tax;
-                $piwikOrder[4] += $shipping;
-                $piwikOrder[5] += $discount;
-            }
+            
             
             $data['sales-order']['header']['order-total-inc-tax'] = $grandTotal;
             
@@ -126,58 +108,109 @@ class Order extends AbstractHelper
             $data['sales-order']['header']['billing-address']['phone'] = $phone;
             $data['sales-order']['header']['billing-address']['mobile'] = $mobile;
             
-            echo "paymemt - ". $paymentInstance->getMethod();
+            //payment details
+            
+            $is_bank = false;
+            
+            $methodInst = $paymentInstance->getMethodInstance();
+            echo "<br> payment - ". $paymentInstance->getMethod();
+            $methodTitle = $methodInst->getTitle();
+            echo "<br >method - ".$methodTitle;
+            
             if ($paymentInstance->getMethod() == "banktransfer") {
                 unset($data);
+                $is_bank = true;
+            }
+
+            if($is_bank){
                 continue;
             }
-            //var_dump($data);
-            if($counter == 2)
-            {
-                //var_dump($order);
-                break;
+            
+            $payment_reference = $paymentInstance->getLastTransId();
+            echo "<br >payment_reference - ".$payment_reference;
+            $payment_type = $this->getPaymentType($paymentInstance);
+            $amount_tendered = $order->getBaseGrandTotal();
+            $amount_tendered = round($amount_tendered, 2);
+            
+            echo "<br >amount_tendered - " .$amount_tendered;
+            
+            
+            $data['sales-order']['header']['payment-details']['payment-detail']['payment-type'] = $payment_type;
+            $data['sales-order']['header']['payment-details']['payment-detail']['payment-reference'] = $payment_reference;
+            $data['sales-order']['header']['payment-details']['payment-detail']['amount-tendered'] = $amount_tendered;
+            //exit;
+            //product lines
+            $x = 0;
+            foreach ($order->getAllVisibleItems() as $item) {
+                /* @var $item \Magento\Sales\Model\Order\Item */
+                
+                $price = (double) $item->getBasePriceInclTax();
+                $qty = (double) $item->getQtyOrdered();
+                $data['sales-order']['detail']['line'][$x]['line-type'] = 'SN';
+                $data['sales-order']['detail']['line'][$x]['stock-code'] = $item->getSku();
+                $data['sales-order']['detail']['line'][$x]['description'] = $item->getName();
+                $data['sales-order']['detail']['line'][$x]['unit-price-inc-tax'] = $price;
+                $data['sales-order']['detail']['line'][$x]['ordered'] = $qty;
+                $data['sales-order']['detail']['line'][$x]['shipped'] = $qty;
+                $data['sales-order']['detail']['line'][$x]['backordered'] = 0;
+                $data['sales-order']['detail']['line'][$x]['sol-disc-rate'] = 0;
+                $data['sales-order']['detail']['line'][$x]['sol-line-total-inc-tax'] = $price * $qty;
+                $x++;
             }
-             
-            //}//if order id 000001174
-        }
-        //should be inside the foreach above
-        //create xml of order data here
-        $xml = \Digidirect\AI\Model\Lib\Adapter\Import\Xml::assocToXml($data, 'sales-orders');
-        
-        echo $xml;
-        
-        //$url = 'https://digi-pronto.abtonline.com.au:8083/rest/abtws/login';
-        $url = 'https://digi-pronto.abtonline.com.au:8083/rest/abtws/sales?call-type=create_orders';
-        $username = 'clint.mercado';
-        $password = '849cd5080faff5ce';
-        $jsonData = '{}';
-        
-        $this->curl->addHeader("Content-Type", "application/xml");
-        $this->curl->addHeader("Accept", "application/json");
-        $this->curl->addHeader("compcode", "UA1");
-        $this->curl->addHeader("user", "clint.mercado");
-        $this->curl->addHeader("token", "849cd5080faff5ce");
-        $this->curl->post($url, $xml);
-        
-        $result = $this->curl->getBody();
-        
-        
-        //var_dump($result);
-        // echo $result;
-        $json = $this->jsonSerializer->unserialize($result);
-        //var_dump($json);
-        if(isset($json['response']['status']) && ($json['response']['status'] == 'FAIL'))
-        {
-            echo $json['response']['message'];
-            exit;
-        }
-        else {
-            //success
-            //update order data with pronto order-no below
-            //$json['sales-order']['sales-order']['order-no']
-            echo "success";
-            $order->setState("processing")->setStatus("processing");
-            $order->save();
+            
+            
+            //should be inside the foreach above
+            //create xml of order data here
+            $xml = \Digidirect\AI\Model\Lib\Adapter\Import\Xml::assocToXml($data, 'sales-orders');
+
+            echo $xml;
+
+            //$url = 'https://digi-pronto.abtonline.com.au:8083/rest/abtws/login';
+            $url = 'https://digi-pronto.abtonline.com.au:8083/rest/abtws/sales?call-type=create_orders';
+            $username = 'clint.mercado';
+            $password = '849cd5080faff5ce';
+            $jsonData = '{}';
+
+            $this->curl->addHeader("Content-Type", "application/xml");
+            $this->curl->addHeader("Accept", "application/json");
+            $this->curl->addHeader("compcode", "UA1");
+            $this->curl->addHeader("user", "clint.mercado");
+            $this->curl->addHeader("token", "849cd5080faff5ce");
+            $this->curl->post($url, $xml);
+
+            $result = $this->curl->getBody();
+
+            //var_dump($result);
+            // echo $result;
+            $json = $this->jsonSerializer->unserialize($result);
+            //var_dump($json);
+            echo "<br>";
+            if(isset($json['response']['status']) && ($json['response']['status'] == 'FAIL'))
+            {
+                echo $json['response']['message'];
+                echo "<br>";
+                
+            }
+            else if (isset($json['sales-orders']['response']['status']) && ($json['sales-orders']['response']['status'] == 'failed')) {
+                echo $json['sales-orders']['response']['message'];
+                echo "<br>";
+            }
+            else {
+                //success
+                //update order data with pronto order-no below
+                //$json['sales-order']['sales-order']['order-no']
+                echo "success";
+                echo "<br>";
+                //$order->setState("processing")->setStatus("processing");
+                $pronto = $json['sales-orders']['sales-order']['order-no'];
+                $order->setData('pronto_order_number',$pronto);
+                $order->save();
+                var_dump($json);
+                exit; //for testing;
+            }
+            
+            
+            
         }
         
     }   
@@ -186,9 +219,44 @@ class Order extends AbstractHelper
     {
        $collection = $this->_orderCollectionFactory->create()
          ->addAttributeToSelect('*')
-         ->addFieldToFilter('status', 'pending'); //Add condition if you wish
+         ->addFieldToFilter('pronto_order_number', array('null' => true)); //Add condition if you wish
      
      return $collection;
      
+    }
+    
+    public function getPaymentType($paymentInstance){
+        
+        echo "<br >get payment type ". $paymentInstance->getMethod();
+        $payment = $paymentInstance->getMethod();
+        switch ($payment) {
+            case "braintree":
+              $type = 'BT';
+              break;
+            case "checkmo":
+              $type = 'H';
+              break;
+            case "free":
+              $type = 'VI';
+              break;
+            case "m2epropayment":
+              $type = 'EB';
+              break;
+          case "braintree_paypal":
+              $type = 'PY';
+              break;
+            default:
+              break;
+        }
+        
+        if($type == 'BT')
+        {
+            $cc = $paymentInstance->getCcType();
+            if($cc == 'AE')
+            {
+                $type = 'X';
+            }
+        }
+        return $type;
     }
 }      
