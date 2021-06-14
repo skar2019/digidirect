6f3c9a55c25f87b9637e9c5794f068e1c2099001
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-06-14 09:38:49 +0800

    Removed unused parameter

--- app/code/Digidirect/OnSaleProducts/Model/ResourceModel/Product/Collection.php
+++ app/code/Digidirect/OnSaleProducts/Model/ResourceModel/Product/Collection.php
@@ -1,12 +1,5 @@
 <?php
 /**
- * GiaPhuGroup Co., Ltd.
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the GiaPhuGroup.com license that is
- * available through the world-wide-web at this URL:
- * https://www.giaphugroup.com/LICENSE.txt
  *
  * DISCLAIMER
  *
@@ -14,23 +7,13 @@
  * version in the future.
  *
  * @category    Digidirect
- * @package     Digidirect_BestsellersProducts
- * @copyright   Copyright (c) 2018-2019 GiaPhuGroup Co., Ltd. All rights reserved. (http://www.giaphugroup.com/)
- * @license     https://www.giaphugroup.com/LICENSE.txt
+ * @package     Digidirect_OnSaleProducts
  */
 
 namespace Digidirect\OnSaleProducts\Model\ResourceModel\Product;
 
-class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
-{
-    /**
-     * Join sales_bestsellers_aggregated_yearly relation table to retrieve the bestseller products
-     *
-     * @param int $storeId
-     * @return $this
-     */
-    public function getOnSaleProduct($storeId)
-    {
+class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection{
+    public function getOnSaleProduct(){
         $this->getSelect()->joinLeft(
                 ['catalogrule' => $this->getTable('catalogrule_product')],
                 'e.entity_id = catalogrule.product_id'
