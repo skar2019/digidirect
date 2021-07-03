92e30d4ecb926706293a6630f76ab2e1e0c30557
clintdigi <dev1@digidirect.com.au>
2021-07-03 15:07:54 +0800

    Fixed logger

--- app/code/Digidirect/Pronto/Cron/ProductEnquiry.php
+++ app/code/Digidirect/Pronto/Cron/ProductEnquiry.php
@@ -26,56 +26,56 @@ class ProductEnquiry
 
     public function productSet()
     {
-        $this->logger->info('Pronto Product Sync', array['info' => 'syncing set');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set'));
         $this->helper->productEnquiry();
         exit;
     }
     
     public function productSet1()
     {
-        $this->logger->info('Pronto Product Sync', array['info' => 'syncing set 1');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set 1'));
         $this->helper->productEnquiryOne();
         exit;
     }
     
     public function productSet2()
     {
-        $this->logger->info('Pronto Product Sync', array['info' => 'syncing set 2');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set2 '));
         $this->helper->productEnquiryTwo();
         exit;
     }
     
     public function productSet3()
     {
-        $this->logger->info('Pronto Product Sync', array['info' => 'syncing set 3');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set 3'));
         $this->helper->productEnquiryThree();
         exit;
     }
     
     public function productSet4()
     {
-        $this->logger->info('Pronto Product Sync', array['info' => 'syncing set 4');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set 4'));
         $this->helper->productEnquiryFour();
         exit;
     }
     
     public function productSet5()
     {
-        $this->logger->info('Pronto Product Sync', array['inf' => 'syncing set 5');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set 5'));
         $this->helper->productEnquiryFive();
         exit;
     }
     
     public function productSet6()
     {
-        $this->logger->info('Pronto Product Sync', array['inf' => 'syncing set 6');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set 6'));
         $this->helper->productEnquirySix();
         exit;
     }
     
     public function productSet7()
     {
-        $this->logger->info('Pronto Product Sync', array['inf' => 'syncing set 7');
+        $this->logger->info('Pronto Product Sync', array('info' => 'syncing set 7'));
         $this->helper->productEnquirySeven();
         exit;
     }
