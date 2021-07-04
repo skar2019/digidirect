fa338302ccdf6286612f3325e76f117691ac6f26
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-07-04 13:07:30 +0800

    Fixed mega menu on mobile

--- app/code/Magezon/NinjaMenus/view/frontend/web/js/ninjamenustop.js
+++ app/code/Magezon/NinjaMenus/view/frontend/web/js/ninjamenustop.js
@@ -42,7 +42,7 @@ define([
          */
         _assignControls: function () {
             this.controls = {
-                toggleBtn: $('[data-action="toggle-nav"]'),
+                toggleBtn: $('[data-action="toggle-nav"], .close-hamburger'),
                 swipeArea: $('.nav-sections')
             };
             return this;
