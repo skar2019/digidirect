f911365a97385e745442ee4b0121cfe4993d4c56
clintdigi <dev1@digidirect.com.au>
2021-07-06 01:14:35 +0800

    Local themes

--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -1,17 +1,16 @@
 module.exports = {
-    digi: {
-        area: 'frontend',
-        name: 'Ewave/digi',
-        locale: 'en_AU',
-        files: [
-            'css/styles-m',
-            'css/styles-l',
-            'css/fonts',
-            'css/print',
-            'mage/gallery/gallery',
-            'css/email-inline',
-            'css/email'
-        ],
-        dsl: 'less'
-    }
+   digi: {
+       area: 'frontend',
+       name: 'Digidirect/digi',
+       locale: 'en_AU',
+       files: [
+           'css/styles-m',
+           'css/styles-l',
+           'css/print',
+           'mage/gallery/gallery',
+           'css/email-inline',
+           'css/email'
+       ],
+       dsl: 'less'
+   }
 };
