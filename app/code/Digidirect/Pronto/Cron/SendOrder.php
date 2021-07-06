ed04cff257f99df3f606e4941d10afef08947b34
clintdigi <dev1@digidirect.com.au>
2021-07-06 16:20:05 +0800

    Removed try catch

--- app/code/Digidirect/Pronto/Cron/SendOrder.php
+++ app/code/Digidirect/Pronto/Cron/SendOrder.php
@@ -26,12 +26,7 @@ class SendOrder
 
     public function execute()
     {
-        
-        try {
-            $this->helper->sendOrder(); //use date today as parameter
-        } catch (\Exception $e) {
-            $this->logger->critical($e);
-        }
-        exit;
+        $this->helper->orderPost(); //use date today as parameter
+
     }
 }
