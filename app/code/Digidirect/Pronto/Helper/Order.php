c538096e2c3833457564117eabef4990201bfe85
clintdigi <dev1@digidirect.com.au>
2021-07-08 19:47:45 +0800

    Removed setting orders to complete

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -326,7 +326,7 @@ class Order extends AbstractHelper
                 //update order data with pronto order-no below
                 //$json['sales-order']['sales-order']['order-no']
 
-                //$order->setState("complete")->setStatus("complete");
+
                 $pronto = $json['sales-orders']['sales-order']['order-no'];
                 $invoiceno = $json['sales-orders']['sales-order']['invoice-no'];
                 $prontostatus = $json['sales-orders']['sales-order']['order-status-code'];
@@ -789,7 +789,7 @@ class Order extends AbstractHelper
                 //$json['sales-order']['sales-order']['order-no']
                 //echo "success";
                 //echo "<br>";
-                $order->setState("complete")->setStatus("complete");
+
                 $pronto = $json['sales-orders']['sales-order']['order-no'];
                 $invoiceno = $json['sales-orders']['sales-order']['invoice-no'];
                 $prontostatus = $json['sales-orders']['sales-order']['order-status-code'];
