9d31bfa8144b59e76589d34c8bb9749711f70f1e
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-07-08 13:15:42 +0800

    Fixed collection

--- app/code/Digidirect/OnSaleProducts/Model/ResourceModel/Product/Collection.php
+++ app/code/Digidirect/OnSaleProducts/Model/ResourceModel/Product/Collection.php
@@ -14,44 +14,48 @@ namespace Digidirect\OnSaleProducts\Model\ResourceModel\Product;
 
 class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection{
    public function getOnSaleProduct(){
-       $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->create(
-           '\Magento\Store\Model\StoreManagerInterface'
-       );
-       $catalogRule = \Magento\Framework\App\ObjectManager::getInstance()->create(
-            '\Magento\CatalogRule\Model\RuleFactory'
-       );
+    $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->create(
+                '\Magento\Store\Model\StoreManagerInterface'
+        );
+        $catalogRule = \Magento\Framework\App\ObjectManager::getInstance()->create(
+                '\Magento\CatalogRule\Model\RuleFactory'
+        );
 
-       $websiteId = $storeManager->getStore()->getWebsiteId();//current Website Id
+        $websiteId = $storeManager->getStore()->getWebsiteId(); //current Website Id
 
-       $resultProductIds = [];
-       $catalogRuleCollection = $catalogRule->create()->getCollection()->setOrder('rule_id','ASC');
-       $catalogRuleCollection->addIsActiveFilter(1);//filter for active rules only
+        $resultProductIds = [];
+        $catalogRuleCollection = $catalogRule->create()->getCollection()->setOrder('created_in', 'DESC');
+        $catalogRuleCollection->addIsActiveFilter(1); //filter for active rules only
 
-       $catalogRuleCollection->setCurPage(1);//filter for active rules only
+        $catalogRuleCollection->setCurPage(1); //filter for active rules only
 
-       $limit = 0;
+        $limit = 0;
 
-       foreach ($catalogRuleCollection as $catalogRule) {
-           $productIdsAccToRule = $catalogRule->getMatchingProductIds();
+        foreach ($catalogRuleCollection as $catalogRule) {
+            $productIdsAccToRule = $catalogRule->getMatchingProductIds();
 
-           if($limit == 15){
-               break;
-           }
+            if ($limit == 15) {
+                break;
+            }
 
-           foreach ($productIdsAccToRule as $productId => $ruleProductArray) {
-               if (!empty($ruleProductArray[$websiteId])) {
-                   if (array_key_exists($productId, $productIdsAccToRule)) {
+            foreach ($productIdsAccToRule as $productId => $ruleProductArray) {
+                if (!empty($ruleProductArray[$websiteId])) {
+                    if (array_key_exists($productId, $productIdsAccToRule)) {
+                        $discount_amount = $catalogRule->getData('discount_amount');
 
-                        if($limit == 15){
+                        if ($limit == 15) {
                             break;
                         }
 
-                        $resultProductIds[$productId] = $productId;
-                        $limit++;
-                   }
-               }
-           }
-       }
+                        if ($discount_amount > 0) {
+                            $resultProductIds[$productId] = $productId;
+                            $limit++;
+                        }
+                    }
+                }
+            }
+        }
+
 
         $this->getSelect()->where('e.entity_id IN (' . implode(',', $resultProductIds) .')')->group('e.entity_id')->limit(15);
         return $this;
