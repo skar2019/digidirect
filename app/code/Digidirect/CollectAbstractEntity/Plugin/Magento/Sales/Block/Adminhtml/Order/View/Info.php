e6fc2f7d6b176aeff12a20a4eaa1f515213db749
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2021-07-10 15:22:14 -0700

    Fix Sales Order Shipping

--- app/code/Digidirect/CollectAbstractEntity/Plugin/Magento/Sales/Block/Adminhtml/Order/View/Info.php
+++ app/code/Digidirect/CollectAbstractEntity/Plugin/Magento/Sales/Block/Adminhtml/Order/View/Info.php
@@ -45,7 +45,7 @@ class Info
             $prefix = __('Store: %1 (ID #%2)', $storeLocatorItem->getName(), $storeLocatorItem->getId());
         }
 
-        //return (!empty($prefix) ? $prefix . '<br /><br />' : '') . $proceed($address);
-        return (!empty($prefix) ? $prefix . '<br /><br />' : '') . $storeLocatorItem->getAddress();
+        return (!empty($prefix) ? $prefix . '<br /><br />' : '') . $proceed($storeLocatorItem->getAddress());
+        //return (!empty($prefix) ? $prefix . '<br /><br />' : '') . $storeLocatorItem->getAddress();
     }
 }
