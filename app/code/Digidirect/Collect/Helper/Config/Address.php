6c4561e08d37b2deffc9f2f3fe9f6e780047d088
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-07-11 14:28:48 +0800

    Added validation on shipping address

--- app/code/Digidirect/Collect/Helper/Config/Address.php
+++ app/code/Digidirect/Collect/Helper/Config/Address.php
@@ -264,8 +264,13 @@ class Address extends \Magento\Framework\App\Helper\AbstractHelper
      */
     public function applyDummyAddress(\Magento\Quote\Model\Quote\Address $address)
     {
-        $qouteAddress = (array) $address;
-        $address->addData($qouteAddress);
+        if(!empty($address)){
+            $qouteAddress = (array) $address;
+            $address->addData($qouteAddress);
+        }else{
+            $address->addData($qouteAddress);
+        }
+
         return $address;
     }
 
