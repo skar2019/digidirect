928a47437e265fd2fddf1b67bdc0ae7baf999b60
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2021-07-10 19:02:40 -0700

    Fix Sales Order Shipping

--- app/code/Digidirect/CollectAbstractEntity/Plugin/Magento/Sales/Block/Adminhtml/Order/View/Info.php
+++ app/code/Digidirect/CollectAbstractEntity/Plugin/Magento/Sales/Block/Adminhtml/Order/View/Info.php
@@ -21,9 +21,11 @@ class Info
      * @param \Digidirect\Collect\Model\OrderStoreLocatorInfo $orderStoreLocatorInfo
      */
     public function __construct(
-        \Digidirect\Collect\Model\OrderStoreLocatorInfo $orderStoreLocatorInfo
+        \Digidirect\Collect\Model\OrderStoreLocatorInfo $orderStoreLocatorInfo,
+        \Magento\Sales\Model\ResourceModel\Order\Address\CollectionFactory $addressCollection
     ) {
         $this->orderStoreLocatorInfo = $orderStoreLocatorInfo;
+        $this->addressCollection = $addressCollection;
     }
 
     /**
@@ -44,7 +46,16 @@ class Info
         ) {
             $prefix = __('Store: %1 (ID #%2)', $storeLocatorItem->getName(), $storeLocatorItem->getId());
         }
+        
+        $proceed($address);
 
-        return (!empty($prefix) ? $prefix . '<br /><br />' : '') . $proceed($address);
+        //return (!empty($prefix) ? $prefix . '<br /><br />' : '') . $proceed($address);
+        
+//        $order = $this->getOrderData($address->getOrder()->sget);
+//        $orderBillingId = $order->getBillingAddressId();
+//        $address = $this->addressCollection->create()->addFieldToFilter('entity_id',array($orderBillingId))->getFirstItem();
+//        return $address;
+        
+        return (!empty($prefix) ? $prefix . '<br /><br />' : '') . " Testing Address!";
     }
 }
