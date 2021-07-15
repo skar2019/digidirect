94331999e1d6b654323423f00b06525e8a484647
clintdigi <dev1@digidirect.com.au>
2021-07-15 17:58:19 +0800

    Category and Metatitle update

---
+++ app/code/Digidirect/Pronto/Controller/Index/CategoryUpdate.php
@@ -0,0 +1,70 @@
+<?php
+namespace Digidirect\Pronto\Controller\Index;
+
+use Digidirect\Pronto\Helper\Product;
+use Magento\Catalog\Api\CategoryLinkRepositoryInterface;
+
+class CategoryUpdate extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+
+    protected $_productCollectionFactory;
+    
+    protected $categoryFactory;
+    
+    protected $categoryLinkRepository;
+            
+    public function __construct(
+            \Magento\Framework\App\Action\Context $context,
+            Product $helper,
+            \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+            \Magento\InventoryApi\Api\SourceItemsSaveInterface $sourceItemsSaveInterface,
+            \Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory $sourceItemFactory,
+            \Magento\InventoryCatalogAdminUi\Model\GetSourceItemsDataBySku $sourceDataBySku,
+            \Magento\Catalog\Model\Product\Attribute\Source\Status $productStatus,
+            \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+            CategoryLinkRepositoryInterface $categoryLinkRepository,
+            array $data = [])
+    {
+            $this->helper = $helper;
+            $this->_productCollectionFactory = $productCollectionFactory;
+            $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
+            $this->sourceItemFactory = $sourceItemFactory;
+            $this->sourceDataBySku = $sourceDataBySku;
+            $this->productStatus = $productStatus;
+            $this->categoryFactory = $categoryFactory;
+            $this->categoryLinkRepository = $categoryLinkRepository;
+            return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        if(isset($_GET["cat"])){
+            $categoryId = $_GET["cat"];
+        }
+        set_time_limit(600);
+        //$categoryId = 339; //Paper ID
+        $category = $this->categoryFactory->create()->load($categoryId);
+        $categoryProducts = $category->getProductCollection()
+                ->addAttributeToSelect('*')
+                ->setPageSize(1000);
+        
+        $isProductUnassigned = false;
+        foreach ($categoryProducts as $product) {
+            $sku = $product->getSku();
+
+            try 
+            {
+                $isProductUnassigned = $this->categoryLinkRepository->deleteByIds($categoryId, $sku);
+                echo $sku." - ".$isProductUnassigned."<br>";
+            } catch (Exception $ex) {
+                continue;
+                echo $sku." - ".$isProductUnassigned."<br>";
+            }
+            
+            
+        }
+
+        exit();
+    }
+}
---
+++ app/code/Digidirect/Pronto/Controller/Index/UpdateMetaTitle.php
@@ -0,0 +1,75 @@
+<?php
+namespace Digidirect\Pronto\Controller\Index;
+
+use Digidirect\Pronto\Helper\Product;
+
+class UpdateMetaTitle extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+
+    protected $_productCollectionFactory;
+
+    protected $productRepository;
+
+    public function __construct(
+            \Magento\Framework\App\Action\Context $context,
+            Product $helper,
+            \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+            \Magento\InventoryApi\Api\SourceItemsSaveInterface $sourceItemsSaveInterface,
+            \Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory $sourceItemFactory,
+            \Magento\InventoryCatalogAdminUi\Model\GetSourceItemsDataBySku $sourceDataBySku,
+            \Magento\Catalog\Model\Product\Attribute\Source\Status $productStatus,
+            \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+            array $data = [])
+    {
+            $this->helper = $helper;
+            $this->_productCollectionFactory = $productCollectionFactory;
+            $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
+            $this->sourceItemFactory = $sourceItemFactory;
+            $this->sourceDataBySku = $sourceDataBySku;
+            $this->productStatus = $productStatus;
+            $this->productRepository = $productRepository;
+            return parent::__construct($context);
+    }
+
+    public function execute(){
+        
+        $page_number = 1;
+        
+        if(isset($_GET["p"])){
+            $page_number = $_GET["p"];
+        }
+        
+        /*Get in stock product collection*/
+        $collection = $this->_productCollectionFactory->create()->addFieldToSelect('*')
+            ->setPageSize(1000) // only get 10 products
+            ->setCurPage($page_number)  // first page (means limit 0,10)
+            ->setFlag('has_stock_status_filter', false);
+        
+        $concat = " | Buy at digiDirect";
+        $counter = 0;
+
+        foreach ($collection as $key => $product) {
+            $sku = $product->getSku();
+            $productName = $product->getName();
+            $meta_title = $product->getMetaTitle();
+            
+            $existing_meta_title = $productName . $concat;
+            
+            if($meta_title != $existing_meta_title){
+                $meta_title = $productName . $concat;
+
+                $productRepo = $this->productRepository->get($sku);
+                $productRepo->setMetaTitle($meta_title);
+                $this->productRepository->save($productRepo);
+
+                echo $productName . " - "  . $sku . " <br />" . $existing_meta_title . "<br /><br />";
+                $counter++;
+            }
+        }
+        
+        echo "Total updated SKUs: " . $counter;
+
+        exit();
+    }
+}
