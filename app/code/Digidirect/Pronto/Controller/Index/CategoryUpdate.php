9e81947c15ef2902b0ecad42499672b13fb626f8
clintdigi <dev1@digidirect.com.au>
2021-07-15 19:18:28 +0800

    Removed limit

--- app/code/Digidirect/Pronto/Controller/Index/CategoryUpdate.php
+++ app/code/Digidirect/Pronto/Controller/Index/CategoryUpdate.php
@@ -46,8 +46,7 @@ class CategoryUpdate extends \Magento\Framework\App\Action\Action
         //$categoryId = 339; //Paper ID
         $category = $this->categoryFactory->create()->load($categoryId);
         $categoryProducts = $category->getProductCollection()
-                ->addAttributeToSelect('*')
-                ->setPageSize(500);
+                ->addAttributeToSelect('*');
         
         $isProductUnassigned = false;
         foreach ($categoryProducts as $product) {
