8dc0dfad4f9b180c00429a6eaf0508be25f33287
clintdigi <dev1@digidirect.com.au>
2021-07-16 17:22:29 +0800

    Restore Qantas

--- app/code/Digidirect/Qantas/registration.php
+++ app/code/Digidirect/Qantas/registration.php
@@ -1,8 +1,6 @@
 <?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Digi_Qantas',
-    __DIR__
-);
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Digidirect_Qantas',
+        __DIR__
+    );
