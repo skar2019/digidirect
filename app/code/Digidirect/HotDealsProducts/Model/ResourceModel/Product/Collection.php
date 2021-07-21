d329e6f8494c810ece556583f6b8ddc739c94c8b
clintdigi <dev1@digidirect.com.au>
2021-07-21 14:41:22 +0800

    Randomiser

--- app/code/Digidirect/HotDealsProducts/Model/ResourceModel/Product/Collection.php
+++ app/code/Digidirect/HotDealsProducts/Model/ResourceModel/Product/Collection.php
@@ -13,9 +13,9 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
         $objectManager =  \Magento\Framework\App\ObjectManager::getInstance();
         $categoryFactory = $objectManager->get('\Magento\Catalog\Model\CategoryFactory');
         $category = $categoryFactory->create()->load(1814);
-        $category->getProductCollection()->setPageSize(15);
         $categoryProducts = $category->getProductCollection()->addAttributeToSelect('*');
-        
+        $categoryProducts->getSelect()->orderRand();
+       
         return $categoryProducts;
     }
 }
--- app/code/Digidirect/OnSaleProducts/Model/ResourceModel/Product/Collection.php
+++ app/code/Digidirect/OnSaleProducts/Model/ResourceModel/Product/Collection.php
@@ -13,49 +13,58 @@
 namespace Digidirect\OnSaleProducts\Model\ResourceModel\Product;
 
 class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection{
-   public function getOnSaleProduct(){
-    $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->create(
-                '\Magento\Store\Model\StoreManagerInterface'
-        );
-        $catalogRule = \Magento\Framework\App\ObjectManager::getInstance()->create(
-                '\Magento\CatalogRule\Model\RuleFactory'
-        );
+  public function getOnSaleProduct(){
+   $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->create(
+               '\Magento\Store\Model\StoreManagerInterface'
+       );
+       $catalogRule = \Magento\Framework\App\ObjectManager::getInstance()->create(
+               '\Magento\CatalogRule\Model\RuleFactory'
+       );
 
-        $websiteId = $storeManager->getStore()->getWebsiteId(); //current Website Id
+       $websiteId = $storeManager->getStore()->getWebsiteId(); //current Website Id
 
-        $resultProductIds = [];
-        $catalogRuleCollection = $catalogRule->create()->getCollection()->setOrder('created_in', 'DESC');
-        $catalogRuleCollection->addIsActiveFilter(1); //filter for active rules only
+       $resultProductIds = [];
+       $catalogRuleCollection = $catalogRule->create()->getCollection();//setOrder('created_in', 'DESC');
+       $catalogRuleCollection->getSelect()->orderRand();
+       $catalogRuleCollection->addIsActiveFilter(1); //filter for active rules only
 
-        $catalogRuleCollection->setCurPage(1); //filter for active rules only
+       $catalogRuleCollection->setCurPage(1); //filter for active rules only
 
-        $limit = 0;
+       $limit = 0;
 
-        foreach ($catalogRuleCollection as $catalogRule) {
-            $productIdsAccToRule = $catalogRule->getMatchingProductIds();
+       foreach ($catalogRuleCollection as $catalogRule) {
+           $ctr = 0;
+           $productIdsAccToRule = $catalogRule->getMatchingProductIds();
 
-            if ($limit == 30) {
-                break;
-            }
+           if ($limit == 30) {
+               break;
+           }
 
-            foreach ($productIdsAccToRule as $productId => $ruleProductArray) {
-                if (!empty($ruleProductArray[$websiteId])) {
-                    if (array_key_exists($productId, $productIdsAccToRule)) {
-                        $discount_amount = $catalogRule->getData('discount_amount');
+           foreach ($productIdsAccToRule as $productId => $ruleProductArray) {
+               
+               if ($ctr == 5) {
+                   $ctr = 0;
+                   break;
+               }
+               
+               if (!empty($ruleProductArray[$websiteId])) {
+                   if (array_key_exists($productId, $productIdsAccToRule)) {
+                       $discount_amount = $catalogRule->getData('discount_amount');
 
-                        if ($limit == 30) {
-                            break;
-                        }
+                       if ($limit == 30) {
+                           break;
+                       }
 
-                        $resultProductIds[$productId] = $productId;
-                        $limit++;
-                    }
-                }
-            }
-        }
+                       $resultProductIds[$productId] = $productId;
+                       $limit++;
+                   }
+               }
+               $ctr++;
+           }
+       }
 
 
-        $this->getSelect()->where('e.entity_id IN (' . implode(',', $resultProductIds) .')')->group('e.entity_id')->limit(15);
-        return $this;
-   }
+       $this->getSelect()->orderRand()->where('e.entity_id IN (' . implode(',', $resultProductIds) .')')->group('e.entity_id')->limit(15);
+       return $this;
+  }
 }
