329d14513bd617c7563ad1fb4ee83ba1d06ea51b
clintdigi <dev1@digidirect.com.au>
2021-07-23 13:15:15 +0800

    comment var dump

--- app/code/Digidirect/Pronto/Helper/TestPronto.php
+++ app/code/Digidirect/Pronto/Helper/TestPronto.php
@@ -1023,8 +1023,8 @@ class TestPronto extends AbstractHelper
             $data['sales-order']['detail']['line'][$x]['sol-chg-type'] = 0;
             $data['sales-order']['detail']['line'][$x]['sol-line-total-inc-tax'] = $shippingprice;
 
-            var_dump($data['sales-order']['detail']['line']);
-            exit;
+            //var_dump($data['sales-order']['detail']['line']);
+            //exit;
             //create xml of order data here
             $this->logger->info('Pronto Order Sync Data - ',$data['sales-order']);
             $xml = \Digidirect\AI\Model\Lib\Adapter\Import\Xml::assocToXml($data, 'sales-orders');
