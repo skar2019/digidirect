bab6939415d924f09def9500f00b21d7e5a49abc
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-07-26 09:31:00 +0800

    Fix for ZipMoney auto cancel

---
+++ app/code/Zip/ZipPayment/Gateway/Request/CancelDataBuilder.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Zip\ZipPayment\Gateway\Request;
+
+use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
+use Magento\Sales\Api\Data\OrderPaymentInterface;
+
+/**
+ * @author    Zip Plugin Team <integrations@zip.co>
+ * @copyright 2020 Zip Co Limited
+ * @link      https://www.zip.co
+ */
+class CancelDataBuilder extends AbstractDataBuilder
+{
+    /**
+     * Builds ENV request
+     *
+     * @param array $buildSubject
+     * @return array
+     */
+    public function build(array $buildSubject)
+    {
+        if (!isset($buildSubject['payment'])
+            || !$buildSubject['payment'] instanceof PaymentDataObjectInterface
+        ) {
+            throw new \InvalidArgumentException('Payment data object should be provided');
+        }
+
+        /** @var PaymentDataObjectInterface $paymentDO */
+        $paymentDO = $buildSubject['payment'];
+
+        $order = $paymentDO->getOrder();
+
+        $payment = $paymentDO->getPayment();
+
+        $this->_logger->debug("Cancel Request");
+
+        if (!$payment instanceof OrderPaymentInterface) {
+            throw new \LogicException('Order payment should be provided.');
+        }
+
+        $return['txn_id'] = $payment->getLastTransId();
+        $return['zip_charge_id'] = $payment->getAdditionalInformation('zip_charge_id');
+
+        return $return;
+    }
+}
---
+++ app/code/Zip/ZipPayment/Gateway/Response/CaptureHandler.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Zip\ZipPayment\Gateway\Response;
+
+use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
+use Magento\Payment\Gateway\Response\HandlerInterface;
+
+/**
+ * @author    Zip Plugin Team <integrations@zip.co>
+ * @copyright 2020 Zip Co Limited
+ * @link      https://zip.co
+ */
+class CaptureHandler implements HandlerInterface
+{
+
+    /**
+     * Handles charge capture response
+     *
+     * @param array $handlingSubject
+     * @param array $response
+     * @return void
+     */
+    public function handle(array $handlingSubject, array $response)
+    {
+        if (!isset($handlingSubject['payment'])
+            || !$handlingSubject['payment'] instanceof PaymentDataObjectInterface
+        ) {
+            throw new \InvalidArgumentException('Payment data object should be provided');
+        }
+
+        /** @var PaymentDataObjectInterface $paymentDO */
+        $paymentDO = $handlingSubject['payment'];
+        /** @var $payment \Magento\Sales\Model\Order\Payment */
+        $payment = $paymentDO->getPayment();
+        $payment->setTransactionId($response['api_response']->getId());
+        $payment->setIsTransactionClosed(false);
+    }
+}
