ece63495f96cba662ff6f53aa61bd7dd6df4537d
JohnryMBPro <johnryvaleriano@Johnrys-MBP>
2021-07-28 14:02:04 +0800

    Fix for unit number

--- app/code/Digidirect/Order/Observer/SetOrderAttribute.php
+++ app/code/Digidirect/Order/Observer/SetOrderAttribute.php
@@ -12,6 +12,9 @@ class SetOrderAttribute implements \Magento\Framework\Event\ObserverInterface {
      */
     protected $_customerRepository;
     protected $customerSession;
+    protected $repositoryAddress;
+    protected $orderAddressInterface;
+    protected $orderRepoInterface;
 
     /**
      * @param \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
@@ -21,13 +24,17 @@ class SetOrderAttribute implements \Magento\Framework\Event\ObserverInterface {
             \Magento\Customer\Model\Session $customerSession,
             \Magento\Framework\App\Filesystem\DirectoryList $directoryList,
             \Magento\Framework\Filesystem $filesystem,
-            \Magento\Framework\File\Csv $csvProcessor
+            \Magento\Framework\File\Csv $csvProcessor,
+            \Magento\Sales\Model\Order\AddressRepository $repositoryAddress,
+            \Magento\Sales\Api\Data\OrderAddressInterface $orderAddressInterface
     ) {
         $this->_customerRepository = $customerRepository;
         $this->customerSession = $customerSession;
         $this->directoryList = $directoryList;
         $this->filesystem = $filesystem;
         $this->csvProcessor = $csvProcessor;
+        $this->repositoryAddress= $repositoryAddress;
+        $this->_orderAddressInterface = $orderAddressInterface;
     }
 
     /**
@@ -42,12 +49,32 @@ class SetOrderAttribute implements \Magento\Framework\Event\ObserverInterface {
         $customerEmail = $order->getCustomerEmail();
         $isGuest = $order->getCustomerIsGuest();
 
+        //Set unit number for Shipping Address
+        $shippingAddressID = $order->getShippingAddress()->getId();
+        $initialShippingAddressUnitNumber = $order->getShippingAddress()->getUnitNumber();
+        $initialShippingAddressUnitNumber = str_replace("unit_number", "", $initialShippingAddressUnitNumber);
+        $shippingAddressUnitNumber = str_replace("\n", "", $initialShippingAddressUnitNumber);
+
+        $shipAddress = $this->repositoryAddress->get($shippingAddressID);
+        $shipAddress->setUnitNumber($shippingAddressUnitNumber);
+        $this->repositoryAddress->save($shipAddress);
+
+        //Set unit number for Billing Address
+        $billingAddressID = $order->getBillingAddress()->getId();
+        $initialBillingAddressUnitNumber = $order->getBillingAddress()->getUnitNumber();
+        $initialBillingAddressUnitNumber = str_replace("unit_number", "", $initialBillingAddressUnitNumber);
+        $billingAddressUnitNumber = str_replace("\n", "", $initialBillingAddressUnitNumber);
+
+        $billingAddress = $this->repositoryAddress->get($billingAddressID);
+        $billingAddress->setUnitNumber($billingAddressUnitNumber);
+        $this->repositoryAddress->save($billingAddress);
+
         if ($isGuest) {
             if (isset($_SESSION["qantasqff"]) && !empty($_SESSION["qantasqff"])) {
-                
+
                 $qff_number = $_SESSION["qff_number"];
                 $qff_lastname = $_SESSION["qff_lastname"];
-                
+
 //                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 //                $path = $objectManager->get('Magento\Framework\App\Filesystem\DirectoryList');
 //
@@ -63,7 +90,7 @@ class SetOrderAttribute implements \Magento\Framework\Event\ObserverInterface {
 //                fwrite($handle, $qff_number);
 //
 //                fclose($handle);
-                
+
                 $order->setQffNumber($qff_number)->save();
 
                 $order->setQffLastname($qff_lastname)->save();
