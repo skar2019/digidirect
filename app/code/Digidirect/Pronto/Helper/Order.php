66aab28b912496f1faff39c4e810034ec340b56e
clintdigi <dev1@digidirect.com.au>
2021-08-01 13:15:37 +0800

    sync 31st

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -1207,7 +1207,7 @@ class Order extends AbstractHelper
 
     public function getTestOrderCollectionByDay()
     {
-        $date = '2021-07-30';
+        $date = '2021-07-31';
         $fromDate = date('Y-m-d'. ' 00:00:00',strtotime($date));
         $toDate = date('Y-m-d'. ' 23:59:59',strtotime($date));
         $collection = $this->_orderCollectionFactory->create()
