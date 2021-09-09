f393a32dabbead13e659e1df62fba6695c91318d
clintdigi <dev1@digidirect.com.au>
2021-09-09 11:21:06 +0800

    Sync order with new amazon logic for IR

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -163,7 +163,7 @@ class Order extends AbstractHelper
             $account = $this->getAccount($order);
             $address = $order->getBillingAddress();
             $countrycode = $address->getCountryId();
-
+            $amShipping = $order->getShippingDescription();
             $is_am_order = false;
             $is_am_fba = false;
             if (strpos($orderId, 'AM') !== false) {
@@ -171,9 +171,8 @@ class Order extends AbstractHelper
             }
 
             if($is_am_order){
-                $rep = "AMAZON MFH";
-                if($accountname == "N/A N/A")
-                {
+                $rep = "AMAZON MFN";
+                if (strpos($amShipping, 'AFN') !== false) {
                     $rep = "AMAZON FBA";
                     $account = "AMAZ00";
                     if($countrycode == "NZ")
@@ -185,6 +184,7 @@ class Order extends AbstractHelper
                     $territory = "AWHS";
                     $is_am_fba = true;
                 }
+
             }
             else
             {
@@ -334,6 +334,12 @@ class Order extends AbstractHelper
                     $payment_type ="MD";
                     $catchRef = $orderId;
                     $catchRef = str_replace("MYD","",$catchRef);
+                    $payment_reference = $catchRef;
+                }
+                else if (strpos($orderId, 'AM') !== false) {
+                    $payment_type ="AM";
+                    $catchRef = $orderId;
+                    $catchRef = str_replace("AM","",$catchRef);
                     $payment_reference = $catchRef;
                 }
             }
