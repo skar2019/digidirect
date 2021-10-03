51e9637ca3041a4f5ea87d6c229b3e2ce60a2fb6
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2021-10-02 21:55:16 -0700

    Reskin (Shop By Category Module)

---
+++ app/code/Digidirect/ShopByCategory/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Author : Rondel Y. Dalumpines
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Digidirect_ShopByCategory',
+    __DIR__
+);
