b3cdec6000d302c1a315db915967fdc5b4206863
clintdigi <dev1@digidirect.com.au>
2021-10-18 16:30:58 +0800

    revert workaround

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/cashback-label.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/cashback-label.js
@@ -1,14 +1,6 @@
 define([
     'jquery'
 ], function ($) {
-    if( $('.qantas-pts-wrapper').length )         // use this if you are using class to check
-    {
-        //$('.qantas-new-container').append($('.qantas-pts-wrapper'));
-        $('.qantas-pts-wrapper').insertAfter($('.product-options-wrapper'));
-    }
-    $('.secure-pay-container').insertAfter($('.product-add-form'));
-
-    $('.current-price-wrapper').insertBefore($('.qantas-pts-wrapper'));
     var cashbackLabel = $('.product-info-main').next('block-extendedrule');
     var productInfoPriceBlock = $('.product-info-price');
     if(cashbackLabel){
@@ -16,22 +8,10 @@ define([
         if($('.price-box.price-final_price.cashback-active').find('.old-price-label').length === 0)
         {
             productInfoPriceBlock.find('.price-box.price-final_price').addClass('pad-top');
-
-            if( $('.current-price-wrapper').length )         // use this if you are using class to check
-            {
-                $('.block-extendedrule').insertAfter( $('.current-price-wrapper'));
-
-            }
-            else
-            {
-                $('.block-extendedrule').insertAfter( $('.qantas-pts-wrapper'));
-            }
         }
 
-
-        //productInfoPriceBlock.append($('.block-extendedrule'));
-     }
+        productInfoPriceBlock.append($('.block-extendedrule'));
 
 
-
+    }
 });
