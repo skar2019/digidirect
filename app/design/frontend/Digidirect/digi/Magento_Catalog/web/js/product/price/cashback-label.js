783452d59a6330c07448578efe2b2ce57e03bf30
clintdigi <dev1@digidirect.com.au>
2021-10-18 12:24:49 +0800

    Qantas and price positions

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/cashback-label.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/cashback-label.js
@@ -1,6 +1,14 @@
 define([
     'jquery'
 ], function ($) {
+    if( $('.qantas-pts-wrapper').length )         // use this if you are using class to check
+    {
+        //$('.qantas-new-container').append($('.qantas-pts-wrapper'));
+        $('.qantas-pts-wrapper').insertAfter($('.product-options-wrapper'));
+    }
+    $('.secure-pay-container').insertAfter($('.product-add-form'));
+
+    $('.current-price-wrapper').insertBefore($('.qantas-pts-wrapper'));
     var cashbackLabel = $('.product-info-main').next('block-extendedrule');
     var productInfoPriceBlock = $('.product-info-price');
     if(cashbackLabel){
@@ -9,9 +17,19 @@ define([
         {
             productInfoPriceBlock.find('.price-box.price-final_price').addClass('pad-top');
         }
+        if( $('.current-price-wrapper').length )         // use this if you are using class to check
+        {
+            $('.block-extendedrule').insertAfter( $('.current-price-wrapper'));
 
-        productInfoPriceBlock.append($('.block-extendedrule'));
+        }
+        else
+        {
+            $('.block-extendedrule').insertAfter( $('.qantas-pts-wrapper'));
+        }
+
+        //productInfoPriceBlock.append($('.block-extendedrule'));
+     }
 
 
-    }
+
 });
