b0c5448bd0f9e2470fd03f1b3107c4b79e5e0c2d
clintdigi <dev1@digidirect.com.au>
2021-10-18 19:24:31 +0800

    debuging

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/cashback-label.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/cashback-label.js
@@ -1,11 +1,9 @@
 define([
     'jquery'
 ], function ($) {
-    if( $('.qantas-pts-wrapper').length )         // use this if you are using class to check
-    {
-        //$('.qantas-new-container').append($('.qantas-pts-wrapper'));
-        $('.qantas-pts-wrapper').insertAfter($('.product-options-wrapper'));
-    }
+    alert("cashback?");
+    $('.qantas-pts-wrapper').insertAfter($('.product-options-wrapper'));
+
     $('.secure-pay-container').insertAfter($('.product-add-form'));
 
     $('.current-price-wrapper').insertBefore($('.qantas-pts-wrapper'));
