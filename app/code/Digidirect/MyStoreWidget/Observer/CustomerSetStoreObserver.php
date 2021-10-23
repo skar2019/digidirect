bdfdd6f5852c1e11b22d4572de8ecd81ab5c8548
clintdigi <dev1@digidirect.com.au>
2021-10-23 09:35:11 +0800

    uncomment to redeploy

--- app/code/Digidirect/MyStoreWidget/Observer/CustomerSetStoreObserver.php
+++ app/code/Digidirect/MyStoreWidget/Observer/CustomerSetStoreObserver.php
@@ -83,7 +83,6 @@ class CustomerSetStoreObserver implements ObserverInterface
 //                $this->messageManager->addErrorMessage(__(
 //                    'Sorry, but we can\'t set up your Store using your Shipping Address.'
 //                ));
-                //comment to redeploy
             }
         }
 
