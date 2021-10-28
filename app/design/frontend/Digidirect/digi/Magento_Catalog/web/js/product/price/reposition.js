98d39fa03d74469b85f20a90970e50c7830d1596
clintdigi <dev1@digidirect.com.au>
2021-10-28 16:43:59 +0800

    work around hide studio 19 if empty

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -21,7 +21,7 @@ define([
     $('.widget-product').insertAfter($('.product-add-form'));
 
     $('.product-info-price .current-price-wrapper').insertBefore($('#main-product-qantas'));
-    if( $('.studio19-wrapper').is(':empty') ) {
+    if($('#leftmenu').contents().length == 0) {
         $('.studio19-wrapper').hide();
     }
 });
