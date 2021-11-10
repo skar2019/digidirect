81e9feb858489033d8d26ab9b9fe7cf58423a16f
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2021-11-10 22:12:49 +0800

    Shop By Brands Carousel Display 6 Brands

--- app/code/Mageplaza/Shopbybrand/view/frontend/web/js/brand-slider.js
+++ app/code/Mageplaza/Shopbybrand/view/frontend/web/js/brand-slider.js
@@ -28,7 +28,7 @@ define([
             loop: true,
             margin: 10,
             autoHeight: true,
-            //autoWidth: true,
+            nav: true,
             autoplay: true,
             autoplayTimeout: 4000,
             autoplayHoverPause: true,
