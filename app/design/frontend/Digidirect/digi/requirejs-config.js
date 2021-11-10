c23f4045818774ad44f258eca365776a25ddb5a9
clintdigi <dev1@digidirect.com.au>
2021-11-10 22:19:58 +0800

    remove form mini

--- app/design/frontend/Digidirect/digi/requirejs-config.js
+++ app/design/frontend/Digidirect/digi/requirejs-config.js
@@ -11,7 +11,7 @@ var config = {
             'truncateCollection' : 'js/dist/widgets/truncate-collection',
             'productTile' : 'js/dist/widgets/product-tile',
             'secondaryNavigation': 'js/widgets/secondary-navigation',
-            'Magento_Search/form-mini': 'Magento_Search/js/form-mini', //Hack for js error in vendor/klevu/module-productsearch/view/frontend/web/js/disableautosuggest.js
+            //'Magento_Search/form-mini': 'Magento_Search/js/form-mini', //Hack for js error in vendor/klevu/module-productsearch/view/frontend/web/js/disableautosuggest.js
             'eventTransporter' : 'js/dist/widgets/event-transporter'
         }
     }
