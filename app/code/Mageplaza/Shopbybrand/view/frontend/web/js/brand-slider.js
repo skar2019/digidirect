4aed2447f4085996de1ea0d6d1d03f3f0acb22ca
clintdigi <dev1@digidirect.com.au>
2021-11-11 15:05:16 +0800

    brandslider

--- app/code/Mageplaza/Shopbybrand/view/frontend/web/js/brand-slider.js
+++ app/code/Mageplaza/Shopbybrand/view/frontend/web/js/brand-slider.js
@@ -40,7 +40,7 @@ define([
                 360: {items: 2},
                 540: {items: 3},
                 720: {items: 4},
-                900: {items: 6}
+                900: {items: 5}
             }
         });
     };
