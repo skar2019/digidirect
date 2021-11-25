e510dfa53479a74ac0c44e0d138ed27a0765bd40
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2021-11-25 10:04:15 +0800

    Shop By Category Product Visibility Test

--- app/code/Digidirect/ShopByCategory/Block/Index/View.php
+++ app/code/Digidirect/ShopByCategory/Block/Index/View.php
@@ -32,7 +32,6 @@ class View extends Template
         $productCollection = $this->_productCollectionFactory->create();
         $productCollection->addAttributeToSelect('*');
         $productCollection->addCategoriesFilter(['in' => $categoryId]);
-        $productCollection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_IN_CATALOG);
         $productCollection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
         $productCollection->addAttributeToFilter('brand', $brandId);
         return $productCollection->count();
