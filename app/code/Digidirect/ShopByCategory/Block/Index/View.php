beec6b35d8da9da70909d24ee593c189e71192ec
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2021-11-26 09:13:16 +0800

    Shop By Category Revert Visibility

--- app/code/Digidirect/ShopByCategory/Block/Index/View.php
+++ app/code/Digidirect/ShopByCategory/Block/Index/View.php
@@ -32,7 +32,6 @@ class View extends Template
         $productCollection = $this->_productCollectionFactory->create();
         $productCollection->addAttributeToSelect('*');
         $productCollection->addCategoriesFilter(['in' => $categoryId]);
-        $productCollection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_IN_SEARCH);
         $productCollection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
         $productCollection->addAttributeToFilter('brand', $brandId);
         return $productCollection->count();
--- app/code/Digidirect/ShopByCategory/Block/Widget/ShopByCategory.php
+++ app/code/Digidirect/ShopByCategory/Block/Widget/ShopByCategory.php
@@ -38,7 +38,6 @@ class ShopByCategory extends \Magento\Framework\View\Element\Template implements
         $productCollection = $this->_productCollectionFactory->create();
         $productCollection->addAttributeToSelect('*');
         $productCollection->addCategoriesFilter(['in' => $categoryId]);
-        $productCollection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_IN_SEARCH);
         $productCollection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
         return $productCollection->count();
     }
