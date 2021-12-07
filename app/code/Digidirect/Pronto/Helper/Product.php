bab0b3194389085e2e792119c47558330da95cb5
Clint <clint@kayweb.com.au>
2021-12-07 13:53:03 +0800

    Disable W condition

--- app/code/Digidirect/Pronto/Helper/Product.php
+++ app/code/Digidirect/Pronto/Helper/Product.php
@@ -460,10 +460,10 @@ class Product extends AbstractHelper
                     {
                         $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_DISABLED);
                     }
-                    else if($prod['stk-user-only-alpha4-1'] == 'W')
-                    {
-                        $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-                    }
+//                    else if($prod['stk-user-only-alpha4-1'] == 'W')
+//                    {
+//                        $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+//                    }
                     else if($prod['stk-user-only-alpha4-1'] == 'N')
                     {
                         $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_DISABLED);
@@ -840,10 +840,10 @@ class Product extends AbstractHelper
                     {
                         $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_DISABLED);
                     }
-                    else if($prod['stk-user-only-alpha4-1'] == 'W')
-                    {
-                        $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-                    }
+//                    else if($prod['stk-user-only-alpha4-1'] == 'W')
+//                    {
+//                        $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+//                    }
                     else if($prod['stk-user-only-alpha4-1'] == 'N')
                     {
                         $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_DISABLED);
@@ -2394,11 +2394,11 @@ class Product extends AbstractHelper
                         $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_DISABLED);
                         $endis = 'disabled';
                     }
-                    else if($prod['stk-user-only-alpha4-1'] == 'W')
-                    {
-                        $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-                        $endis = 'enabled';
-                    }
+//                    else if($prod['stk-user-only-alpha4-1'] == 'W')
+//                    {
+//                        $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+//                        $endis = 'enabled';
+//                    }
                     else if($prod['stk-user-only-alpha4-1'] == 'N')
                     {
                         $product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_DISABLED);
