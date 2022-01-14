3381349131c211ca5491d183417aac5f77ab2bb9
Clint <clint@kayweb.com.au>
2022-01-14 09:56:59 +0800

    fix top brands only on category menu

--- app/code/Mageplaza/Shopbybrand/Block/Widget/CategoryId.php
+++ app/code/Mageplaza/Shopbybrand/Block/Widget/CategoryId.php
@@ -74,24 +74,14 @@ class CategoryId extends AbstractBrand
      */
     public function getOptionIds()
     {
-        $top = array(137,120,8253,221,134,223,135,139,138,517,
-            353,54119521,7177,535,9307,12287,12269,
-            495,7105,13345,271,445,461,293,231,463,9364,
-            13321,273,355,243,507,567,
-            281,18988,19465,7095,9405,9370,315,
-            333,10265,305,19569,10179,19918,
-            9256,19563,7153,19888,19984,12347,14558,14501,18775,19891,
-            433,9214,10346,14420,13465,19533,19830,583,7159,9241);
 
         $str = $this->getData('category_id');
         $sql = 'main_table.cat_id IN (' . $str . ')';
         $result = [];
         $brands = $this->_categoryFactory->create()->getCategoryCollection($sql, null)->getData();
         foreach ($brands as $brand => $item) {
-            if (in_array($item['option_id'], $top, TRUE))
-            {
+
                 $result[] = $item['option_id'];
-            }
         }
     }
 
