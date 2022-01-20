bdf09ce8b273955cb39c2a4381dc1500e67aa113
Clint <clint@kayweb.com.au>
2022-01-20 14:42:27 +0800

    repcode miranda for pickup

--- app/code/Digidirect/Pronto/Helper/TestPronto.php
+++ app/code/Digidirect/Pronto/Helper/TestPronto.php
@@ -46,8 +46,10 @@ class TestPronto extends AbstractHelper
      */
     protected $repCodeForPickUp = [
         '11' => 'S7P',
+        '31' => 'B4P',
         '17' => 'M1P',
         '21' => 'M6P',
+        '19' => 'B5P',
         '23' => 'C3P',
         '1'  => 'S7P',
         '4'  => 'B4P',
@@ -55,8 +57,7 @@ class TestPronto extends AbstractHelper
         '10' => 'B5P',
         '13' => 'M6P',
         '16' => 'C3P',
-        '19' => 'B5P',
-        '31' => 'B4P',
+        '19' => 'B5P'
 
     ];
 
