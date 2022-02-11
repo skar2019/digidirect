bfa24526235f8c7583104beb9b6faffef8d13728
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-02-11 12:44:47 +0800

    Change Breakpoint To Reposition Product Title

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -35,8 +35,9 @@ define([
         $('.studio19-wrapper').addClass('studio19-hide');
     }
 
-    //for mobile
-    let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+    //for mobile = 760
+    //Changed to 1439 for tablet *Rondel
+    let isMobile = window.matchMedia("only screen and (max-width: 1439px)").matches;
 
     if (isMobile) {
         $('.page-title-wrapper').insertBefore($('.media-area'));
