c1a6f754c2d6ef671fc51ed512dfe2732ebcb036
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-02-13 10:43:03 +0800

    FIx Responsive Title Header On Product Page

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -40,10 +40,19 @@ define([
     //Changed to 1439 for tablet *Rondel
     let isMobile = window.matchMedia("only screen and (max-width: 1439px)").matches;
 
-    if (isMobile) {
-        $('.page-title-wrapper').insertBefore($('.product-basic'));
-    }else {
-        $('.page-title-wrapper').insertBefore($('.product-info-price'));
-    }
+//    if (isMobile) {
+//        $('.page-title-wrapper').insertBefore($('.product-basic'));
+//    }else {
+//        $('.page-title-wrapper').insertBefore($('.product-info-price'));
+//    }
+    
+    $(window).on('resize', function(){
+        var win = $(this); //this = window
+        if (win.width() >= 1440) {
+            $('.page-title-wrapper').insertBefore($('.product-info-price'));
+        } else {
+            $('.page-title-wrapper').insertBefore($('.product-basic'));
+        } 
+    });
 
 });
