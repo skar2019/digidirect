a1792deb7a0a012653c19b73177ac85a39a833d0
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-02-17 14:18:56 +0800

    Test Fix Rating & SKU Reposition On Product Page Mobile View

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -55,8 +55,8 @@ define([
         } 
         
         if (win.width() <= 768) {
-            $('.product-reviews-summary').insertAfter($('.page-title-wrapper'));
-            $('.product-info-price>.product.attribute.sku').insertBefore($('.product-basic'));
+            $('.product-reviews-summary').insertAfter($('.page-title'));
+            $('.product-info-price>.product.attribute.sku').insertBefore($('.product-reviews-summary'));
         } else {
             //$('.product-info-main .product-reviews-summary').insertBefore($('.product-info-main .price-final_price'));
             //$('.product-info-main .product.attribute.sku').insertBefore($('.product-info-main .product-reviews-summary'));
