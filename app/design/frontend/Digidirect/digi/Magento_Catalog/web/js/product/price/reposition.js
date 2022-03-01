56b8a23d2ce8f48b9b5ac8a95233f8deeabe9310
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-01 19:24:42 +0800

    Product Page Image Preloader Fix

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -15,7 +15,7 @@ define([
 
         } 
         $('.custom-preloader').attr("style", "display:none !important;")
-        $('.gallery-placeholder__image').attr("style", "display:block !important;")
+        $('.fotorama-item').attr("style", "display:block !important;")
     };
     
     if( $('#main-product-qantas').length )
