cfbc8c6d2b5c7d40b3c1805a762e134d0320fac1
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-08 11:44:46 +0800

    Test Fix Category Page Rendering

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/list/toolbar-mixin.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/list/toolbar-mixin.js
@@ -27,4 +27,9 @@ define([
         });
         return $.mage.productListToolbarForm;
     };
+    
+    window.onload = function() {
+        $('.desktop-row').attr("style", "visibility: visible !important;");
+        $('.category-page-banner').attr("style", "visibility: visible !important;");
+    };
 });
