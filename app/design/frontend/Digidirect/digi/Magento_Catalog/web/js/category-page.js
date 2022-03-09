b7d58ba27216430d70c81c23efe6d196d4f324da
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-09 10:24:28 +0800

    Test Fix Category Page Rendering

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
@@ -7,4 +7,9 @@ define([
         $('.category-page-banner').attr("style", "display: block !important; margin-left: -55px; width: 1095px;");
         $('.custom-preloader-container').attr("style", "display: none !important;");
     };
+    
+    $(".sidebar-main").ready(function(){
+        console.log("Sidebar Ready!");   
+        $(".custom-preloader-container").css("margin-left","-55px");
+    });
 });
