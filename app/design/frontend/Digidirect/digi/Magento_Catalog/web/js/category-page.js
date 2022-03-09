bf37d8193512f33704862375d2307ea19788ad35
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-09 13:38:46 +0800

    Test Fix Category Page Rendering

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
@@ -2,24 +2,21 @@
 define([
     'jquery'
 ], function ($) {
+    
     window.onload = function() {
-        
+        console.log("On Load!");   
+        $('.desktop-row').attr("style", "visibility: visible !important;");
+        $('.category-page-banner').attr("style", "display: block !important; margin-left: -55px; width: 1095px;");
+        $('.custom-preloader-container').attr("style", "display: none !important;");
+        $('.sidebar-main').attr("style", "display: block !important;");
     };
     
 //    $("#catalogSidebar").ready(function(){
 //        console.log("Sidebar Ready!");   
-//        //$(".custom-preloader-container").css("margin-left","-55px");
 //        $('.desktop-row').attr("style", "visibility: visible !important;");
 //        $('.category-page-banner').attr("style", "display: block !important; margin-left: -55px; width: 1095px;");
 //        $('.custom-preloader-container').attr("style", "display: none !important;");
 //    });
-//    
-    if( $("#catalogSidebar").length ){
-        console.log("Sidebar Exist!");   
-        //$(".custom-preloader-container").css("margin-left","-55px");
-        $('.desktop-row').attr("style", "visibility: visible !important;");
-        $('.category-page-banner').attr("style", "display: block !important; margin-left: -55px; width: 1095px;");
-        $('.custom-preloader-container').attr("style", "display: none !important;");
-    }
+    
     
 });
