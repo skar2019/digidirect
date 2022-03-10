49e05349f8fd6296e05fbbbee380ec81847ce535
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-10 08:02:19 +0800

    Hide Category Page Custom Preloader On Mobile View

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
@@ -5,17 +5,9 @@ define([
     
     window.onload = function() {   
         $('.desktop-row').attr("style", "visibility: visible !important;");
-        $('.category-page-banner').attr("style", "display: block !important; margin-left: -55px; width: 1095px;").fadeIn();
+        $('.category-page-banner').attr("style", "display: block; margin-left: -55px; width: 1095px;").fadeIn();
         $('.custom-preloader-container').attr("style", "display: none !important;");
         $('.sidebar-main').attr("style", "display: block !important;").fadeIn();
     };
-    
-//    $("#catalogSidebar").ready(function(){
-//        console.log("Sidebar Ready!");   
-//        $('.desktop-row').attr("style", "visibility: visible !important;");
-//        $('.category-page-banner').attr("style", "display: block !important; margin-left: -55px; width: 1095px;");
-//        $('.custom-preloader-container').attr("style", "display: none !important;");
-//    });
-    
     
 });
