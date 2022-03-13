67eaaf77c897b000663c10204f6e3e475428c2a6
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-13 20:59:48 +0800

    Test Fix Product Page Caching Issue

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -17,7 +17,7 @@ define([
         //$('.gallery-placeholder').attr("style", "visibility: visible !important;");
     });
     
-    $('.fotorama__nav').ready(function() {
+    $('.fotorama-item').ready(function() {
         $('.gallery-placeholder').attr("style", "visibility: visible !important;");
     });
     
