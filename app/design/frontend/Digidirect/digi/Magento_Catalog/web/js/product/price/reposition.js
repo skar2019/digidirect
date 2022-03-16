19b529300834f0ef3d0c582dba25618796c1b48b
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-16 23:02:36 +0800

    Fix Product Page Rating & SKU Position

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -5,19 +5,10 @@ define([
 ], function ($) {
     //clint changes cashback
     //Redeploy
-    $('.braintree-paypal-logo').ready(function() {
+    $('.secure-pay-container').ready(function() {
         $(".secure-pay-container").attr("style", "display: none");
-        if ($(window).width() <= 768) {
-            $('.testfreaks-badge').insertAfter($('.page-title'));
-            $('.product-info-price>.product.attribute.sku').insertAfter($('.testfreaks-badge'));
-            console.log("Test OnLoad : Window width is " + $(window).width());
-        } else {
-
-        } 
-        
         $('#product-options-wrapper').insertAfter($('.box-tocart'));
         $('.studio19-wrapper').insertAfter($('#product-options-wrapper'));
-        
     });
     
     $('.fotorama-item').ready(function() {
@@ -26,7 +17,13 @@ define([
     });
     
     $(window).load(function(){
-        
+        if ($(window).width() <= 768) {
+            $('.testfreaks-badge').insertAfter($('.page-title'));
+            $('.product-info-price>.product.attribute.sku').insertAfter($('.testfreaks-badge'));
+            console.log("Test OnLoad : Window width is " + $(window).width());
+        } else {
+
+        } 
     });
     
     if( $('#main-product-qantas').length )
