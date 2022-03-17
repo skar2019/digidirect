c24971cca886137953a05d0d8e80dcdee6e7bdce
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-17 10:47:59 +0800

    Test Fix Category Page Banner

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
@@ -10,7 +10,6 @@ define([
     
     $('.products-grid').ready(function() {
         $('.desktop-row').attr("style", "visibility: visible !important;");
-        $('.category-page-banner').attr("style", "display: block; margin-left: -55px; width: 1095px;").fadeIn();
         $('.custom-preloader-container').attr("style", "display: none !important;");
         $('.sidebar-main').attr("style", "display: block !important;").fadeIn();
     });
