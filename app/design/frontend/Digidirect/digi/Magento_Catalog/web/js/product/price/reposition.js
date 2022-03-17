a4d283e9de536d5904a971410ba9fe8619a6ade6
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-17 12:37:35 +0800

    Test Fix Product Page Rendering Issue

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -11,7 +11,7 @@ define([
         $('.studio19-wrapper').insertAfter($('#product-options-wrapper'));
     });
     
-    $('.fotorama-item').load(function() {
+    $('.fotorama__img').ready(function() {
         $('.custom-preloader').attr("style", "display:none !important;");
         $('.gallery-placeholder').attr("style", "visibility: visible !important;");
     });
