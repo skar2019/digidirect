b52d28bdd60ce4b9edab5ca66c5d851da8afa236
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-19 23:28:24 +0800

    Test Fix Category Page Preloader

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
@@ -5,13 +5,6 @@ define([
 ], function ($) {
     
     //$('.columns').append('<div class="custom-preloader-container""><div class="custom-preloader"></div></div>');
-    
-    $(document).ready(function() {
-        setTimeout(function() {
-            $('.custom-preloader-container').attr("style", "display: block !important;");
-        }, 1500);
-    });
-    
     window.onload = function() {   
         
     };
