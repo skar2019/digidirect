f85ce67d5a7ceb181f7a1eb5ce93431850eeb3b0
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-19 21:59:02 +0800

    Test Fix Category Page Preloader

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
@@ -4,8 +4,7 @@ define([
     'jquery'
 ], function ($) {
     
-    $('#catalogSidebar').append('<div class="custom-preloader-container" style="margin-top: 70px;margin-left: 25px;">\n\
-    <div class="custom-preloader" style="display: block;margin: auto;vertical-align: middle;border: 7px solid #f5f5f5;border-radius: 50%;border-top: 7px solid #000;width: 50px;height: 50px;-webkit-animation: spin 2s linear infinite; /* Safari */animation: spin 2s linear infinite;"></div></div>');
+    $('.columns').append('<div class="custom-preloader-container""><div class="custom-preloader"></div></div>');
     
     window.onload = function() {   
         
@@ -14,7 +13,7 @@ define([
     $('.products-grid').ready(function() {
         $('.desktop-row').attr("style", "visibility: visible !important;");
         $('.block-content.filter-content').attr("style", "display: block !important;").fadeIn();
-        //$('.custom-preloader-container').attr("style", "display: none !important;");
+        $('.custom-preloader-container').attr("style", "display: none !important;");
     });
     
 });
