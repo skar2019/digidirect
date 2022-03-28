4fbf04ea3952a46148837029d33ae7adf60b3e81
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-03-28 14:52:55 +0800

    Create Plugin For Item Subtotal In Minicart

--- app/code/Digidirect/Checkout/Plugin/DefaultItemPlugin.php
+++ app/code/Digidirect/Checkout/Plugin/DefaultItemPlugin.php
@@ -8,9 +8,11 @@ class DefaultItemPlugin
     public function afterGetItemData(
         \Magento\Checkout\CustomerData\AbstractItem $subject,
         $result,
-        \Magento\Quote\Model\Quote\Item $item)
+        \Magento\Quote\Model\Quote\Item $item,
+        \Magento\Checkout\Helper\Data $checkoutHelper
+            )
     {
-        $data['item_subtotal'] = $item->getQty() * 1;
+        $data['item_subtotal'] = ($item->getQty() * 1) * $checkoutHelper->formatPrice($item->getCalculationPrice());
 
         return \array_merge(
             $result,
