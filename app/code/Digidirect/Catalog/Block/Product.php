e463112ef4a3f2ccd6da125487a0301d74a1d92a
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-04-19 15:12:21 +0800

    Test Fix On Catalog/Block Product File

--- app/code/Digidirect/Catalog/Block/Product.php
+++ app/code/Digidirect/Catalog/Block/Product.php
@@ -4,17 +4,14 @@ namespace Digidirect\Catalog\Block;
 class Product extends \Magento\Framework\View\Element\Template
 {
     protected $_registry;
-    protected $_productRepository;
         
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,        
         \Magento\Framework\Registry $registry,
-        \Magento\Catalog\Model\ProductRepository $productRepository,
         array $data = []
     )
     {        
         $this->_registry = $registry;
-        $this->_productRepository = $productRepository;
         parent::__construct($context, $data);
     }
     
@@ -31,10 +28,5 @@ class Product extends \Magento\Framework\View\Element\Template
     public function getCurrentProduct()
     {        
         return $this->_registry->registry('current_product');
-    }    
-    
-    public function getProductBySku($sku) {
-        return $this->_productRepository->get($sku);
-    }
-    
+    }   
 }
