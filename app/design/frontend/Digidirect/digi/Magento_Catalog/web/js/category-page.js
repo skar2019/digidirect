52c8eec62cf1c97328cec679eff919bb12e9a907
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-05-03 15:07:38 +0800

    digiSeconds PDP Changes

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/category-page.js
@@ -18,4 +18,12 @@ define([
         //$('.custom-preloader-container').attr("style", "display: none !important;");
     });
     
+    $(".product-category-listing .product-items").bind("DOMSubtreeModified", function() {
+        $('.desktop-row').attr("style", "visibility: visible !important;");
+        setTimeout(function() {
+            $('.block-content.filter-content').attr("style", "display: block !important;").fadeIn();
+            console.log('Timeout Executed!');
+        }, 3000);
+    });
+    
 });
