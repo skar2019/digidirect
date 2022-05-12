c2a9d73811685aa73bfcbc66b60aa10a569762d1
Clint <clint@kayweb.com.au>
2022-05-12 19:51:52 +0800

    local themes

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,16 @@
+module.exports = {
+   digi: {
+       area: 'frontend',
+       name: 'Digidirect/digi',
+       locale: 'en_AU',
+       files: [
+           'css/styles-m',
+           'css/styles-l',
+           'css/print',
+           'mage/gallery/gallery',
+           'css/email-inline',
+           'css/email'
+       ],
+       dsl: 'less'
+   }
+};
