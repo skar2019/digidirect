b0e5144c0fe8dafba9012c1628f49096f6451a99
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-05-13 14:21:59 +0800

    Test Fix Minicart DefaultItem.php

--- app/code/Digidirect/Qantas/Overrides/CustomerData/DefaultItem.php
+++ app/code/Digidirect/Qantas/Overrides/CustomerData/DefaultItem.php
@@ -98,7 +98,7 @@ class DefaultItem extends \Magento\Checkout\CustomerData\DefaultItem {
                 'height' => $imageHelper->getHeight(),
             ],
             'canApplyMsrp' => $this->msrpHelper->isShowBeforeOrderConfirm($this->item->getProduct()) && $this->msrpHelper->isMinimalPriceLessMsrp($this->item->getProduct()),
-            'qantas_points' => $this->getQffPoints(),
+            'qantas_points' => 100,
         ];
     }
 
