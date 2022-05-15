1b40225bcbae125395b9864c743582df6cd02ffd
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-05-15 16:18:28 +0800

    Test Fix Minicart default.html

--- app/code/Digidirect/Checkout/Plugin/DefaultItemPlugin.php
+++ app/code/Digidirect/Checkout/Plugin/DefaultItemPlugin.php
@@ -12,7 +12,7 @@ class DefaultItemPlugin
             )
     {
         $data['item_subtotal'] = '$'.number_format((float)(($item->getQty() * 1) * ($item->getCalculationPrice())), 2, '.', '');
-        $data['qantas_points_new'] = number_format((float)(($item->getQty() * 1) * ($item->getCalculationPrice())), 2, '.', '');
+        $data['qantas_points_new'] = $this->getQffPoints();
         
         return \array_merge(
             $result,
@@ -22,28 +22,28 @@ class DefaultItemPlugin
     
     public function getQffPoints() {
 
-        $qty = $this->item->getQty() * 1;
+        $qty = $item->getQty() * 1;
 
-        if ($this->item->getPrice() == 0) {
+        if ($item->getPrice() == 0) {
 
             $promotionPoints = 0;
         } else {
 
-            $finalProductPrice = $this->item->getProduct()->getFinalPrice();
+            $finalProductPrice = $item->getProduct()->getFinalPrice();
 
-            if ($this->item->getProduct()->offsetExists("qff_bonus_points") && $this->item->getProduct()->offsetExists("qff_base")) {
+            if ($item->getProduct()->offsetExists("qff_bonus_points") && $item->getProduct()->offsetExists("qff_base")) {
 
-                $qff_bonus_points = $this->item->getProduct()->getQffBonusPoints();
+                $qff_bonus_points = $item->getProduct()->getQffBonusPoints();
 
-                $qff_base_points = $this->item->getProduct()->getQffBase();
+                $qff_base_points = $item->getProduct()->getQffBase();
 
                 $pointsSum = $qff_bonus_points + $qff_base_points;
 
                 $promotionPoints = number_format($pointsSum * ($qty * $finalProductPrice));
             } else {
-                if ($this->item->getProduct()->offsetExists("qff_base")) {
+                if ($item->getProduct()->offsetExists("qff_base")) {
 
-                    $qff_base_points = $this->item->getProduct()->getQffBase();
+                    $qff_base_points = $item->getProduct()->getQffBase();
 
                     $promotionPoints = number_format($qff_base_points * ($qty * $finalProductPrice));
                 } else {
