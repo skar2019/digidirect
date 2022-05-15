26355ea90b98a87a033e87cd4459ff133734caf3
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-05-15 12:00:19 +0800

    Revert DefaultItem.php

--- app/code/Digidirect/Qantas/Overrides/CustomerData/DefaultItem.php
+++ app/code/Digidirect/Qantas/Overrides/CustomerData/DefaultItem.php
@@ -87,7 +87,6 @@ class DefaultItem extends \Magento\Checkout\CustomerData\DefaultItem {
             'product_id' => $this->item->getProduct()->getId(),
             'product_name' => $productName,
             'product_sku' => $this->item->getProduct()->getSku(),
-            'qantas_points' => $this->getQffPoints(),
             'product_url' => $this->getProductUrl(),
             'product_has_url' => $this->hasProductUrl(),
             'product_price' => $this->checkoutHelper->formatPrice($this->item->getCalculationPrice()),
@@ -98,7 +97,8 @@ class DefaultItem extends \Magento\Checkout\CustomerData\DefaultItem {
                 'width' => $imageHelper->getWidth(),
                 'height' => $imageHelper->getHeight(),
             ],
-            'canApplyMsrp' => $this->msrpHelper->isShowBeforeOrderConfirm($this->item->getProduct()) && $this->msrpHelper->isMinimalPriceLessMsrp($this->item->getProduct()),            
+            'canApplyMsrp' => $this->msrpHelper->isShowBeforeOrderConfirm($this->item->getProduct()) && $this->msrpHelper->isMinimalPriceLessMsrp($this->item->getProduct()), 
+            'qantas_points' => $this->getQffPoints(),           
         ];
     }
 
