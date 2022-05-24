6322ccf6ea9655ab1355f70a17734135731ae30f
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-05-24 14:57:58 +0800

    Test Fix Checkout Pickup Store requirejs-config.js

---
+++ app/design/frontend/Digidirect/digi/Digidirect_Locator/web/requirejs-config.js
@@ -0,0 +1,10 @@
+/* eslint no-unused-vars: [1] */
+var config = {
+    config: {
+        mixins: {
+            'Digidirect_Collect/js/view/block': {
+                'Digidirect_CollectStoreLocator/js/view/block-mixin': true
+            }
+        }
+    }
+};
