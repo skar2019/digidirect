9c748960c7bfce9c1fa11ac6c6bdfc42f0aae6fa
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-05-25 12:05:50 +0800

    Test Fix Checkout Store Pickup Digidirect_CollectStoreLocator

--- app/design/frontend/Digidirect/digi/Digidirect_CollectStoreLocator/web/js/view/block-mixin.js
+++ app/design/frontend/Digidirect/digi/Digidirect_CollectStoreLocator/web/js/view/block-mixin.js
@@ -45,6 +45,9 @@ define([
             submitLocatorStore: function (item) {
                 this.applyCollectPlaceToAllItems(item.entity_id, this.entityName);
             }
+            testLocatorFunction: function (item) {
+                console.log("testLocatorFunction Called Successfully!");
+            }
         });
     };
 });
