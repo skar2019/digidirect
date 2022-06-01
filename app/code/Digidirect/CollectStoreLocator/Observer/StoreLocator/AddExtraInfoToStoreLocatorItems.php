c9d2a9705ccff13f9cc9c42f7e883b7642f9da00
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-01 12:19:51 +0800

    Test Fic Clicl & Collect On Checkout

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -76,7 +76,7 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
             
             $place = $places[$id];
             if ($place->hasData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE)) {
-                $items[$key]['available'] = true; // Andrew requested that all store is selectable; !$place->getData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE);
+                $items[$key]['available'] = false; // Andrew requested that all store is selectable; !$place->getData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE);
             }
         }
         return $items;
