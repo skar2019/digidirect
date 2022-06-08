be5591f9122f868a5593698a77b8e2c59d60e46b
Clint <clint@kayweb.com.au>
2022-06-08 19:00:23 +0800

    itoris price match

--- app/etc/config.php
+++ app/etc/config.php
@@ -652,7 +652,9 @@ return [
         'Temando_ShippingRemover' => 1,
         'WebPanda_SalesProductImage' => 1,
         'Zendesk_Zendesk' => 1,
-        'Zip_ZipPayment' => 1
+        'Zip_ZipPayment' => 1,
+        'Itoris_Core' => 1,
+        'Itoris_PriceMatch' => 1
     ],
     'admin_user' => [
         'locale' => [
