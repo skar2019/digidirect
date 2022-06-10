cce9dc47f993d40d7eb295d76fe4b71f5751133d
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-10 20:57:05 +0800

    Modify ninjamenus.js

--- app/code/Magezon/NinjaMenus/view/frontend/web/js/ninjamenus.js
+++ app/code/Magezon/NinjaMenus/view/frontend/web/js/ninjamenus.js
@@ -254,6 +254,7 @@ define([
                 $(this).parent().parent().toggleClass('ninjamenus-hamburger-active');
             });
             self._initAddCustomClass();
+            self._initThirdLayerOpener();
         },
 
         onMouseHoverIntent: function(event) {
@@ -572,6 +573,15 @@ define([
                 //
                 // ..ninjamenus
             }
+        }, 
+        
+        _initThirdLayerOpener: function () {
+            $(".tablinks").click(function(){
+                console.log("Tablinks OnClick Working!");
+                $(".shop-by-category-menu .item-submenu.expand").attr("style", "width: 850px !important;");
+                $(".menu-second-level .tab").attr("style", "width: 50% !important;");
+            });
+            
         }
     });
 
