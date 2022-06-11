86cea86878e5cf72106d330ffa7c1669334302af
Clint <clint@kayweb.com.au>
2022-06-11 11:50:40 +0800

    disable csp

--- app/etc/config.php
+++ app/etc/config.php
@@ -203,7 +203,7 @@ return [
         'Magento_Contact' => 1,
         'Magento_Cookie' => 1,
         'Magento_Cron' => 1,
-        'Magento_Csp' => 1,
+        'Magento_Csp' => 0,
         'Magento_CurrencySymbol' => 1,
         'Magento_CustomAttributeManagement' => 1,
         'Magento_AdvancedCheckout' => 1,
