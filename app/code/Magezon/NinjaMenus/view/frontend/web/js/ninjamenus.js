a6e37c957e965a30bdb531f935f1bb33bbff8c0c
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-13 22:42:53 +0800

    Modify ninjamenus.js

--- app/code/Magezon/NinjaMenus/view/frontend/web/js/ninjamenus.js
+++ app/code/Magezon/NinjaMenus/view/frontend/web/js/ninjamenus.js
@@ -585,18 +585,18 @@ define([
                     $(".shop-by-category-menu .item-submenu.expand").attr("style", "width: 850px !important;");
                 }
             
-                $(".shop-by-category-menu").focusout(function(){
-                    $("#shop-by-category-menu-id .item-submenu").removeAttr("style");
-                });
+//                $(".shop-by-category-menu").focusout(function(){
+//                    $("#shop-by-category-menu-id .item-submenu").removeAttr("style");
+//                });
             
             });
             
-            if (windowsize < 768) {
-                console.log("Screen Width: " + windowsize);
-                $("#shop-by-category-menu-id").trigger('click');
-            }
-
-            
+            $(window).load(function(){
+                if ($(window).width() <= 768) {
+                    console.log("Screen Width: " + windowsize);
+                    $("#shop-by-category-menu-id").trigger('click');
+                }
+            });
         }
     });
 
