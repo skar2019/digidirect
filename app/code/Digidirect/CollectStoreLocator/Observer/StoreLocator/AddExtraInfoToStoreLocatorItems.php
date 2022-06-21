b5ef896bcce94edcb496f6e3f6b36107932fc754
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-21 10:20:23 +0800

    Test Console Log On AddExtraInfoToStoreLocatorItems.php

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -56,6 +56,7 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
 
         $locatorStores = $this->addAvailabilityInfoToItems($locatorStores);
         $transportObject->setData(['items' => $locatorStores]);
+        echo $this->console_log($locatorStores);
     }
 
     /**
@@ -80,5 +81,14 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
             }
         }
         return $items;
+    }
+    
+    function console_log($output, $with_script_tags = true) {
+        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+    ');';
+        if ($with_script_tags) {
+            $js_code = '<script>' . $js_code . '</script>';
+        }
+        echo $js_code;
     }
 }
