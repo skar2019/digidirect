ce604656acb709bd749d17c3d32e54b08711d3e6
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-21 09:22:23 +0800

    Test Fix On AddExtraInfoToStoreLocatorItems.php

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -75,10 +75,8 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
             }
 
             $place = $places[$id];
-            if (!$place->getData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE)) {
-                $items[$key]['available'] = true; // Andrew requested that all store is selectable; !$place->getData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE);
-            }else{
-                $items[$key]['available'] = false;
+            if ($place->hasData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE)) {
+                //$items[$key]['available'] = true; // Andrew requested that all store is selectable; !$place->getData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE);
             }
         }
         return $items;
