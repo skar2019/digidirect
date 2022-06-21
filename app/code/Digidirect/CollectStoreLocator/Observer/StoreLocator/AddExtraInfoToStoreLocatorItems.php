c708098a93f4ad193d06d483e28e63ea301d87f5
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-21 12:46:58 +0800

    Test Console Log On AddExtraInfoToStoreLocatorItems.php

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -83,8 +83,8 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
     }
     
     function console_log($output, $with_script_tags = true) {
-        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-    ');';
+        $js_code = 'console.log(JSON.stringify(' . json_encode($output, JSON_HEX_TAG) . 
+    '));';
         if ($with_script_tags) {
             $js_code = '<script>' . $js_code . '</script>';
         }
