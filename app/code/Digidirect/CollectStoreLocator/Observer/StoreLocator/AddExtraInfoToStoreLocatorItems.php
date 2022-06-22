bcdfeb59bc08dfc89e4b89ece2a3aba0d90f3355
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-22 21:22:41 +0800

    Fix Click & Collect On Checkout

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -7,6 +7,7 @@ use Magento\Checkout\Model\Session;
 use Digidirect\CollectAbstractEntity\Helper\Places;
 use Digidirect\Collect\Helper\Data;
 use Digidirect\Collect\Api\CollectPlaceRepositoryInterface;
+use Magento\Inventory\Model\SourceItem\Command\GetSourceItemsBySku;
 
 /**
  * Class AddExtraInfoToStoreLocatorItems
@@ -38,11 +39,13 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
     public function __construct(
         Session $checkoutSession,
         Places $placesHelper,
-        Data $collectHelper
+        Data $collectHelper,
+        GetSourceItemsBySku $getSourceItemsBySku
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->placesHelper = $placesHelper;
         $this->collectHelper = $collectHelper;
+        $this->getSourceItemsBySku = $getSourceItemsBySku;
     }
 
     /**
@@ -53,9 +56,9 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
     {
         $transportObject = $observer->getTransportObject();
         $locatorStores = $transportObject->getData('items');
-
         $locatorStores = $this->addAvailabilityInfoToItems($locatorStores);
         $transportObject->setData(['items' => $locatorStores]);
+        echo $this->console_log($locatorStores);
     }
 
     /**
@@ -67,18 +70,86 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
         $quoteItems = $this->checkoutSession->getQuote()->getAllVisibleItems();
         $skuQty = $this->collectHelper->getSkuToQtyByItems($quoteItems);
         $places = $this->placesHelper->getAllCollectPlacesEntities($skuQty);
+        
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $cart = $objectManager->get('\Magento\Checkout\Model\Cart');
+        $cartItems = $cart->getQuote()->getAllItems();
 
         foreach ($items as $key => $storeData) {
+            
             $id = $storeData['entity_id'];
+            $qty = 0;
+            
             if (empty($places[$id])) {
                 continue;
             }
-            
+
             $place = $places[$id];
             if ($place->hasData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE)) {
                 $items[$key]['available'] = true; // Andrew requested that all store is selectable; !$place->getData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE);
             }
+            
+            $sydnQty = 1;
+            $bondQty = 1;
+            $melbQty = 1;
+            $brisQty = 1;
+            $miraQty = 1;
+            $cannQty = 1;
+            
+            foreach ($cartItems as $cartItem) {
+            
+                $prodId = $cartItem->getProductId();
+                $product = $objectManager->get('\Magento\Catalog\Model\Product')->load($prodId);
+
+                $sourceItems = $this->getSourceItemsBySku->execute($product->getSku());
+                
+                foreach ($sourceItems as $sourceItemId => $sourceItem) {
+                    //echo $this->console_log($sourceItem->getQuantity());
+                    //echo $this->console_log($sourceItem->getSourceCode());
+                    //$qty .= $sourceItem->getQuantity();
+                    
+                    if ($id == 1 && $sourceItem->getSourceCode() == 'SYDN') {
+                        $sydnQty = $sydnQty * $sourceItem->getQuantity();
+                    } elseif ($id == 31 && $sourceItem->getSourceCode() == 'BOND') {
+                        $bondQty = $bondQty * $sourceItem->getQuantity();
+                    } elseif ($id == 7 && $sourceItem->getSourceCode() == 'MELB') {
+                        $melbQty = $melbQty * $sourceItem->getQuantity();
+                    } elseif ($id == 10 && $sourceItem->getSourceCode() == 'BRIS') {
+                        $brisQty = $brisQty * $sourceItem->getQuantity();
+                    } elseif ($id == 13 && $sourceItem->getSourceCode() == 'MIRA') {
+                        $miraQty = $miraQty * $sourceItem->getQuantity();
+                    } elseif ($id == 16 && $sourceItem->getSourceCode() == 'CANN') {
+                        $cannQty = $cannQty * $sourceItem->getQuantity();
+                    }
+                }
+            }
+                    
+            if ($sydnQty > 1) {
+                $items[$key]['available'] = true;
+            } elseif ($bondQty > 1) {
+                $items[$key]['available'] = true;
+            } elseif ($melbQty > 1) {
+                $items[$key]['available'] = true;
+            } elseif ($brisQty > 1) {
+                $items[$key]['available'] = true;
+            } elseif ($miraQty > 1) {
+                $items[$key]['available'] = true;
+            } elseif ($cannQty > 1) {
+                $items[$key]['available'] = true;
+            } else {
+                $items[$key]['available'] = false;
+            }
+            
         }
         return $items;
+    }
+    
+    function console_log($output, $with_script_tags = true) {
+        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+    ');';
+        if ($with_script_tags) {
+            $js_code = '<script>' . $js_code . '</script>';
+        }
+        echo $js_code;
     }
 }
