ae2d70f4e6626bcd722bd89f54d9f7069701db09
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-26 21:09:54 +0800

    Reposition Itoris Pricematch Via XML

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -41,7 +41,6 @@ define([
         if( $('.itoris-pm-product-marker').length )
         {
             $('.itoris-pm-product-marker').insertAfter($('.product-info-main-bottom'));
-            $(".itoris-pm-product-marker").attr("style", "display: block");
         }
 
     });
