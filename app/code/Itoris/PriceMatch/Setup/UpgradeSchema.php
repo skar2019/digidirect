4a1879cb179fb6ccabb2fca6b55eefdd416f8646
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-27 14:46:28 +0800

    Upgrade Itoris Pricematch Table

--- app/code/Itoris/PriceMatch/Setup/UpgradeSchema.php
+++ app/code/Itoris/PriceMatch/Setup/UpgradeSchema.php
@@ -35,6 +35,18 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
         $setup->startSetup();
         
         if (version_compare($context->getVersion(), '1.0.4') < 0) {
+            $connection = $setup->getConnection();
+            $connection->addColumn(
+                $setup->getTable('itoris_pricematch_data'),
+                'contact_number',
+                [
+                    'type' => Table::TYPE_TEXT,
+                    'length' => 255,
+                    'nullable' => true,
+                    'default' => '',
+                    'comment' => 'Contact Number'
+                ]
+            );
             $setup->run("
                 ALTER TABLE `{$setup->getTable('itoris_pricematch_data')}`
                     MODIFY `status` VARCHAR(255),
