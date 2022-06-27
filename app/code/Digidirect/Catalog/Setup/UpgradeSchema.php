078b69df194ce8396535c2d96fcbbd6e881cb073
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-27 14:21:31 +0800

    Create Custom UpgradeSchema.php

---
+++ app/code/Digidirect/Catalog/Setup/UpgradeSchema.php
@@ -0,0 +1,27 @@
+<?php
+namespace Digidirect\Catalog\Setup;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+ 
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if (version_compare($context->getVersion(), '1.0.4') < 0) {
+            $connection = $setup->getConnection();
+            $connection->addColumn(
+                $setup->getTable('itoris_pricematch_data'),
+                'contact_number',
+                [
+                    'type' => Table::TYPE_TEXT,
+                    'length' => 255,
+                    'nullable' => true,
+                    'default' => '',
+                    'comment' => 'Contact Number'
+                ]
+            );
+        }
+    }
+}
