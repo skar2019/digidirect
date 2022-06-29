2807d588da8cd2789d38cb456a1334f25789b9a2
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-06-29 14:56:06 +0800

    Test Fix Admin Email Pricematch

--- app/code/Itoris/PriceMatch/Model/SenderAdmin.php
+++ app/code/Itoris/PriceMatch/Model/SenderAdmin.php
@@ -29,6 +29,9 @@ class SenderAdmin extends SenderAbstract
 {
     protected function getFommatVar($item, $method)
     {
+        $rawCommentData = $item['comment'];
+        $splitCommentData = explode("#contact_number:", $rawCommentData);
+        
         return [
             'send_vars' => [
                 'product_name' => $item['product_name'],
@@ -37,11 +40,12 @@ class SenderAdmin extends SenderAbstract
                 'storeview' => $this->storeFactory->create()->load($item['store_id'])->getName(),
                 'customer_name' => $item['name'], //$this->helperData->genEmailAdmin($item['store_id']),
                 'date_time' => $item['date_created'],
+                'contact' => $splitCommentData[0],
                 'email' => $item['customer_email'],
                 'current_price' => $this->formatPrice($item['final_price'], $item['store_id']),
                 'requested_price' => $this->formatPrice($item['match_price'], $item['store_id']),
                 'url' => ($item['match_url']) ? $item['match_url'] : __('n/a'),
-                'comment' => $item['comment'],
+                'comment' => $splitCommentData[0],
             ],
             'email' => $this->helperData->genEmailAdmin($item['store_id']),
             'store_id' => $item['store_id'],
