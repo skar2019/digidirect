1df0d9a6d1f4b8df6af0369441c14915960bd141
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-07-04 22:20:14 +0800

    Test Fix On Click & Collect Bug

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -89,6 +89,14 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
                 $items[$key]['available'] = true; // Andrew requested that all store is selectable; !$place->getData(CollectPlaceRepositoryInterface::KEY_IS_UNAVAILABLE);
             }
             
+            $sydnQty = 0;
+            $bondQty = 0;
+            $melbQty = 0;
+            $brisQty = 0;
+            $miraQty = 0;
+            $cannQty = 0;
+            $parrQty = 0;
+            
             foreach ($cartItems as $cartItem) {
             
                 $prodId = $cartItem->getProductId();
@@ -97,15 +105,64 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
                 $sourceItems = $this->getSourceItemsBySku->execute($product->getSku());
                 
                 foreach ($sourceItems as $sourceItemId => $sourceItem) {
-                    $qty = $qty + $sourceItem->getQuantity();
+                    //echo $this->console_log($sourceItem->getQuantity());
+                    //echo $this->console_log($sourceItem->getSourceCode());
+                    //$qty .= $sourceItem->getQuantity();
+                    
+                    if ($id == 1 && $sourceItem->getSourceCode() == 'SYDN') {
+                        $sydnQty = $sourceItem->getQuantity();
+                    } elseif ($id == 31 && $sourceItem->getSourceCode() == 'BOND') {
+                        $bondQty = $sourceItem->getQuantity();
+                    } elseif ($id == 7 && $sourceItem->getSourceCode() == 'MELB') {
+                        $melbQty = $sourceItem->getQuantity();
+                    } elseif ($id == 10 && $sourceItem->getSourceCode() == 'BRIS') {
+                        $brisQty = $sourceItem->getQuantity();
+                    } elseif ($id == 13 && $sourceItem->getSourceCode() == 'MIRA') {
+                        $miraQty = $sourceItem->getQuantity();
+                    } elseif ($id == 16 && $sourceItem->getSourceCode() == 'CANN') {
+                        $cannQty = $sourceItem->getQuantity();
+                    } elseif ($id == 32 && $sourceItem->getSourceCode() == 'PARR') {
+                        $parrQty = $sourceItem->getQuantity();
+                    }
                 }
             }
                     
-            if ($qty > 0) {
+            if ($sydnQty > 0) {
                 $items[$key]['available'] = true;
             } else {
                 $items[$key]['available'] = false;
             }
+            if ($bondQty > 0) {
+                $items[$key]['available'] = true;
+            } else {
+                $items[$key]['available'] = false;
+            }
+            if ($melbQty > 0) {
+                $items[$key]['available'] = true;
+            } else {
+                $items[$key]['available'] = false;
+            }
+            if ($brisQty > 0) {
+                $items[$key]['available'] = true;
+            } else {
+                $items[$key]['available'] = false;
+            }
+            if ($miraQty > 0) {
+                $items[$key]['available'] = true;
+            } else {
+                $items[$key]['available'] = false;
+            }
+            if ($cannQty > 0) {
+                $items[$key]['available'] = true;
+            } else {
+                $items[$key]['available'] = false;
+            }
+            if ($parrQty > 0) {
+                $items[$key]['available'] = true;
+            } else {
+                $items[$key]['available'] = false;
+            }
+            
             
         }
         return $items;
