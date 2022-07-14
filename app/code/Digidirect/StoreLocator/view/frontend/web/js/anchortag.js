80b80d8619d75dfbf95ef74af188229b64078bbf
Clint <clint@kayweb.com.au>
2022-07-14 13:40:00 +0800

    remove display blcok anchor tag store locator

--- app/code/Digidirect/StoreLocator/view/frontend/web/js/anchortag.js
+++ app/code/Digidirect/StoreLocator/view/frontend/web/js/anchortag.js
@@ -11,12 +11,12 @@ define(['jquery'], function($){
                 var hash = window.location.hash.substr(1);
                 if(hash === '')
                 {
-                    hash = 'Bondi.Junction';
+                    hash = 'Bondi Junction';
                 }
 
                 $(".scontent-"+hash).ready(function() {
                     $('#'+hash).attr('checked', true); //can be removed if it scrolls up the page
-                    $(".scontent-"+hash).css({ display: "block" });
+                    //$(".scontent-"+hash).css({ display: "block" });
                 });
 
             });
