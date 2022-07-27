99920f58daeaee212ccb8c03339fbbe2f4807a16
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-07-27 10:31:29 +0800

    Add New Key On Store Array

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -79,7 +79,7 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
             
             $id = $storeData['entity_id'];
             $qty = 0;
-            $items[$key]['click_and_collect'] = true;
+            //$items[$key]['click_and_collect'] = true;
             
             if (empty($places[$id])) {
                 continue;
