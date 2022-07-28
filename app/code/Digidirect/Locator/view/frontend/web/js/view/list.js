3d035d072f597618b737f6f4d880668148a5387a
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-07-28 14:59:30 +0800

    Test JS On Checkout

--- app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
@@ -30,6 +30,7 @@ define([
                 };
                 this.locationsList = locations.items;
             }
+            console.log("locations.items : " + json_encode(locations.items));
         },
         paginationObservable: function () {
             var self = this;
