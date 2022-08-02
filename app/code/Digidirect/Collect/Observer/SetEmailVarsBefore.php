6fc783706dd6e2ec51b2c9eae5e8e21478f19731
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-02 09:59:03 +0800

    Revert SetEmailVarsBefore.php

--- app/code/Digidirect/Collect/Observer/SetEmailVarsBefore.php
+++ app/code/Digidirect/Collect/Observer/SetEmailVarsBefore.php
@@ -3,131 +3,45 @@
 namespace Digidirect\Collect\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
-use Psr\Log\LoggerInterface;
-use Magento\Framework\App\ObjectManager;
-use Magento\Payment\Helper\Data as PaymentHelper;
-use Magento\Sales\Model\Order;
-use Magento\Sales\Model\Order\Email\Container\OrderIdentity;
-use Magento\Sales\Model\Order\Email\Container\Template;
-use Magento\Sales\Model\ResourceModel\Order as OrderResource;
-use Magento\Sales\Model\Order\Address\Renderer;
-use Magento\Framework\DataObject;
+use Digidirect\Collect\Helper\Data as CollectHelper;
 
-class SetEmailVarsBefore extends \Magento\Sales\Model\Order\Email\Sender implements ObserverInterface 
+class SetEmailVarsBefore implements ObserverInterface
 {
     /**
-     * @var LoggerInterface
-     */
-    protected $logger;
-    
-    /**
-     * @var Template
-     */
-    protected $templateContainer;
-
-    
-    /**
-     * @var PaymentHelper
-     */
-    protected $paymentHelper;
-
-    /**
-     * @var OrderResource
-     */
-    protected $orderResource;
-
-    /**
-     * Global configuration storage.
+     * CollectHelper
      *
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     * @var \Digidirect\Collect\Helper\Data
      */
-    protected $globalConfig;
+    protected $_collectHelper;
 
     /**
-     * @var Renderer
-     */
-    protected $addressRenderer;
-
-    /**
-     * Application Event Dispatcher
+     * SetEmailVarsBefore constructor.
      *
-     * @var ManagerInterface
-     */
-    protected $eventManager;
-
-    /**
-     * @param Template $templateContainer
-     * @param OrderIdentity $identityContainer
-     * @param Order\Email\SenderBuilderFactory $senderBuilderFactory
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param Renderer $addressRenderer
-     * @param PaymentHelper $paymentHelper
-     * @param OrderResource $orderResource
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig
-     * @param ManagerInterface $eventManager
+     * @param CollectHelper $collectHelper
      */
     public function __construct(
-        Template $templateContainer,
-        OrderIdentity $identityContainer,
-        \Magento\Sales\Model\Order\Email\SenderBuilderFactory $senderBuilderFactory,
-        \Psr\Log\LoggerInterface $logger,
-        Renderer $addressRenderer,
-        PaymentHelper $paymentHelper,
-        OrderResource $orderResource,
-        \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig
+        \Digidirect\Collect\Helper\Data $collectHelper
     ) {
-        parent::__construct($templateContainer, $identityContainer, $senderBuilderFactory, $logger, $addressRenderer);
-        $this->paymentHelper = $paymentHelper;
-        $this->orderResource = $orderResource;
-        $this->globalConfig = $globalConfig;
-        $this->addressRenderer = $addressRenderer;
-        $this->identityContainer = $identityContainer;
-        $this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
-        $this->templateContainer = $templateContainer;
+        $this->_collectHelper = $collectHelper;
     }
-    
 
+    /**
+     * Execute
+     *
+     * @param   \Magento\Framework\Event\Observer $observer
+     * @return  void
+     * @throws \Exception
+     */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $this->logger->debug('Observer '.__CLASS__.' called.');
-        $transportObject = $observer->getEvent()->getTransport();
-        
-        // $transportObject = $observer->getEvent()->getTransportObject();
-        $order = $transportObject->getOrder(); 
-        $this->logger->debug('Order Id: ' . $order->getId());
-        
-        // you can use get this var in your order email template
-        $transportObject->setData('test_var','Test Value'); // or $transportObject->getTestVar('Test Value');
-        
-        // get order's shipping method and set the email template based on that
-        $shippingMethod = $order->getShippingMethod();
-        $this->logger->debug('Order Shipping Method  ' . $shippingMethod);
-        
-        if ($this->templateContainer) {
-            
-            $this->templateContainer->setTemplateOptions($this->getTemplateOptions());
-
-            if ($order->getCustomerIsGuest()) {
-                $templateId = $this->identityContainer->getGuestTemplateId();
-                $customerName = $order->getBillingAddress()->getName();
-            } else {
-                //$templateId = $this->identityContainer->getTemplateId();
-                $templateId = 15;
-                $customerName = $order->getCustomerName();
+        if ($this->_collectHelper->isCollectEnable()) {
+            $transport = $observer->getTransport();
+            $order = $transport->getOrder();
+            $collectDescription = $this->_collectHelper->getOrderCollectDescription($order);
+            if (is_array($collectDescription)) {
+                $collectDescription = implode(',', $collectDescription);
             }
-            
-            if ($shippingMethod == 'freeshipping_freeshipping') {
-                $templateId = 10; // set your template id here based on the shipping method
-            }
-            
-            $this->templateContainer->setTemplateId($templateId);
-            
-            $orderEmailTemplateId = $this->templateContainer->getTemplateId();
-            
-            $this->logger->debug('set order email template id based on shipping method : ' . $orderEmailTemplateId);
+            $transport->addData(['collectDescription' => $collectDescription]);
         }
-        
-        
     }
-    
 }
