734cb9f58e85dcdb6b957ef58e232ddd56cda953
Clint <clint@kayweb.com.au>
2022-08-04 14:55:12 +0800

    Debug disable product.

--- app/code/Digidirect/Pronto/Controller/Index/Test.php
+++ app/code/Digidirect/Pronto/Controller/Index/Test.php
@@ -18,10 +18,23 @@ class Test extends \Magento\Framework\App\Action\Action
 	public function execute()
 	{
             $test = 0;
+            $disable = 0;
             
             if(isset($_GET["test"])){
                 $test = $_GET["test"];
             }
-            $this->helper->toEnableProducts($test);
+            if(isset($_GET["disable"])){
+                $test = $_GET["disable"];
+            }
+            
+            if($disable)
+            {
+                $this->helper->toDisableProducts($test);
+            }
+            else 
+            {
+                $this->helper->toEnableProducts($test);
+            }
+            
     }
 }
