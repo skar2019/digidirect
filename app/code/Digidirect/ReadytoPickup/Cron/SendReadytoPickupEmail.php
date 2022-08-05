1c0ab3aae0f08212473fb3a451ac1e52c9dc21a8
Clint <clint@kayweb.com.au>
2022-08-05 19:11:47 +0800

    Click and collect

---
+++ app/code/Digidirect/ReadytoPickup/Cron/SendReadytoPickupEmail.php
@@ -0,0 +1,33 @@
+<?php
+namespace Digidirect\ReadytoPickup\Cron;
+
+use Psr\Log\LoggerInterface;
+use Digidirect\ReadytoPickup\Helper\ReadytoPickup;
+
+class SendReadytoPickupEmail
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var Inventory
+     */
+    protected $helper;
+    
+    public function __construct(
+            LoggerInterface $logger,
+            ReadytoPickup $helper)
+    {
+        $this->logger = $logger;
+        $this->helper = $helper;
+    }
+
+    public function execute()
+    {
+        $this->helper->sendReadytoPickupEmail();
+
+    }
+    
+}
