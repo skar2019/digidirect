d5b8db37e484bc5f2803418ca11168d1b698b08b
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-10 00:25:58 +0800

    Test Fix Click & Collect

--- app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
@@ -32,15 +32,12 @@ define([
                 };
                 this.locationsList = locations.items;
                 console.log("locationsList : " + this.locationsList);
-                
-                this.locationsList.forEach(myFunction);
-                function myFunction(storeItem, index) {
-                    console.log("storeItem : " + storeItem);
-                }
             }
             
-            this.pickUpAvailable = $('.pickup-available .collectlocator-wrapper .locator-items .locator-item').length;
-            this.pickUpUnavailable = $('.pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length;
+            $('.store-section').ready(function(){
+                this.pickUpAvailable = $('.pickup-available .collectlocator-wrapper .locator-items .locator-item').length;
+                this.pickUpUnavailable = $('.pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length;
+            });
             console.log("testCount : " + this.testCount);
         },
         toggleStoreListDisplay: function () {
