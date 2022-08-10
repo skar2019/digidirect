871d1564d21c6f46974b209442abbae547c8163a
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-10 19:54:21 +0800

    Test Fix Click & Collect

--- app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
@@ -49,14 +49,14 @@ define([
             
             $(".store-locator-wrapper").ready(function() {
                 alert("store-locator-wrapper ready!");
-                if ($('.pickup-available .collectlocator-wrapper .locator-items .locator-item').length == 0) {
-                    $(".pickup-available").removeAttr("style");
+                //if ($('.pickup-available .collectlocator-wrapper .locator-items .locator-item').length == 0) {
+                    //$(".pickup-available").removeAttr("style");
                     $(".pickup-available").attr("style", "display:none !important;");
-                }
-                if ($('.pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length == 0) {
-                    $(".pickup-unavailable").removeAttr("style");
+                //}
+                //if ($('.pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length == 0) {
+                    //$(".pickup-unavailable").removeAttr("style");
                     $(".pickup-unavailable").attr("style", "display:none !important;");
-                }
+                //}
             });
             //this.pickUpAvailable = $('.pickup-available .collectlocator-wrapper .locator-items .locator-item').length;
             //this.pickUpUnavailable = $('.pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length;
