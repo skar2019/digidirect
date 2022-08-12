13e4ba218cbb5acdb69682d4d309694e2d29989c
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-12 15:04:00 +0800

    Test Fix Click & Collect

--- app/code/Digidirect/Locator/view/frontend/web/js/action/set-locations.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/action/set-locations.js
@@ -8,5 +8,7 @@ define([
         locations.settings(settings);
         locations.pager([]);
         locations.currentPage(1);
+        
+        alert('set-locations.js : ' + $('#pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length);
     };
 });
