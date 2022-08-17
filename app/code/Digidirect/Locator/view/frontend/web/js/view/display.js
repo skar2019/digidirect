35f7c9485611ceb3548048904d9c92c6a71cc379
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-17 14:01:37 +0800

    Test Fix Click & Collect

--- app/code/Digidirect/Locator/view/frontend/web/js/view/display.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/view/display.js
@@ -12,16 +12,21 @@ define([
         },
         toggleBlockDisplay: function () {
             //alert('New JS File Works!');
-            //if ($('#pickup-available .collectlocator-wrapper .locator-items .locator-item').length != 0) {
-                alert("#pickup-available length is " + $('#pickup-available .collectlocator-wrapper .locator-items .locator-item').length);
-                //$("#pickup-available").removeAttr("style");
-                //$("#pickup-available").attr("style", "display:none !important;");
-            //}
-            //if ($('#pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length != 0) {
-                alert("#pickup-unavailable length is " + $('#pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length);
-                //$("#pickup-available").removeAttr("style");
-                //$("#pickup-available").attr("style", "display:none !important;");
-            //}
+            
+            
+            $(".locator-list").bind("DOMSubtreeModified", function() { 
+                if ($('#pickup-available .collectlocator-wrapper .locator-items .locator-item').length != 0) {
+                    alert("#pickup-available length is " + $('#pickup-available .collectlocator-wrapper .locator-items .locator-item').length);
+                    //$("#pickup-available").removeAttr("style");
+                    //$("#pickup-available").attr("style", "display:none !important;");
+                }
+                if ($('#pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length != 0) {
+                    alert("#pickup-unavailable length is " + $('#pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length);
+                    //$("#pickup-available").removeAttr("style");
+                    //$("#pickup-available").attr("style", "display:none !important;");
+                }
+            });
+            
         }
     });
 });
