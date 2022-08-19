6dc975a836aa6eb92fb67e9dec276ced8e826c9e
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-19 10:55:10 +0800

    Test Fix Click & Collect

--- app/code/Digidirect/Locator/view/frontend/web/js/view/display.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/view/display.js
@@ -13,12 +13,12 @@ define([
         toggleBlockDisplay: function () {
             //alert('New JS File Works!');
             //if ($('#pickup-available .collectlocator-wrapper .locator-items .locator-item').length != 0) {
-                alert("#pickup-available length is " + $('#pickup-available .collectlocator-wrapper .locator-items .locator-item').length);
+                alert("#pickup-available length is " + $('#pickup-available').length);
                 //$("#pickup-available").removeAttr("style");
                 //$("#pickup-available").attr("style", "display:none !important;");
             //}
             //if ($('#pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length != 0) {
-                alert("#pickup-unavailable length is " + $('#pickup-unavailable .collectlocator-wrapper .locator-items .locator-item').length);
+                alert("#pickup-unavailable length is " + $('#pickup-unavailable').length);
                 //$("#pickup-available").removeAttr("style");
                 //$("#pickup-available").attr("style", "display:none !important;");
             //}
