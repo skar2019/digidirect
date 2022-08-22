5d249e91f4ef5126aa23cb9dffc4509a28463942
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-22 12:14:24 +0800

    Test Fix Click & Collect

--- app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
@@ -48,8 +48,8 @@ define([
             
         },
         testDataBind: function () {
-            alert("#pickup-available length is " + $('#pickup-available .collectlocator-wrapper .mCustomScrollBox .mCSB_container').length);
-            alert("#pickup-unavailable length is " + $('#pickup-unavailable .collectlocator-wrapper .mCustomScrollBox .mCSB_container').length);
+            alert("#pickup-available length is " + $('#pickup-available .collectlocator-wrapper .mCustomScrollBox').length);
+            alert("#pickup-unavailable length is " + $('#pickup-unavailable .collectlocator-wrapper .mCustomScrollBox').length);
         },
         paginationObservable: function () {
             var self = this;
