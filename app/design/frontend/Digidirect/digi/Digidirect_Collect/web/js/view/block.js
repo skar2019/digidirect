d81b05c6bb0d4b2313fc6c8cc7d71da935bfbed5
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-24 13:26:30 +0800

    Test Fix Click & Collect

--- app/design/frontend/Digidirect/digi/Digidirect_Collect/web/js/view/block.js
+++ app/design/frontend/Digidirect/digi/Digidirect_Collect/web/js/view/block.js
@@ -83,11 +83,6 @@ define([
                 };
                 this.locationList = locations.items;
             }
-            
-            $(document).on('click', '#collect_type_collect', function () {
-                alert("#pickup-available length is " + $('#pickup-available .collectlocator-wrapper .mCustomScrollBox .locator-item').length);
-                alert("#pickup-unavailable length is " + $('#pickup-unavailable .collectlocator-wrapper .mCustomScrollBox .locator-item').length);
-            });
         },
         onSubscribe: function () {
 //            var self = this;
