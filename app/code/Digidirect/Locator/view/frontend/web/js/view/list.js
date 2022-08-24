2f67861005aec26ac65271b12b89faa1c3225df8
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-08-25 00:03:19 +0800

    Test Fix Click & Collect

--- app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
+++ app/code/Digidirect/Locator/view/frontend/web/js/view/list.js
@@ -192,12 +192,12 @@ define([
                 console.log('#pickup-available : ' + pickUpAvailable);
                 console.log('#pickup-unavailable : ' + pickUpUnavailable);
             
-                if (pickUpAvailable > 0) {
-                    $('#pickup-available').attr("style", "margin-bottom: 50px; display:block !important;");
+                if (pickUpUnavailable > 0) {
+                    $('#pickup-unavailable').attr("style", "margin-bottom: 50px; display:block !important;");
                 } 
                 
-                if (pickUpAvailable == 0) {
-                    $('#pickup-available').attr("style", "margin-bottom: 50px; display:none !important;");
+                if (pickUpUnavailable == 0) {
+                    $('#pickup-unavailable').attr("style", "margin-bottom: 50px; display:none !important;");
                 }
                 
             });
