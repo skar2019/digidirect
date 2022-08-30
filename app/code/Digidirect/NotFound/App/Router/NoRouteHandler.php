6e797e53100946348c728a8f771236ab754a261d
Clint <clint@kayweb.com.au>
2022-08-30 08:04:59 +0800

    to redeploy

--- app/code/Digidirect/NotFound/App/Router/NoRouteHandler.php
+++ app/code/Digidirect/NotFound/App/Router/NoRouteHandler.php
@@ -10,6 +10,4 @@ class NoRouteHandler implements \Magento\Framework\App\Router\NoRouteHandlerInte
         $request->setModuleName('catalogsearch')->setControllerName('result')->setActionName('index');
         return true;
     }
-    //to redeploy
-    //cant commit?
 }
