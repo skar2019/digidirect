458c3e34d74a6a1c699ba5dbebff4a2559268342
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-09-01 10:23:11 +0800

    Test Fix Itoris Pricematch Require Contact Number

--- app/code/Itoris/PriceMatch/view/frontend/web/js/itorispm.js
+++ app/code/Itoris/PriceMatch/view/frontend/web/js/itorispm.js
@@ -206,8 +206,8 @@ define([
                         checkValidName = $.validator.validateSingleElement(document.getElementById("itoris_pm_modal_name"));
                         checkValidEmail = $.validator.validateSingleElement(document.getElementById("itoris_pm_modal_email"));
                     }
-                    $(".itoris-custom-main-grid .mage-error").remove();
-                    
+                    //$(".itoris-custom-main-grid .mage-error").remove();
+                    $("#itoris-pm-modal [generated=true].mage-error").remove();
                     if(checkValidName && checkValidContact && checkValidEmail && checkValidPrice && checkValidUrl){
                         var dataPost = {
                             id: config['productId'],
