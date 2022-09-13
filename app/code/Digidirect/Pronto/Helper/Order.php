4a821ec3c69cc6410f74c0153d6debd9b8e95093
Clint <clint@kayweb.com.au>
2022-09-13 20:57:17 +0800

    shipped and backordered

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -817,11 +817,11 @@ class Order extends AbstractHelper
                 $data['sales-order']['detail']['line'][$x]['description'] = $item->getName();
                 $data['sales-order']['detail']['line'][$x]['unit-price-inc-tax'] = $price;
 
-                if($data['sales-order']['header']['set-on-status'] == "P")
+                if($data['sales-order']['header']['set-on-status'] == "B")
                 {
                     $data['sales-order']['detail']['line'][$x]['ordered'] = $qty;
-                    $data['sales-order']['detail']['line'][$x]['shipped'] = $qty;
-                    $data['sales-order']['detail']['line'][$x]['backordered'] = 0;
+                    $data['sales-order']['detail']['line'][$x]['shipped'] = 0;
+                    $data['sales-order']['detail']['line'][$x]['backordered'] = $qty;
                 }
                 else
                 {
