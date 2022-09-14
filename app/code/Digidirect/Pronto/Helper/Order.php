0f8a0ade27fcf49f9618be8cd4fc9c2298e587ab
Clint <clint@kayweb.com.au>
2022-09-14 12:11:59 +0800

    quantity on ready to pic

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -826,8 +826,8 @@ class Order extends AbstractHelper
                 else
                 {
                     $data['sales-order']['detail']['line'][$x]['ordered'] = $qty;
-                    $data['sales-order']['detail']['line'][$x]['shipped'] = 0;
-                    $data['sales-order']['detail']['line'][$x]['backordered'] = $qty;
+                    $data['sales-order']['detail']['line'][$x]['shipped'] = $qty;
+                    $data['sales-order']['detail']['line'][$x]['backordered'] = 0;
                 }
 
 
