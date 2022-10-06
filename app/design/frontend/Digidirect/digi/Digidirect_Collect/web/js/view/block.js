840b43a0f712457d5f7e0d95178942f4414923ce
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-10-06 17:46:27 +0800

    Test Fix Click & Collect

--- app/design/frontend/Digidirect/digi/Digidirect_Collect/web/js/view/block.js
+++ app/design/frontend/Digidirect/digi/Digidirect_Collect/web/js/view/block.js
@@ -23,7 +23,6 @@ define([
         placesUrl = window.checkoutConfig.quoteData.get_places_url,
         distanceList = window.checkoutConfig.quoteData.distance_list,
         selectedStore = window.checkoutConfig.quoteData.selected_collect_place;
-        productsAvailableInAnyStore = window.checkoutConfig.quoteData.products_available_in_any_store;
 
     return Component.extend({
         defaults: {
@@ -51,7 +50,6 @@ define([
 
             this._super();
 
-            this.checkProductAvailabilityForCC();
             this.setPreselectedStore();
             this.checkIsCollectSelected();
             this.onSubscribe();
@@ -61,7 +59,6 @@ define([
         formItemId: '',
         isSingleCartFormPopUpVisible: formPopUpState.isVisible,
         isCollectEnableOnCheckout: ko.observable(isCollectEnableOnCheckout || false),
-        productsAvailableInAnyStore: ko.observable(productsAvailableInAnyStore || false),
         isSingleCartCollectVariation: ko.observable(isSingleCartCollectVariation || false),
         placesUrl: placesUrl,
         distanceList: distanceList,
@@ -72,13 +69,6 @@ define([
             } else {
                 this.isCollectSelected(false);
                 quote.isCollectSelected = false;
-            }
-        },
-        checkProductAvailabilityForCC: function () {
-            console.log("block.js checkProductAvailabilityForCC : " + productsAvailableInAnyStore);
-            if (productsAvailableInAnyStore == false) {
-                $('.collect-type-collect-label').hide();
-                $('.collect-type-delivery-label').attr("style", "width: 100% !important");
             }
         },
         renderItems: function () {
@@ -221,6 +211,7 @@ define([
         },
         onErrorDelivery: function (response) {},
         applyCollectPlaceToAllItems: function (id, name) {
+            console.log("applyCollectPlaceToAllItems : " + id + ", " + name);
             var self,
                 serviceUrl,
                 payload;
