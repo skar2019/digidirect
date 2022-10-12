548bb1bbae4af6b8e795de4499fb0e81b45431b5
Clint <clint@kayweb.com.au>
2022-10-12 22:01:22 +0800

    do not log order data.

--- app/code/Digidirect/Vii/Observer/QuoteSubmitSuccess.php
+++ app/code/Digidirect/Vii/Observer/QuoteSubmitSuccess.php
@@ -119,7 +119,7 @@ class QuoteSubmitSuccess extends \Digidirect\AbstractGiftCard\Observer\QuoteSubm
                 $entityOrderData->setToken($entity->getToken());
                 $entityOrderData->setAbstractGiftCardEntityId($entity->getEntityId());
                 $this->abstractGiftCardEntityRepository->saveEntityOrderData($entityOrderData);
-                $this->logger->info('GiftCardLog Save Entity Order Data '.$entityOrderData);
+                $this->logger->info('GiftCardLog Save Entity Order Data');
             } catch (NoSuchEntityException $e) {
                 $this->logger->info('GiftCardLog Error : ' .$e->getMessage());
                 continue;
