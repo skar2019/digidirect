18fed17b03a794a62bc6ab911bb3d868b19084d8
Dev4 <jireh@kayweb.com.au>
2022-10-28 07:14:56 +0800

    Update email function

--- app/code/Digidirect/JaiTestEmail/Observer/CustomerLoginSuccess.php
+++ app/code/Digidirect/JaiTestEmail/Observer/CustomerLoginSuccess.php
@@ -68,7 +68,7 @@ class CustomerLoginSuccess implements ObserverInterface
             'note' => 'test note'
         ];
 
-        $templateParams = ['store' => $store, 'customer' => $customer, 'sender_details' => $senderDetails['ds_firstname'], $senderDetails['ds_lastname']];
+        $templateParams = ['store' => $store, 'customer' => $customer, 'ds_firstname' => $senderDetails['ds_firstname'], 'ds_lastname' => $senderDetails['ds_firstname'] ];
 
         // $sender = [
         //     'name' => $this->_escaper->escapeHtml($post['name']),
