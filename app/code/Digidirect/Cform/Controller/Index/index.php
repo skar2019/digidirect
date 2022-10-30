20fd80d91abfa5bf9ba75e05d2045c3b7d39bc73
Dev4 <jireh@kayweb.com.au>
2022-10-30 18:28:14 +0800

    Add Cform

---
+++ app/code/Digidirect/Cform/Controller/Index/index.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Digidirect\Cform\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+
+class Index extends Action
+{
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context
+    ) {
+        parent::__construct($context);
+    }   
+    public function execute()
+    {
+        $post = $this->getRequest()->getPostValue();
+        
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();       
+        $data = $objectManager->create('Digidirect\Cform\Model\Cform');
+        $data->setData($post);
+        $data->save();
+        /* echo "hello";
+        exit; */
+        
+        $this->messageManager->addSuccess(__('Form successfully submitted'));
+             
+    }
+}
