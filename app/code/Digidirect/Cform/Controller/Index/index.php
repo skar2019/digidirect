8a3cb25c96935961c6221b9cbfde35d9fb07717c
Dev4 <jireh@kayweb.com.au>
2022-10-31 08:29:43 +0800

    Cform display data

--- app/code/Digidirect/Cform/Controller/Index/index.php
+++ app/code/Digidirect/Cform/Controller/Index/index.php
@@ -23,8 +23,32 @@ class Index extends Action
         /* echo "hello";
         exit; */
 
-        print_r($post);
-        print_r($data);
+        echo $post;
+        echo $data;
+
+
+        // Get your post values
+        $firstname = "Jireh";
+        $lastname = "Sy";
+
+        // Send Mail functionality starts from here 
+        $from = "from_email_address@example.com";
+        $nameFrom = "From Name";
+        $to = "dev4@digidirect.com.au";
+        $nameTo = "To Name";
+        $body = "
+        <div>
+        <b>".$firstname."</b>
+        <i>".$lastname."</i>
+        </div>";
+
+        $email = new \Zend_Mail();
+        $email->setSubject("Cform Test"); 
+        $email->setBodyHtml($body);     // use it to send html data
+        //$email->setBodyText($body);   // use it to send simple text data
+        $email->setFrom($from, $nameFrom);
+        $email->addTo($to, $nameTo);
+        $email->send();
         
         $this->messageManager->addSuccess(__('Form successfully submitted'));
              
