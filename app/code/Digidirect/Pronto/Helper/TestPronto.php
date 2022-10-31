b90f504bb9b52cf05141feca6401cf6098e249ce
Clint <clint@kayweb.com.au>
2022-10-31 13:00:13 +0800

    giftcard

--- app/code/Digidirect/Pronto/Helper/TestPronto.php
+++ app/code/Digidirect/Pronto/Helper/TestPronto.php
@@ -1000,11 +1000,17 @@ class TestPronto extends AbstractHelper
             //gift cards
             $withGC = false;
             $gift_amount = $order->getGiftCardsAmount();
+            
             if($gift_amount > 0)
             {
                 $withGC = true;
                 $gift_amount = round($gift_amount, 2);
-                $gc_reference = $order->getGiftCards('c');
+                $gc_reference = $order->getGiftCards();
+                if($test)
+                {
+                    var_dump($gc_reference);
+                }
+                
                 $data['sales-order']['header']['payment-details']['payment-detail'][0]['payment-type'] = "VI";
                 $data['sales-order']['header']['payment-details']['payment-detail'][0]['payment-reference'] = $gc_reference;
                 $data['sales-order']['header']['payment-details']['payment-detail'][0]['amount-tendered'] = $gift_amount;
