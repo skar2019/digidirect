531d29cbaf86664c614b683547e7076d62539901
Dev4 <jireh@kayweb.com.au>
2022-11-01 18:28:21 +0800

    Update digidirect form

--- app/code/Digidirect/DigiSecondsForm/Controller/Index/index.php
+++ app/code/Digidirect/DigiSecondsForm/Controller/Index/index.php
@@ -17,7 +17,7 @@ class Index extends Action
         $post = $this->getRequest()->getPostValue();
         
         // Get post values
-        $fname = $this->getRequest()->getParam('name');
+        $fname = $this->getRequest()->getParam('firstname');
         $email = $this->getRequest()->getParam('email');
 
         // Send Mail functionality starts from here 
