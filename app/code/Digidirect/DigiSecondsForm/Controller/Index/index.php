96cc14fb83a9e1f39d2ec37b00c63aaae7897442
Dev4 <jireh@kayweb.com.au>
2022-11-02 07:07:26 +0800

    updated DigiSecondsForm

--- app/code/Digidirect/DigiSecondsForm/Controller/Index/index.php
+++ app/code/Digidirect/DigiSecondsForm/Controller/Index/index.php
@@ -26,23 +26,28 @@ class Index extends Action
         $purchaseYear = $this->getRequest()->getParam('purchaseYear');
         $notes = $this->getRequest()->getParam('notes');
         $askingPrice = $this->getRequest()->getParam('askingPrice');
-        
-        $testme = "Test value ito";
 
         // Send Mail functionality starts from here 
         $from = $email;
         $nameFrom = "From Name";
         $to = "dev4@digidirect.com.au";
         $nameTo = "To Name";
-        
-        $templateParams = ['testme' => $testme];
-        
+        $body = "
+        <div>
+            <li>".$firstname."</li>
+            <li>".$lastname."</li>
+            <li>".$phone."</li>
+            <li>".$email."</li>
+            <li>".$brands."</li>
+            <li>".$productName."</li>
+            <li>".$purchaseYear."</li>
+            <li>".$notes."</li>
+            <li>".$askingPrice."</li>
+        </div>";
 
         $email = new \Zend_Mail();
-        $email->setTemplateIdentifier('sending_email');
         $email->setSubject("DigiSecondsform Test"); 
-        $email->setTemplateVars($templateParams);
-//        $email->setBodyHtml($body);     // use it to send html data
+        $email->setBodyHtml($body);     // use it to send html data
         //$email->setBodyText($body);   // use it to send simple text data
         $email->setFrom($from, $nameFrom);
         $email->addTo($to, $nameTo);
