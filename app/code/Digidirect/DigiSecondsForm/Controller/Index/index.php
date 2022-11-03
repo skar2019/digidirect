43d951d2ecf7e13e20fef8167b971ab1fe719fa8
Dev4 <jireh@kayweb.com.au>
2022-11-03 22:24:52 +0800

    Update digisecconds form

--- app/code/Digidirect/DigiSecondsForm/Controller/Index/index.php
+++ app/code/Digidirect/DigiSecondsForm/Controller/Index/index.php
@@ -34,7 +34,9 @@ class Index extends Action
         // Send Mail functionality starts from here 
         $from = $email;
         $nameFrom = $firstname." ".$lastname;
-        $to = "dev4@digidirect.com.au".","."paul@digidirect.com.au".","."geoff.n@digidirect.com.au";
+        $receiver1 = "geoff.n@digidirect.com.au";
+        $receiver2 = "paul@digidirect.com.au";
+        $receiver3 = "dev4@digidirect.com.au";
         $bcc = "orders@kayweb.com.au";
         $nameTo = "Digidirect";
         $brandColor = "#990000";
@@ -55,7 +57,7 @@ class Index extends Action
         $email->setBodyHtml($body);     // use it to send html data
         //$email->setBodyText($body);   // use it to send simple text data
         $email->setFrom($from, $nameFrom);
-        $email->addTo($to, $nameTo);
+        $email->addTo($receiver1, $receiver2, $receiver3, $nameTo);
         $email->addBcc($bcc);
         $email->send();
         
