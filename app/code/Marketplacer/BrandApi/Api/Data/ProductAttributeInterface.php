9e481d139bd444419751a15055c014db36139712
Clint <clint@kayweb.com.au>
2022-11-09 10:10:28 +0800

    Remove app code Marketplacer. Updated it via composer.json

--- app/code/Marketplacer/BrandApi/Api/Data/ProductAttributeInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Marketplacer\BrandApi\Api\Data;
-
-interface ProductAttributeInterface
-{
-    public const BRAND_ATTRIBUTE_CODE = 'marketplacer_brand';
-}
--- app/code/Marketplacer/Marketplacer/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Marketplacer_Marketplacer', __DIR__);
