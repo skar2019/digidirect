2e697f0866182c12a6be5bd4adbdd607d2c9095a
Dev4 <jireh@kayweb.com.au>
2022-11-16 08:36:05 +0800

    update ready to pick up

--- app/code/Digidirect/ReadytoPickup/Observer/CustomerLoginSuccess.php
+++ app/code/Digidirect/ReadytoPickup/Observer/CustomerLoginSuccess.php
@@ -58,6 +58,7 @@ class CustomerLoginSuccess implements ObserverInterface
         ];
 
         $store = $this->storeManager->getStore();
+
         $customer_firstname = $order->getCustomerFirstname();
 
         $templateParams = ['store' => $store, 'customer' => $customer, 'customer_firstname' => $customer_firstname, 'administrator_name' => $receiverInfo['name']];
@@ -71,7 +72,7 @@ class CustomerLoginSuccess implements ObserverInterface
         )->setTemplateVars(
             $templateParams
         )->setFrom(
-            'general'
+            'jireh@kayweb.com.au'
         )->getTransport();
 
         try {
