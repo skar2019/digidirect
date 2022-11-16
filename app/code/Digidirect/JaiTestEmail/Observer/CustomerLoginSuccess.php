228cb0f4eb6c8582b9c11bd2309fdc8c420fdb6f
Dev4 <jireh@kayweb.com.au>
2022-11-16 12:34:51 +0800

    update ready to pick up

--- app/code/Digidirect/JaiTestEmail/Observer/CustomerLoginSuccess.php
+++ app/code/Digidirect/JaiTestEmail/Observer/CustomerLoginSuccess.php
@@ -62,6 +62,7 @@ class CustomerLoginSuccess implements ObserverInterface
         // $customer_firstname = $order->getCustomerFirstname();
         $customer_fullname = $observer->getEvent()->getCustomerName();
 
+        // $templateParams = ['store' => $store, 'customer' => $customer, 'customer_firstname' => $customer_firstname, 'customer_fullname' => $customer_fullname, 'administrator_name' => $receiverInfo['name']];
         $templateParams = ['store' => $store, 'customer' => $customer, 'customer_firstname' => $customer_firstname, 'customer_fullname' => $customer_fullname, 'administrator_name' => $receiverInfo['name']];
 
         $transport = $this->transportBuilder->setTemplateIdentifier(
