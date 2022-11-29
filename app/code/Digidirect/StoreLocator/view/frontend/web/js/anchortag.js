b3020c71d30c8a8f2572e5d02252ad8997d95a05
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-11-29 21:40:40 +0800

    Revert Close Button On Store Locator (Jireh)

--- app/code/Digidirect/StoreLocator/view/frontend/web/js/anchortag.js
+++ app/code/Digidirect/StoreLocator/view/frontend/web/js/anchortag.js
@@ -27,16 +27,6 @@ define(['jquery'], function($){
                     }
                 });
 
-                $(".sl-closebtn").click(function() {
-                    var status = $(document.querySelector("input[type=radio][name=storelist]:checked"));
-            
-                    if(status.is(":checked")) {
-                        status.prop("checked", false);
-                    } else {
-                        status.prop("checked", true);
-                    }
-                });
-
             });
         })
 
