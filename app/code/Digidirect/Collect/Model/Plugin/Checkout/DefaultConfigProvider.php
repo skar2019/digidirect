d2291151f7d0857f6b440740c5c6cef76bdf494b
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-12-06 08:55:37 +0800

    Test Fix Store Locator On Staging

--- app/code/Digidirect/Collect/Model/Plugin/Checkout/DefaultConfigProvider.php
+++ app/code/Digidirect/Collect/Model/Plugin/Checkout/DefaultConfigProvider.php
@@ -124,7 +124,6 @@ class DefaultConfigProvider
             $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
             $cart = $objectManager->get('\Magento\Checkout\Model\Cart');
             $items = $cart->getQuote()->getAllItems();
-
             
             $totalqty = 1;
             foreach ($items as $item) {
