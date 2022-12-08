55fbf7d6db9aa7d6b7088fa95efb28880a95ba4c
Clint <clint@kayweb.com.au>
2022-12-08 08:12:49 +0800

    MSI

---
+++ app/code/Digidirect/CollectAbstractEntityMSI/Api/Data/CollectFields/Constants.php
@@ -0,0 +1,11 @@
+<?php
+namespace Digidirect\CollectAbstractEntityMSI\Api\Data\CollectFields;
+
+/**
+ * Interface Constants
+ * @package Digidirect\CollectAbstractEntityMSI\Api\Data\CollectFields
+ */
+interface Constants
+{
+    const COLLECT_FIELD_INVENTORY_SOURCE = 'msi_sources';
+}
