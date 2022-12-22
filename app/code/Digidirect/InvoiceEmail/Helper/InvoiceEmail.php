3eeb7c36afc4cf972d30ae148768776ac440b88e
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-12-22 13:02:21 +0800

    Fixing Invoice Mail Email Template

--- app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
+++ app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
@@ -173,8 +173,8 @@ class InvoiceEmail extends AbstractHelper
             $customerFullName = $order->getCustomerFirstname() . ' ' . $order->getCustomerLastname();
             $customerEmail = $order->getCustomerEmail();
             $orderNumber = $order->getIncrementId();
-            $billingAddress = $this->getBillingAddress();
-            $shippingAddress = $this->getShippingAddress();
+            $billingAddress = $order->getBillingAddressId();
+            $shippingAddress = $order->getShippingAddressId();
             
             if($test)
             {
