59513d81dbd93a901da0c4f0e86cddc1cad89594
Dev4 <jireh@kayweb.com.au>
2022-12-23 10:46:15 +0800

    update invoice template

--- app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
+++ app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
@@ -175,17 +175,17 @@ class InvoiceEmail extends AbstractHelper
             $orderNumber = $order->getIncrementId();
             
             $billingAddress = $order->getBillingAddress();
-            // $billingStreet = $billingAddress->getStreet();
-            // if(is_array($billingStreet))
-            // {
-            //     $billingStreet = implode(",", $billingStreet);
-            // }
+            $billingStreet = $billingAddress->getStreet();
+            if(is_array($billingStreet))
+            {
+                $billingStreet = implode(",", $billingStreet);
+            }
             $billingCity = $billingAddress->getCity();
             $billingRegion = $billingAddress->getRegion();
             $billingPostal = $billingAddress->getPostcode();
             $billingCountry = $billingAddress->getCountryId();
-            $billingAddressConcat = $billingCity ."<br>". $billingRegion ."<br>". $billingPostal ." ". $billingCountry;
-            // $billingAddressConcat = $billingStreet ." ". $billingCity ."<br>". $billingRegion ."<br>". $billingPostal ." ". $billingCountry;
+            // $billingAddressConcat = $billingCity ."<br>". $billingRegion ."<br>". $billingPostal ." ". $billingCountry;
+            $billingAddressConcat = $billingStreet ." ". $billingCity ."<br>". $billingRegion ."<br>". $billingPostal ." ". $billingCountry;
             
             $shippingAddress = $order->getShippingAddress();
             // $shippingStreet = $shippingAddress->getStreet();
