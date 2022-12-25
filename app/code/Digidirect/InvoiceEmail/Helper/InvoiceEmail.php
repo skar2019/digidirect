dd74f00805045f8a1cb947487d4c6059656cf533
Dev4 <jireh@kayweb.com.au>
2022-12-25 19:58:04 +0800

    update invoice email template

--- app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
+++ app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
@@ -198,8 +198,8 @@ class InvoiceEmail extends AbstractHelper
             $shippingCountry = $shippingAddress->getCountryId();
             $shippingAddressConcat = $shippingStreet ."<br>". $shippingCity ."<br>". $shippingRegion ."<br>". $shippingPostal ." ". $shippingCountry;
 
-            $trackTitle = $order->getTracksCollection()->fetchItem()->getTitle();
-            $trackNumber = $order->getTracksCollection()->fetchItem()->getTrackNumber(); 
+//            $trackTitle = $order->getTracksCollection()->fetchItem()->getTitle();
+//            $trackNumber = $order->getTracksCollection()->fetchItem()->getTrackNumber(); 
 
             // product line
 //            foreach ($order->getAllVisibleItems() as $item) {
@@ -234,8 +234,8 @@ class InvoiceEmail extends AbstractHelper
             
             $store = $this->storeManager->getStore();
 
-            // $templateParams = ['store' => $store, 'order_number' => $orderNumber, 'customer_firstname' => $customerFirstName, 'customer_fullname' => $customerFullName,'billingAddress' => $billingAddressConcat, 'shippingAddress' => $shippingAddressConcat];
-            $templateParams = ['store' => $store, 'order_number' => $orderNumber, 'customer_firstname' => $customerFirstName, 'customer_fullname' => $customerFullName,'billingAddress' => $billingAddressConcat, 'shippingAddress' => $shippingAddressConcat, 'trackTitle' => $trackTitle, 'trackNumber' => $trackNumber, 'item' => $sku];
+             $templateParams = ['store' => $store, 'order_number' => $orderNumber, 'customer_firstname' => $customerFirstName, 'customer_fullname' => $customerFullName,'billingAddress' => $billingAddressConcat, 'shippingAddress' => $shippingAddressConcat];
+//            $templateParams = ['store' => $store, 'order_number' => $orderNumber, 'customer_firstname' => $customerFirstName, 'customer_fullname' => $customerFullName,'billingAddress' => $billingAddressConcat, 'shippingAddress' => $shippingAddressConcat, 'trackTitle' => $trackTitle, 'trackNumber' => $trackNumber, 'item' => $sku];
 
             $transport = $this->transportBuilder->setTemplateIdentifier(
                 'digidirect_invoice_email_template'
