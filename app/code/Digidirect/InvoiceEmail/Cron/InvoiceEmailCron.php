62bdef9d43a48ed540598e5382ba3db8bc468435
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2022-12-27 19:07:07 +0800

    Invoice Email Module

---
+++ app/code/Digidirect/InvoiceEmail/Cron/InvoiceEmailCron.php
@@ -0,0 +1,32 @@
+<?php
+namespace Digidirect\InvoiceEmail\Cron;
+
+use Psr\Log\LoggerInterface;
+use Digidirect\InvoiceEmail\Helper\InvoiceEmail;
+
+class InvoiceEmailCron
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var Inventory
+     */
+    protected $helper;
+    
+    public function __construct(
+            LoggerInterface $logger,
+            InvoiceEmail $helper)
+    {
+        $this->logger = $logger;
+        $this->helper = $helper;
+    }
+    public function execute()
+    {
+        $test = 1;
+        $this->helper->sendInvoiceEmail($test);
+   
+    }
+}
