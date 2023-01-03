e533be213b0058eb1260451704ac5dc2dd485eff
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-01-03 14:25:29 +0800

    Test Fix Invoice Mail

--- app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
+++ app/code/Digidirect/InvoiceEmail/Helper/InvoiceEmail.php
@@ -181,6 +181,10 @@ class InvoiceEmail extends AbstractHelper
             $shippingAddressConcat = $shippingStreet ."<br>". $shippingCity ."<br>". $shippingRegion ."<br>". $shippingPostal ." ". $shippingCountry;
             $shippingAmount = round($order->getShippingAmount(), 2);
             
+            $totalFOrGst = round($orderGrandTotal - $shippingAmount, 2);
+            $totalEx = round($totalFOrGst / 1.1, 2);
+            $gst = round($totalFOrGst - $totalEx, 2);
+            
             $invoiceDate = date('d/m/Y', strtotime($this->date->gmtDate()));
             
             $tracksCollection = $order->getTracksCollection();
@@ -209,6 +213,9 @@ class InvoiceEmail extends AbstractHelper
                 'order_number' => $orderNumber, 
                 'order_subtotal' => $orderSubtotal, 
                 'order_grandtotal' => $orderGrandTotal,
+                'total_ex' => $totalEx,
+                'gst' => $gst,
+                'total_for_gst' => $totalFOrGst,
                 'coupon_discount' => $couponDiscount,
                 'invoice_date' => $invoiceDate, 
                 'customer_firstname' => $customerFirstName, 
