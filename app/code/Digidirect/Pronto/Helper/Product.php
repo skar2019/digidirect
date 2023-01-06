d0b78f78e94b8db5560a0cfa30bb6657a0f237b4
Clint <clint@kayweb.com.au>
2023-01-06 09:02:47 +0800

    fix manual sync

--- app/code/Digidirect/Pronto/Helper/Product.php
+++ app/code/Digidirect/Pronto/Helper/Product.php
@@ -1681,16 +1681,18 @@ class Product extends AbstractHelper
                 $product->setCustomAttribute('apn', $prod['stk-apn-number']);
                 $product->setCustomAttribute('qff_base', $prod['qff-base-points-per-dollar']);
                 $product->setCustomAttribute('qff_bonus_points', $prod['qff-bonus-points-per-dollar']);
-                echo $prod['qff-store-product-name'] . "<br/>";
-                echo $prod['qff-store-price'] . "<br/>";
+                
                 if(isset($prod['qff-store-product-name']))
                 {
+                    echo $prod['qff-store-product-name'] . " qff-store-product-name<br/>";
+                
                     $product->setCustomAttribute('qff_store_product_name', $prod['qff-store-product-name']);   
                 }
                 if(isset($prod['qff-store-price']))
                 {
                     $product->setCustomAttribute('qff_store_price', $prod['qff-store-price']);   
                     echo "set qff-store-price <br/>";
+                    echo $prod['qff-store-price'] . "<br/>";
                 }
                 
                 if($prod['stk-condition-code'] == 'T')
@@ -2383,16 +2385,18 @@ class Product extends AbstractHelper
                 $product->setCustomAttribute('apn', $prod['stk-apn-number']);
                 $product->setCustomAttribute('qff_base', $prod['qff-base-points-per-dollar']);
                 $product->setCustomAttribute('qff_bonus_points', $prod['qff-bonus-points-per-dollar']);
-                echo $prod['qff-store-product-name'] ."<br/>";
-                echo $prod['qff-store-price'] ."<br/>";
+                
                 if(isset($prod['qff-store-product-name']))
                 {
                     $product->setCustomAttribute('qff_store_product_name', $prod['qff-store-product-name']);   
+                    echo $prod['qff-store-product-name'] ."<br/>";
+                
                 }
                 if(isset($prod['qff-store-price']))
                 {
                     $product->setCustomAttribute('qff_store_price', $prod['qff-store-price']);   
                     echo "set qff-store-price <br/>";
+                    echo $prod['qff-store-price'] ."<br/>";
                 }
                 
                 if($prod['stk-condition-code'] == 'T')
