545ac0047b5340d35e1360bffe60f8887c9fc25f
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-01-15 12:52:46 +0800

    Test Fix SHWS Ready To Pickup Email

--- app/code/Digidirect/ReadytoPickup/Helper/ReadytoPickup.php
+++ app/code/Digidirect/ReadytoPickup/Helper/ReadytoPickup.php
@@ -264,6 +264,7 @@ class ReadytoPickup extends AbstractHelper
     }
     
     public function getStoreSwhsStoreHOurs(OrderInterface $order) {
+        
         $storeHours = "";
         if (!isset($this->warehouseCode[$order->getEntityId()])) {
             $whse = '';
