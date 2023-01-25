7051f6001e834590368ea07fdd6b6ea05d5f6bd6
Clint <clint@kayweb.com.au>
2023-01-25 08:45:54 +0800

    workaround brands.html error

--- app/code/Digidirect/AbstractAttributes/Model/UrlProcessor/Attribute.php
+++ app/code/Digidirect/AbstractAttributes/Model/UrlProcessor/Attribute.php
@@ -41,6 +41,11 @@ class Attribute extends ProcessorAbstract
 
             $urlSuffix = $this->urlHelper->getUrlSuffix($store->getId());
             $requestPath = sprintf(self::REQUEST_PATH_PATTERN, $attrUrlKey) . $urlSuffix;
+            //clint work around brands error 25-01-23
+            if($requestPath == 'brands.html')
+            {
+                $requestPath = 'digibrands.html';
+            }
             $urls[] = $this->urlRewriteFactory->create()
                 ->setEntityType(self::URL_ENTITY_TYPE)
                 ->setEntityId($attrId)
