eae815738aaba7cb3cc8fda1b8cebf689691374d
Clint <clint@kayweb.com.au>
2023-02-21 20:17:25 +0800

    CollectAbstractEntityMSI

---
+++ app/code/Digidirect/CollectAbstractEntityMSI/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Digidirect_CollectAbstractEntityMSI',
+    __DIR__
+);
