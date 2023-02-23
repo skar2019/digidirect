23a820f1544569b8324e22f686e12c4e0481b885
Dev4 <jireh@kayweb.com.au>
2023-02-23 14:10:59 +0800

    push extendedpricematchpermission

---
+++ app/code/Digidirect/ExtendedPriceMatchPermission/Controller/Index/Index.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Digidirect\ExtendedPriceMatchPermission\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action {
+    public function execute() {
+        echo "This is ny first module. Created by Jireh";
+    }
+}
