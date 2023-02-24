d41725e6feb5a3170e11ef98dd0627649e532f22
Dev4 <jireh@kayweb.com.au>
2023-02-24 10:45:34 +0800

    push extendedpricematchpermission

--- app/code/Digidirect/ExtendedPriceMatchPermission/Observer/AdminhtmlBlockHtmlBeforeObserver.php
+++ app/code/Digidirect/ExtendedPriceMatchPermission/Observer/AdminhtmlBlockHtmlBeforeObserver.php
@@ -124,34 +124,18 @@ class AdminhtmlBlockHtmlBeforeObserver implements ObserverInterface
         $getlayout = !empty($block->getNameInLayout()) ? $block->getNameInLayout() : '';
 
         // Handle prices that are shown when admin reviews customers shopping cart
-        // if(!empty($block->getNameInLayout())) {
-        //     if (stripos($block->getNameInLayout(), 'customer_cart_') === 0) {
-        //         if (!$this->observerData->isCanReadProductPrice()) {
-        //             if ($block->getParentBlock()->getNameInLayout() == 'admin.customer.carts') {
-        //                 $this->_removeColumnFromGrid($block, 'price');
-        //                 $this->_removeColumnFromGrid($block, 'total');
-        //             }
-        //         }
-        //     }
-        // }
-
         
-        if(!$getlayout)
-        {
-           $getlayout = '';
-        }
-
-        return "test";
-
-
-        if (stripos($block->getNameInLayout(), 'customer_cart_') === 0) {
-            if (!$this->observerData->isCanReadProductPrice()) {
-                if ($block->getParentBlock()->getNameInLayout() == 'admin.customer.carts') {
-                    $this->_removeColumnFromGrid($block, 'price');
-                    $this->_removeColumnFromGrid($block, 'total');
+        if(!empty($block->getNameInLayout())) {
+                if (!$this->observerData->isCanReadProductPrice()) {
+                    if ($block->getParentBlock()->getNameInLayout() == 'admin.customer.carts') {
+                        $this->_removeColumnFromGrid($block, 'price');
+                        $this->_removeColumnFromGrid($block, 'total');
+                    }
                 }
             }
-        }
+        
+
+
         
     }
 
