569519089eb51e01e38e797dba91f78191befe23
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-02-26 21:11:32 +0800

    Fix ZIP Custom Link

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -115,8 +115,8 @@ define([
     });
     
     $('#zip-custom').on('click', function(){
-        //$('.zip-container').trigger("click");
-        $('.zip-container')[0].click();
+        $('.zip-widget__wrapper').trigger("click");
+        //$('.zip-container')[0].click();
         console.log("ZIP Custom Clicked!");
     });
 
