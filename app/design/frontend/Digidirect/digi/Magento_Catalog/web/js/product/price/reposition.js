4d1990a471002e88f283a7c2f4fcd4687da940c6
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-02-26 15:59:47 +0800

    Trigger Click ZIP From Custom Button

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -113,5 +113,10 @@ define([
         $('#itoris-pm-link').trigger("click");
         console.log("Changed To Trigger Click!");
     });
+    
+    $('#zip-custom').on('click', function(){
+        $('#zip-info-link').trigger("click");
+        console.log("ZIP Custom Clicked!");
+    });
 
 });
