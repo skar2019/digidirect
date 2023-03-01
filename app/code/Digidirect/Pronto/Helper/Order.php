8a78c2669f499cd44ac953b4107961fc540ca4eb
Clint <clint@kayweb.com.au>
2023-03-01 10:05:52 +0800

    filter latitude pending

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -1073,7 +1073,6 @@ class Order extends AbstractHelper
             ->addAttributeToSelect('*')
             ->addFieldToFilter('pronto_order_number', array('null' => true))
             ->addFieldToFilter('status',array('nin' => array('canceled', 'pending_latitude_approval')))
-            //->addFieldToFilter('status',array(array('neq' => 'canceled'), array('neq' => 'pending_latitude_approval')))
             ->addFieldToFilter('entity_id', array('gteq' => 1499838)) //615813
             ->setOrder('created_at', 'asc');
 
