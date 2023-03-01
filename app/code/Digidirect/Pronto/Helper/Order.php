8fc6928865bde073794ba68102716fa001f04736
Clint <clint@kayweb.com.au>
2023-03-01 18:10:03 +0800

    new ordersync

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -1072,9 +1072,10 @@ class Order extends AbstractHelper
         $collection = $this->_orderCollectionFactory->create()
             ->addAttributeToSelect('*')
             ->addFieldToFilter('pronto_order_number', array('null' => true))
-            ->addFieldToFilter('status',array('nin' => array('canceled', 'pending_latitude_approval')))
+            ->addFieldToFilter('status',array('nin' => array('canceled','pending_latitude_approval')))
             ->addFieldToFilter('entity_id', array('gteq' => 1499838)) //615813
             ->setOrder('created_at', 'asc');
+        //->addFieldToFilter('status',array('neq' =>'canceled'))
 
      return $collection;
 
