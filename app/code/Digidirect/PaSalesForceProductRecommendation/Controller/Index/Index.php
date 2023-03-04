34a70f5eca3b94bb4b074acba3608b1c1ab8f7e4
Dev4 <jireh@kayweb.com.au>
2023-03-04 08:56:39 +0800

    Push PaSalesForce module

---
+++ app/code/Digidirect/PaSalesForceProductRecommendation/Controller/Index/Index.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Digidirect\PaSalesForceProductRecommendation\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\Result\RawFactory;
+
+class Index implements ActionInterface {
+
+    /**
+     * @var  RawFactory
+    */
+
+    protected $resultFactory;
+
+    /**
+     * Index constructor
+     * 
+     * @param RawFactory $resultFactory
+    */
+
+
+    public function __construct(RawFactory $resultFactory) {
+        $this->resultFactory = $resultFactory;
+    }
+
+    public function execute() {
+        // die('Test module');
+        return $this->resultFactory->create()->setContents('Test Customer ID');
+    }
+}
