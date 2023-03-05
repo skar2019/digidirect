e499bf453a7fc16407b5f4402f50d1f640d2453c
Dev4 <jireh@kayweb.com.au>
2023-03-05 20:37:29 +0800

    update PaSalesForce module

--- app/code/Digidirect/PaSalesForceProductRecommendation/Controller/Index/Index.php
+++ app/code/Digidirect/PaSalesForceProductRecommendation/Controller/Index/Index.php
@@ -28,11 +28,14 @@ class Index implements ActionInterface {
 
     public function execute() {
         // die('Test module');
-        return $this->resultFactory->create()->setContents('Test Customer ID');
+        // return $this->resultFactory->create()->setContents('Test Customer ID');
+        
 
         // $resultPage = $this->resultPageFactory ->create();
         // $blockInstance = $resultPage->getLayout()->getBlock('pa-salesforce-data');
 
         // return $resultPage;
+
+        echo '<script>alert("This is alert")</script>';
     }
 }
