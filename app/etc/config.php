5b3c8d6983febc749faa9284c0638263c2cd28ab
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-03-06 21:54:17 +0800

    Remove FAQ Module On Config

--- app/etc/config.php
+++ app/etc/config.php
@@ -580,7 +580,6 @@ return [
         'Digidirect_ExtendedCatalogPriceRule' => 1,
         'Digidirect_ExtendedShippingRates' => 1,
         'Digidirect_ExtendedShippingRatesLocalization' => 1,
-        'Digidirect_Faq' => 1,
         'Digidirect_Feed' => 1,
         'Digidirect_FilterShipping' => 1,
         'Digidirect_FreeGift' => 1,
