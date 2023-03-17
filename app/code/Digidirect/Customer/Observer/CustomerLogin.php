f10a3f3c2a8eaac2ab6207518fee0de6f8e73775
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-03-17 13:21:22 +0800

    Save PA ID On Customers Table

--- app/code/Digidirect/Customer/Observer/CustomerLogin.php
+++ app/code/Digidirect/Customer/Observer/CustomerLogin.php
@@ -1,16 +1,40 @@
 <?php
 
-namespace Vendor\Module\Observer;
+namespace Digidirect\Customer\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 
 class CustomerLogin implements ObserverInterface
 {
+    protected $customerFactory;
+    protected $customerRepository;
+    
+
+    public function __construct(
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        CustomerRepositoryInterface $customerRepository
+    ){
+        $this->customerFactory = $customerFactory;
+        $this->customerRepository = $customerRepository;
+    }
+    
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        echo "Customer LoggedIn";
+        /*echo "Customer LoggedIn";
         $customer = $observer->getEvent()->getCustomer();
         echo $customer->getName(); //Get customer name
-        exit;
+        exit;*/
+        
+        $customer = $observer->getCustomer();
+        if(!$customer instanceof \Magento\Customer\Model\Customer){
+            $customer = $this->customerFactory->create()->load($customer->getId());
+        }
+
+        /* Save customer Custom*/
+        /*$controller = $observer->getAccountController();
+        $family_name = $controller->getRequest()->getParam('pa_customer_id');*/
+
+        $customer->setData('pa_customer_id', 'Testing!')->save();
     }
 }
