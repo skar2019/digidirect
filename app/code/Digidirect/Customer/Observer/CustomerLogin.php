f0ed60a2d6526f070ff8a2cb6085473e89d424b2
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-03-17 12:59:02 +0800

    Save PA ID On Customers Table

--- app/code/Digidirect/Customer/Observer/CustomerLogin.php
+++ app/code/Digidirect/Customer/Observer/CustomerLogin.php
@@ -3,22 +3,9 @@
 namespace Vendor\Module\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Customer\Api\CustomerRepositoryInterface;
 
 class CustomerLogin implements ObserverInterface
 {
-    protected $customerFactory;
-    protected $customerRepository;
-    
-
-    public function __construct(
-        \Magento\Customer\Model\CustomerFactory $customerFactory,
-        CustomerRepositoryInterface $customerRepository
-    ){
-        $this->customerFactory = $customerFactory;
-        $this->customerRepository = $customerRepository;
-    }
-    
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         echo "Customer LoggedIn";
