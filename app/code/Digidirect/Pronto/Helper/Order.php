ed514486c39b9cab9b6b6949f36dabd98cd81848
Clint <clint@kayweb.com.au>
2023-03-27 08:43:09 +0800

    latipay debug

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -586,6 +586,7 @@ class Order extends AbstractHelper
                     {
                         $data['sales-order']['header']['on-hold-reason-code'] = "WP";
                         $data['sales-order']['header']['set-on-status'] = "H";
+                        continue;
                     }
 
                 }
--- app/code/Digidirect/Pronto/Helper/TestPronto.php
+++ app/code/Digidirect/Pronto/Helper/TestPronto.php
@@ -984,6 +984,11 @@ class TestPronto extends AbstractHelper
 
             $payment_reference = $paymentInstance->getLastTransId();
             echo "payment_reference - " .$payment_reference ."<br>";
+            if($method == 'latipay')
+            {
+                echo $paymentInstance->getAdditionalInformation();
+
+            }
             if (empty($payment_reference) && ($method == 'latipay')) {
                 //$payment_reference = $paymentInstance->getAdditionalInformation('klarna_order_id');
                 //if (empty($payment_reference)){
