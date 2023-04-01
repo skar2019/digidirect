990502ea92f1ba81c1af1db1ff64fa55ab1157ef
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-04-01 11:24:38 +0800

    Fix Overlay Bug

--- app/code/Digidirect/ProductOverlay/Model/AbstractOverlays.php
+++ app/code/Digidirect/ProductOverlay/Model/AbstractOverlays.php
@@ -1392,9 +1392,11 @@ class AbstractOverlays extends AbstractModel implements OverlayInterface, Identi
     protected function _getDataArray($key)
     {
         $value = $this->getData($key);
-        if (!is_array($value)) {
-            $value = array_filter(explode(',', $value));
-            $this->setData(self::CATALOG_PRICE_RULES_IDS, $value);
+        if ($value) {
+            if (!is_array($value)) {
+                $value = array_filter(explode(',', $value));
+                $this->setData(self::CATALOG_PRICE_RULES_IDS, $value);
+            }
         }
         return $value;
     }
