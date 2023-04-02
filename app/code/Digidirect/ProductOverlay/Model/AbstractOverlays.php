2b07f6dd21a7df8858218989fd04e0ea12a20cf3
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-04-02 13:43:31 +0800

    Test Fix Overlay Bug

--- app/code/Digidirect/ProductOverlay/Model/AbstractOverlays.php
+++ app/code/Digidirect/ProductOverlay/Model/AbstractOverlays.php
@@ -262,7 +262,7 @@ class AbstractOverlays extends AbstractModel implements OverlayInterface, Identi
             /** @var \Digidirect\ProductOverlay\Model\Rule $overlayRule */
             $overlayRule = $this->_overlayRuleFactory->create();
             $overlayRule->setConditions([]);
-            $overlayRule->setStores($this->getStores());
+            $overlayRule->setStores([0,1]);
             $overlayRule->setConditionsSerialized($this->getCondSerialize());
             $overlayRule->setProduct($product);
 
@@ -731,7 +731,7 @@ class AbstractOverlays extends AbstractModel implements OverlayInterface, Identi
      */
     public function getStores()
     {
-        return $this->_getDataArray(self::STORES);
+        //return $this->_getDataArray(self::STORES);
     }
 
     /**
@@ -1340,7 +1340,7 @@ class AbstractOverlays extends AbstractModel implements OverlayInterface, Identi
      */
     public function getCatalogPriceRulesIds()
     {
-        return $this->_getDataArray(self::CATALOG_PRICE_RULES_IDS);
+        //return $this->_getDataArray(self::CATALOG_PRICE_RULES_IDS);
     }
 
     /**
@@ -1392,12 +1392,10 @@ class AbstractOverlays extends AbstractModel implements OverlayInterface, Identi
     protected function _getDataArray($key)
     {
         $value = $this->getData($key);
-        if ($value) {
-            if (!is_array($value)) {
-                $value = array_filter(explode(',', $value));
-                $this->setData(self::CATALOG_PRICE_RULES_IDS, $value);
-            }
-            return $value;
+        if (!is_array($value)) {
+            $value = array_filter(explode(',', $value));
+            $this->setData(self::CATALOG_PRICE_RULES_IDS, $value);
         }
+        return $value;
     }
 }
