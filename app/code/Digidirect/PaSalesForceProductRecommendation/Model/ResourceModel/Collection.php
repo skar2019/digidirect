6a24c27b70917d91c481868b17d6c60079f2fe6a
Dev4 <jireh@kayweb.com.au>
2023-04-17 14:26:14 +0800

    PaSalesForce update module

--- app/code/Digidirect/PaSalesForceProductRecommendation/Model/ResourceModel/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-namespace Digidirect\PaSalesForceProductRecommendation\Model\ResourceModel\DataExample;
-class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection{
-	public function _construct(){
-		$this->_init("Digidirect\PaSalesForceProductRecommendation\Model\DataExample","Digidirect\PaSalesForceProductRecommendation\Model\ResourceModel\DataExample");
-	}
-}
- ?>
