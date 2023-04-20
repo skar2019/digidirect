7c5957d17238710bcc9f4f8ad2600129b74db2d7
Dev4 <jireh@kayweb.com.au>
2023-04-20 12:48:29 +0800

    update sales force PA module

---
+++ app/code/Digidirect/PaSalesForceProductRecommendation/Model/ResourceModel/Collection.php
@@ -0,0 +1,8 @@
+<?php 
+namespace Digidirect\PaSalesForceProductRecommendation\Model\ResourceModel\DataExample;
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection{
+	public function _construct(){
+		$this->_init("Digidirect\PaSalesForceProductRecommendation\Model\DataExample","Digidirect\PaSalesForceProductRecommendation\Model\ResourceModel\DataExample");
+	}
+}
+ ?>
