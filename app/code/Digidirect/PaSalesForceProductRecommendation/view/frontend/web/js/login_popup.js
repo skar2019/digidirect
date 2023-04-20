d5b3b5bcfc77c068f16781ab056fce500404135a
Dev4 <jireh@kayweb.com.au>
2023-04-20 13:45:20 +0800

    update sales force PA module

--- app/code/Digidirect/PaSalesForceProductRecommendation/view/frontend/web/js/login_popup.js
+++ app/code/Digidirect/PaSalesForceProductRecommendation/view/frontend/web/js/login_popup.js
@@ -66,29 +66,8 @@ define([
             return this.renderer(data);
         },
         showPopup: function () {
-
-            alert("test1")
-    
-            var param = '{pa_id='+magento_pa_id+'}';
-            var YOUR_URL_HERE = 'getpaidsalesforce'
-        
-              $.ajax({
-                url: YOUR_URL_HERE,
-                type: "POST",
-                data: {pa_id: magento_pa_id},
-                dataType: "json",
-                success: function() {
-                  alert("Thank you for subscribing!");
-                },
-                error: function() {
-                  alert("There was an error. Try again please!");
-                }
-              });
-
             return $.mage.cookies.get('can_show_login_popup') === "1" && $.mage.cookies.get('displayed_login_popup') === "0";
         }
     });
-
-    alert("test3")
 
 });
