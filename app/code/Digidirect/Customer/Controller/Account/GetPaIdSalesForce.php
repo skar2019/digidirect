0f52b3b1f317bfa4c0bea18e3fdc5529b89a01a8
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-05-03 10:50:29 +0800

    Test PA Fix (Echo PA ID)

--- app/code/Digidirect/Customer/Controller/Account/GetPaIdSalesForce.php
+++ app/code/Digidirect/Customer/Controller/Account/GetPaIdSalesForce.php
@@ -13,6 +13,7 @@ class GetPaIdSalesForce extends \Magento\Framework\App\Action\Action {
     }
 
     public function execute(){
+        echo $this->getRequest()->getPost('pa_id');
         if ($this->getRequest()->getPost('pa_id')):
             $query = $this->getRequest()->getPost('pa_id');
             $data = array($query);
