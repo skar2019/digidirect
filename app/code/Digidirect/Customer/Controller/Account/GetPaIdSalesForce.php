540aa01eed90a2be29edf4e058ccf006819541ea
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-05-04 13:30:51 +0800

    Test PA Fix (Login JS Override)

--- app/code/Digidirect/Customer/Controller/Account/GetPaIdSalesForce.php
+++ app/code/Digidirect/Customer/Controller/Account/GetPaIdSalesForce.php
@@ -3,22 +3,32 @@
 namespace Digidirect\Customer\Controller\Account;
 
 use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\App\Action\Context;
 
 class GetPaIdSalesForce extends \Magento\Framework\App\Action\Action {
     
     protected $request;
-    public function __construct(Context $context) {
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Customer\Model\Session $customerSession
+    ) 
+    {
         parent::__construct($context);
+        $this->_customerSession = $customerSession;
+        $this->_customerRepository = $customerRepository;
     }
 
     public function execute(){
         if ($this->getRequest()->getPost('pa_id')):
-            $query = $this->getRequest()->getPost('pa_id');
-            $data = array($query);
+            $pa_id = $this->getRequest()->getPost('pa_id');
+            $data = array($pa_id);
             $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
             $resultJson->setData($data);
-            return $resultJson;
+            //return $resultJson;
+            $customerID = $this->_customerSession->getCustomer()->getId();
+            $customer = $this->_customerRepository->getById($customerID);
+            $customer->setCustomAttribute('pa_customer_id', $pa_id);
+            $this->_customerRepository->save($customer);
         endif;
     }
     
