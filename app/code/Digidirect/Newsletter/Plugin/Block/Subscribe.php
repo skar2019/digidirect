d78ec2c474db12ea2b74d6509f7afa54de050f0e
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-05-10 15:14:06 +0800

    Newsletter Plugin (Marketing Consent)

--- app/code/Digidirect/Newsletter/Plugin/Block/Subscribe.php
+++ app/code/Digidirect/Newsletter/Plugin/Block/Subscribe.php
@@ -20,7 +20,8 @@ class Subscribe
     {
         $customerID = $this->customerSession->getCustomer()->getId();
         $customer = $this->customerRepository->getById($customerID);
-        $customer->setCustomAttribute('pa_id', 'Testing!');
+        $customer->setCustomAttribute('marketing_consent', 1);
         $this->customerRepository->save($customer);
+        echo json_encode($customer);
     }
 }
