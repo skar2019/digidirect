355154efea1fd2de21a38ccf3aa6fdb5e063faa7
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-05-10 12:17:40 +0800

    Newsletter Plugin (Marketing Consent)

---
+++ app/code/Digidirect/Newsletter/Plugin/Block/Subscribe.php
@@ -0,0 +1,23 @@
+<?php
+namespace Digidirect\Newsletter\Plugin\Block;
+
+class Subscribe
+{
+    protected $customerRepository;
+    
+    protected $customerSession;
+    
+    public function __construct(
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Customer\Model\Session $customerSession
+    ) 
+    {
+        $this->customerSession = $customerSession;
+        $this->customerRepository = $customerRepository;
+    }
+    
+    public function beforeGetFormActionUrl(\Magento\Newsletter\Block\Subscribe $newsletter)
+    {
+        echo 'Test Newsletter Plugin!';
+    }
+}
