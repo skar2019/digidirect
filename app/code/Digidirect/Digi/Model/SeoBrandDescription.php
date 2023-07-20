7061a7cef9f443f5a3c28e3a97953afc33ca6e71
Clint <clint@kayweb.com.au>
2023-07-20 18:52:23 +0800

    TypeError: str_replace(): Argument #1 ($search) must be of type array|string, null given in /app/6ycwrjafqqprk/app/code/Digidirect/Digi/Model/SeoBrandDescription.php:242

--- app/code/Digidirect/Digi/Model/SeoBrandDescription.php
+++ app/code/Digidirect/Digi/Model/SeoBrandDescription.php
@@ -239,7 +239,14 @@ class SeoBrandDescription
     {
         $url = preg_replace('/\?.*/i', '', $this->url->getCurrentUrl());
         $categorySuffix = $this->scopeConfig->getValue('catalog/seo/category_url_suffix');
-        $seoPart = trim(str_replace($categorySuffix, '', last(explode(Url::FILTERS_DELIMITER, $url))), '/');
+        if(!empty($categorySuffix))
+        {
+            $seoPart = trim(str_replace($categorySuffix, '', last(explode(Url::FILTERS_DELIMITER, $url))), '/');
+        }
+        else
+        {
+            $seoPart = "";
+        }
         $params = $this->urlParser->parseSeoPart($seoPart);
 
         if (!empty($params)) {
