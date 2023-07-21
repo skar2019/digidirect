9a4dc03eb837f1b79da0b1078e5701a05a605371
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-07-21 13:12:08 +0800

    Fix Next Day Delivery Label On Checkout

--- app/code/Digidirect/ExtendedShippingRates/Model/Carrier/Artificial.php
+++ app/code/Digidirect/ExtendedShippingRates/Model/Carrier/Artificial.php
@@ -346,6 +346,8 @@ class Artificial extends AbstractCarrier implements CarrierInterface
                 else{
                     $method->setMethodTitle(" (4 to 9 Days)");
                 }
+            }elseif ($methodCode == "nextdaydelivery") {
+                $method->setMethodTitle(" ");
             }elseif ($methodCode == "intlshippingnz") {
                 if($qty > 0){
                     $method->setMethodTitle(" (5 to 9 Days)");
@@ -365,7 +367,7 @@ class Artificial extends AbstractCarrier implements CarrierInterface
             //end modification
             //
             //$method->setMethodTitle($methodData->getData('title'));
-             $method->setPrice($methodData->getData('price'));
+            $method->setPrice($methodData->getData('price'));
         }
 
         return $method;
