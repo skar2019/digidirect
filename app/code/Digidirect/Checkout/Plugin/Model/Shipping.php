daca66a4f4fe8dc6e67d804ea2796a0be38c00c0
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-07-24 17:51:08 +0800

    Test Disable Shipping

--- app/code/Digidirect/Checkout/Plugin/Model/Shipping.php
+++ app/code/Digidirect/Checkout/Plugin/Model/Shipping.php
@@ -2,17 +2,50 @@
 
 namespace Digidirect\Checkout\Plugin\Model;
 
+use Magento\Inventory\Model\SourceItem\Command\GetSourceItemsBySku;
+
 class Shipping {
     
+    public function __construct(
+        GetSourceItemsBySku $getSourceItemsBySku
+    ) {
+        $this->getSourceItemsBySku = $getSourceItemsBySku;
+    }
+       
     public function aroundCollectCarrierRates(
         \Magento\Shipping\Model\Shipping $subject,
         \Closure $proceed,
         $carrierCode,
         $request
     ) {
-        if ($carrierCode == 'standard') {
+        
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $cart = $objectManager->get('\Magento\Checkout\Model\Cart');
+
+        $items = $cart->getQuote()->getAllItems();
+        $qty = 1;
+        
+        foreach ($items as $item) {
+
+            $prodId = $item->getProductId();
+            $_objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $product = $_objectManager->get('\Magento\Catalog\Model\Product')->load($prodId);
+
+            $sourceItems = $this->getSourceItemsBySku->execute($product->getSku());
+
+            foreach ($sourceItems as $sourceItemId => $sourceItem) {
+                if ($sourceItem->getSourceCode() == 'SWHS') {
+                    $qty = $qty * $sourceItem->getQuantity();
+                }
+            }
+        }
+        
+        if ($carrierCode == 'standard' & $qty = 0) {
             return false;
+        } else {
+            return true;
         }
         return $proceed($carrierCode, $request);
     }
+   
 }
