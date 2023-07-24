52a3d269a41e7f4e0787ffb03181a751e12a658f
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-07-24 19:30:39 +0800

    Test Disable Shipping

--- app/code/Digidirect/Checkout/Plugin/Model/Shipping.php
+++ app/code/Digidirect/Checkout/Plugin/Model/Shipping.php
@@ -3,21 +3,12 @@
 namespace Digidirect\Checkout\Plugin\Model;
 
 use Magento\Inventory\Model\SourceItem\Command\GetSourceItemsBySku;
-use Magento\Checkout\Model\Session;
-use Digidirect\CollectAbstractEntity\Helper\Places;
-use Digidirect\Collect\Helper\Data;
 
 class Shipping {
     
     public function __construct(
-        Session $checkoutSession,
-        Places $placesHelper,
-        Data $collectHelper,
         GetSourceItemsBySku $getSourceItemsBySku
     ) {
-        $this->checkoutSession = $checkoutSession;
-        $this->placesHelper = $placesHelper;
-        $this->collectHelper = $collectHelper;
         $this->getSourceItemsBySku = $getSourceItemsBySku;
     }
        
@@ -34,7 +25,7 @@ class Shipping {
         $items = $cart->getQuote()->getAllItems();
         $qty = 1;
         
-        foreach ($items as $item) {
+        /*foreach ($items as $item) {
 
             $prodId = $item->getProductId();
             $_objectManager = \Magento\Framework\App\ObjectManager::getInstance();
@@ -47,12 +38,11 @@ class Shipping {
                     $qty = $qty * $sourceItem->getQuantity();
                 }
             }
-        }
+        }*/
         
-        if ($carrierCode == 'standard' && $qty == 0) {
+        //if ($carrierCode == 'standard' && $qty == 0) {
+        if ($carrierCode == 'standard') {
             return false;
-        } else {
-            return true;
         }
         return $proceed($carrierCode, $request);
     }
