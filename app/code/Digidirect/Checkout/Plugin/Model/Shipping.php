cd1ec96a265b5e3c0de984fb0b0c23d8e06040d5
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-07-24 19:05:21 +0800

    Test Disable Shipping

--- app/code/Digidirect/Checkout/Plugin/Model/Shipping.php
+++ app/code/Digidirect/Checkout/Plugin/Model/Shipping.php
@@ -43,8 +43,6 @@ class Shipping {
             $sourceItems = $this->getSourceItemsBySku->execute($product->getSku());
 
             foreach ($sourceItems as $sourceItemId => $sourceItem) {
-                echo $this->console_log('Source: ' . $sourceItem->getSourceCode());
-                echo $this->console_log('Qty: ' . $sourceItem->getQuantity());
                 if ($sourceItem->getSourceCode() == 'SWHS') {
                     $qty = $qty * $sourceItem->getQuantity();
                 }
@@ -57,16 +55,6 @@ class Shipping {
             return true;
         }
         return $proceed($carrierCode, $request);
-    }
-    
-    function console_log($output, $with_script_tags = true) {
-        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
-            ');';
-        if ($with_script_tags) {
-            $js_code = '<script>' . $js_code . '</script>';
-        }
-        echo $js_code;
-        
     }
    
 }
