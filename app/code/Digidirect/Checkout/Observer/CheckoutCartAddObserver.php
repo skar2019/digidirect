41eb1783a9f1b4651122752e6b6d5116aef81290
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-07-27 10:52:32 +0800

    Test API Request On Add To Cart

--- app/code/Digidirect/Checkout/Observer/CheckoutCartAddObserver.php
+++ app/code/Digidirect/Checkout/Observer/CheckoutCartAddObserver.php
@@ -38,8 +38,8 @@ class CheckoutCartAddObserver extends AbstractHelper implements \Magento\Framewo
         $getTokenResult = $getTokenCurl->getBody();
 
         $getTokenJson = $this->jsonSerializer->unserialize($getTokenResult);
-        /*echo $this->console_log('$getTokenJson: ' . json_encode($getTokenJson));
-        $webSignUpUrl = 'https://digidirect2022.my.salesforce.com/services/apexrest/WebSignup';
+        echo $this->console_log('$getTokenJson: ' . json_encode($getTokenJson));
+        /*$webSignUpUrl = 'https://digidirect2022.my.salesforce.com/services/apexrest/WebSignup';
         $webSignUpParams = ["email"=>$email,"source"=>"Web"];
 
         $webSignUpCurl = $this->curl;
