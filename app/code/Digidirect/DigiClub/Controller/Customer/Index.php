ddf362ec6eeb1c3d884723ed9b7548f66504dc01
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-08-07 14:44:55 +0800

    Test digiClub Module

--- app/code/Digidirect/DigiClub/Controller/Customer/Index.php
+++ app/code/Digidirect/DigiClub/Controller/Customer/Index.php
@@ -4,9 +4,31 @@ namespace Digidirect\DigiClub\Controller\Customer;
 
 class Index extends \Magento\Framework\App\Action\Action 
 {
+    protected $_customerSession;
+    
+    public function __construct
+    (
+        \Magento\Customer\Model\Session $customerSession,
+    ) {
+        $this->_customerSession = $customerSession;
+    }
+    
     public function execute() 
     {
-        $this->_view->loadLayout();
+        $layout = $this->_view->loadLayout();
+        $layout->getlayout()->getBlock('digiclub-tab')->setData('isDigiClub', $this->getGroupId());
         $this->_view->renderLayout();
+    }
+    
+    public function getGroupId()
+    {
+        if($this->_customerSession->isLoggedIn()):
+            $customerGroup = $this->_customerSession->getCustomer()->getGroupId();
+            if($customerGroup == 10) {
+                return true;
+            } else {
+                return false;
+            }
+        endif;
     }
 }
