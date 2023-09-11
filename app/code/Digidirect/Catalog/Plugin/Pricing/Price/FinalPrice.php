6d0e6594efda0b8b9248220e66eda87817592356
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-09-11 11:16:29 +0800

    Plugin Test For Wiser Price

--- app/code/Digidirect/Catalog/Plugin/Pricing/Price/FinalPrice.php
+++ app/code/Digidirect/Catalog/Plugin/Pricing/Price/FinalPrice.php
@@ -2,15 +2,39 @@
 
 namespace Digidirect\Catalog\Plugin\Pricing\Price;
 
+use Magento\Catalog\Helper\Data as CatalogHelper;
+use Magento\Framework\Registry;
+
 class FinalPrice
 {
-    /**
-     * @param \Magento\Catalog\Pricing\Price\FinalPrice $subject
-     * @param float $result
-     * @return float
-     */
+    protected $registry;
+    
+    protected $catalogHelper;
+
+    public function __construct(
+        CatalogHelper $catalogHelper,
+        Registry $registry,
+        array $data = []
+    ) {
+        $this->catalogHelper = $catalogHelper;
+        $this->registry = $registry;
+        parent::__construct($context, $data);
+    }
+    
     public function afterGetValue(\Magento\Catalog\Pricing\Price\FinalPrice $subject, $result)
     {
-        return $result * 2;
+        $product = $this->registry->registry('current_product');
+        if ($product) {
+            $price = $this->catalogHelper->getPrice($product);
+            $wiserPrice = $this->catalogHelper->getWiserPrice($product);
+            
+            if ($wiserPrice != 0 || !empty($wiserPrice)) {
+                if ($wiserPrice < $price) {
+                    $result = $wiserPrice;
+                }
+            }
+            
+            return $result;
+        }
     }
 }
