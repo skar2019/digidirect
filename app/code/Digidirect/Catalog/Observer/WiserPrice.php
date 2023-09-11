e4dee00a885152f1b9174640ea8dd9397a1bbc0d
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-09-11 14:25:51 +0800

    Test Wiser Price Add To Cart

---
+++ app/code/Digidirect/Catalog/Observer/WiserPrice.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Digidirect\Catalog\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\App\RequestInterface;
+
+class WiserPrice implements ObserverInterface
+{
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+        $item = $observer->getEvent()->getData('quote_item');            
+        $item = ( $item->getParentItem() ? $item->getParentItem() : $item );
+        
+        $product = $item->getProduct();
+        
+        $price = $product->getData('price');
+        $wiserPrice = $product->getData('wiser_price');
+
+        if ($wiserPrice != 0 || !empty($wiserPrice)) {
+            if ($wiserPrice < $price) {
+                $finalPrice = $wiserPrice;
+            } else {
+                $finalPrice = $price;
+            }
+        }
+        
+        $item->setCustomPrice($finalPrice);
+        $item->setOriginalCustomPrice($finalPrice);
+        $product->setIsSuperMode(true);
+    }
+
+}
