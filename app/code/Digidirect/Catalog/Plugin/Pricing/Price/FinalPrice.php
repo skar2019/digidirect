a80bb2a26fdcb05b2dfb002b7cebfefd96f235a9
ronde <ronde@DESKTOP-43FC5AG>
2023-09-11 20:28:22 +0800

    Wiser Price

---
+++ app/code/Digidirect/Catalog/Plugin/Pricing/Price/FinalPrice.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Digidirect\Catalog\Plugin\Pricing\Price;
+
+use Magento\Catalog\Helper\Data as CatalogHelper;
+use Magento\Framework\Registry;
+
+class FinalPrice
+{
+    protected $registry;
+    
+    protected $catalogHelper;
+
+    public function __construct(
+        CatalogHelper $catalogHelper,
+        Registry $registry
+    ) {
+        $this->catalogHelper = $catalogHelper;
+        $this->registry = $registry;
+    }
+    
+    public function afterGetValue(\Magento\Catalog\Pricing\Price\FinalPrice $subject, $result)
+    {
+        $product = $this->registry->registry('current_product');
+        if ($product) {
+            $price = $product->getData('price');
+            $wiserPrice = $product->getData('wiser_price');
+            
+            if ($wiserPrice != 0 || !empty($wiserPrice)) {
+                if ($wiserPrice < $price) {
+                    $result = $wiserPrice;
+                }
+            }
+            
+            return $result;
+        }
+    }
+}
