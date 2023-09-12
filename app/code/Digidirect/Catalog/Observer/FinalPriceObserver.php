25372cc6e0112c41b66fcb04355bd8d4027ea8be
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-09-12 20:38:12 +0800

    Wiser Price Test Observer

---
+++ app/code/Digidirect/Catalog/Observer/FinalPriceObserver.php
@@ -0,0 +1,26 @@
+<?php
+namespace Jesadiya\MarketingPrice\Observer;
+
+class ProcessFinalPriceObserver
+{
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $product = $observer->getEvent()->getProduct();
+        $price = $product->getData('final_price');
+        $wiserPrice = $product->getData('wiser_price');
+
+        if ($wiserPrice != 0 && !empty($wiserPrice)) {
+            if ($wiserPrice < $price) {
+                $finalPrice = $wiserPrice;
+            } else {
+                $finalPrice = $price;
+            }
+        } else {
+            $finalPrice = $price;
+        }
+        
+        $product->setFinalPrice($finalPrice);
+
+        return $this;
+    }
+}
