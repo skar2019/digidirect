1dbabf5c4459e71ea5204ab4cd3917cf09f09bf5
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-09-12 14:20:08 +0800

    Wiser Price Test

--- app/code/Magezon/Builder/Block/AbstractProduct.php
+++ app/code/Magezon/Builder/Block/AbstractProduct.php
@@ -133,39 +133,14 @@ class AbstractProduct extends \Magezon\Builder\Block\Element
         if (!isset($arguments['zone'])) {
             $arguments['zone'] = $renderZone;
         }
-        $arguments['price_id'] = isset($arguments['price_id'])
-            ? $arguments['price_id']
-            : 'old-price-' . $product->getId() . '-' . $priceType;
-        $arguments['include_container'] = isset($arguments['include_container'])
-            ? $arguments['include_container']
-            : true;
-        $arguments['display_minimal_price'] = isset($arguments['display_minimal_price'])
-            ? $arguments['display_minimal_price']
-            : false;
-        $arguments['use_link_for_as_low_as'] = isset($arguments['use_link_for_as_low_as'])
-            ? $arguments['use_link_for_as_low_as']
-            : false;
 
-            /** @var \Magento\Framework\Pricing\Render $priceRender */
+        /** @var \Magento\Framework\Pricing\Render $priceRender */
         $priceRender = $this->getLayout()->getBlock('product.price.render.default');
-        if (!$priceRender) {
-            $priceRender = $this->getLayout()->createBlock(
-                \Magento\Framework\Pricing\Render::class,
-                'product.price.render.default',
-                [
-                    'data' => [
-                        'price_render_handle'    => 'catalog_product_prices',
-                        'use_link_for_as_low_as' => false,
-                        'display_minimal_price'  => false
-                    ]
-                ]
-            );
+        $price = '';
+
+        if ($priceRender) {
+            $price = $priceRender->render($priceType, $product, $arguments);
         }
-        $price = $priceRender->render(
-            \Magento\Catalog\Pricing\Price\FinalPrice::PRICE_CODE,
-            $product,
-            $arguments
-        );
         return $price;
     }
 
