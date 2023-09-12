abe45068201490322ea61ac78e93c582da8e0272
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-09-12 19:17:03 +0800

    Wiser Price Test

--- app/code/Digidirect/Catalog/Plugin/Pricing/Price/FinalPrice.php
+++ app/code/Digidirect/Catalog/Plugin/Pricing/Price/FinalPrice.php
@@ -22,18 +22,17 @@ class FinalPrice
     public function afterGetValue(\Magento\Catalog\Pricing\Price\FinalPrice $subject, $result)
     {
         $product = $this->registry->registry('current_product');
-        $price = $product->getPriceInfo()->getPrice('final_price')->getValue();
-        //$wiserPrice = $product->getData('wiser_price');
         
-        /*if ($product) {
+        if ($product) {
+            $price = $product->getData('final_price');
+            $wiserPrice = $product->getData('wiser_price');
+            
             if ($wiserPrice != 0 && !empty($wiserPrice)) {
                 if ($wiserPrice < $price) {
                     $result = $wiserPrice;
                 }
             }
-        }*/
-        
-        $result = $price;
+        }
             
         return $result;
     }
