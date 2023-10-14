e14b31c64a2413b6e1de48c3c12b274f1141dc4c
Clint <clint@kayweb.com.au>
2023-10-14 12:27:31 +0800

    final price issue.

--- app/code/Digidirect/Pronto/Helper/ProductEntHelper.php
+++ app/code/Digidirect/Pronto/Helper/ProductEntHelper.php
@@ -291,11 +291,11 @@ class ProductEntHelper extends AbstractHelper
                 }
             }
 
-//            $final_price = $product->getPriceInfo()->getPrice('final_price')->getValue();
-//            $final_price2 = $product->getFinalPrice();
+            $final_price = $product->getPriceInfo()->getPrice('final_price')->getValue();
+            $final_price2 = $product->getFinalPrice();
             $final_price3 = $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
-//            echo "final price ".$final_price."<br/>";
-//            echo "final price2 ".$final_price2."<br/>";
+            echo "final price ".$final_price."<br/>";
+            echo "final price2 ".$final_price2."<br/>";
             echo $product->getSku()." final price3 ".$final_price3."<br/>";
             $stockC = "Other";
             $stockcondition = $product->getCustomAttribute('stock_condition');
