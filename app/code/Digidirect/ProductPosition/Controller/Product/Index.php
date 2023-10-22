24be091fe0be6293d7f9cb8ae30235d468dd05ac
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-10-22 14:35:01 +0800

    Test Product Position

--- app/code/Digidirect/ProductPosition/Controller/Product/Index.php
+++ app/code/Digidirect/ProductPosition/Controller/Product/Index.php
@@ -3,7 +3,7 @@
 namespace Digidirect\ProductPosition\Controller\Product;
 
 use Magento\Catalog\Model\Product;
-use Magento\Catalog\Model\ResourceModel\ProductCollectionFactory as ProductCollectionFactory;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Catalog\Model\Category\Factory;
 use Magento\Catalog\Model\ResourceModel\Category as CategoryResource;
