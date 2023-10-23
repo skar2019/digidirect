b76ecf9a132a02198388fcb065634e51357d741a
Clint <clint@kayweb.com.au>
2023-10-23 11:15:16 +0800

    itoris final price

--- app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
+++ app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
@@ -68,9 +68,13 @@ class Bootstrap extends \Magento\Framework\App\Action\Action
         $product = $this->productRepository->getById($productId,false, $storeId);
 
         $resultJson = $this->resultJsonFactory->create();
+        $final_price = $product->getPriceInfo()->getPrice('final_price')->getValue();
+        $final_price2 = $product->getFinalPrice();
+        $final_price3 = $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
+
         $response = [
             'product_name' => $product->getName(),
-            'final_price' => $product->getFinalPrice(),
+            'final_price' => $final_price3,//$product->getFinalPrice(),
             'check_render_link' => 1,//($this->calculateRenderLink()) ? 1 : '',
         ];
 
