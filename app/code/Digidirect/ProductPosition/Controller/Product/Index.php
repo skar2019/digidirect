05d4850ca699802ce10c8e40bc1fdcee819b07e5
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-10-23 08:28:44 +0800

    Test Product Position

--- app/code/Digidirect/ProductPosition/Controller/Product/Index.php
+++ app/code/Digidirect/ProductPosition/Controller/Product/Index.php
@@ -2,11 +2,76 @@
 
 namespace Digidirect\ProductPosition\Controller\Product;
 
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Catalog\Model\ResourceModel\Category as CategoryResource;
+
 class Index extends \Magento\Framework\App\Action\Action
 {
+    /**
+     * @var ProductCollectionFactory
+     */
+    private $productCollectionFactory;
+     /**
+     * @var StoreManagerInterface
+     */
+    
+    protected $storeManager;
+
+    /**
+     * @var CategoryFactory
+     */
+    protected $categoryFactory;
+
+    /**
+     * @var CategoryResource
+     */
+    protected $categoryResource;
+    
+    
+    protected $_productFactory;
+    
+    
+    protected $logger;
+    
+
+    public function __construct(
+        ProductCollectionFactory $productCollectionFactory,  
+        StoreManagerInterface $storeManager,
+        CategoryFactory $categoryFactory,
+        CategoryResource $categoryResource,
+        \Magento\Catalog\Model\ProductFactory $productFactory
+    )
+    {
+        $this->productCollectionFactory = $productCollectionFactory;
+        $this->categoryFactory = $categoryFactory;
+        $this->categoryResource = $categoryResource;
+        $this->storeManager = $storeManager;
+        $this->_productFactory = $productFactory;
+    }
 
     public function execute()
     {
-        
+        $product = $this->_productFactory->create()->load(3793);
+        // Get the category ID of the new product.
+        $categoryIds = $product->getCategoryIds();
+        // Get the new product position.
+        $newPosition = 5;
+        // Change the product position.
+        //$this->changeProductPosition($categoryIds, $product->getId(), $newPosition);
+    }
+    
+    private function changeProductPosition($categoryIds, $productId, $newPosition)
+    {
+        foreach($categoryIds as $categoryId)
+        {
+             $category = $this->categoryFactory->create()->load($categoryId);
+             $products = $category->getProductsPosition();
+             $products[$productId] = $newPosition;
+             $category->setPostedProducts($products);
+             $category->save();
+        }
     }
 }
