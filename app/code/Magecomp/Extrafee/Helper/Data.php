4206d313ae303959e98828740b7b3d06b48b92fd
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-10-26 11:40:32 +0800

    Remove Marketplacer Shipping

--- app/code/Magecomp/Extrafee/Helper/Data.php
+++ app/code/Magecomp/Extrafee/Helper/Data.php
@@ -18,13 +18,13 @@ class Data extends AbstractHelper
     
     protected $logger;
     
-    public function __construct(
+    /*public function __construct(
         \Magento\Checkout\Model\Cart $cart,
         \Psr\Log\LoggerInterface $logger
     ){
         $this->cart = $cart;
         $this->logger = $logger;
-    }
+    }*/
 
     /**
      * @return mixed
@@ -42,10 +42,10 @@ class Data extends AbstractHelper
      */
     public function getExtrafee()
     {
-        $items = $this->cart->getQuote()->getAllItems();
+        /*$items = $this->cart->getQuote()->getAllItems();
         foreach($items as $item) {
             $this->logger->info('Seller: ' . $item->getAttributeText('marketplacer_seller'));
-        }
+        }*/
         
         return 15;
         
