de8ae2be75e91c088d7e1fce91a9f0f7983f6ee8
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-10-30 09:04:41 +0800

    Sellers Items List

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/summary/cart-items.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/summary/cart-items.js
@@ -79,10 +79,14 @@ define([
                 this.setItems(items);
             }.bind(this));
             
-            console.log($('.items-in-cart .product-item .seller-name').length);
-            $('.items-in-cart .product-item .seller-name').each(function(){
-                console.log("Test each seller!");
+            $(window).on('load', function(){
+                console.log($('.items-in-cart .product-item .seller-name').length);
+            
+                $('.items-in-cart .product-item .seller-name').each(function(){
+                    console.log("Test each seller!");
+                });
             });
+            
             /*console.log("JS for seller!");
             var elements = document.getElementsByClassName("seller-name");
             var elementsArr = Array.from(elements);
