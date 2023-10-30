ef0eee93905337532f62269b4d747c6ea2f81a8d
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-10-30 23:04:32 +0800

    Sellers Items List

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
@@ -24,7 +24,7 @@ define([
                     console.log("seller: " + seller);
                     if (sellerPerItem === seller) {
                         console.log("sellerPerItem === seller");
-                        $(thisSellerPerItem).closest('.product-item').insertAfter(thisSeller);
+                        $(thisSellerPerItem).parent().insertAfter(thisSeller);
                     }
                 });
             });
