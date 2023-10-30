c13771876e63583fbd36d6e0401b1a60f47ea8a0
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-10-30 21:29:56 +0800

    Sellers Items List

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
@@ -11,8 +11,20 @@ define([
         isVisible: function () {
             
             console.log("cms-payments");
-            console.log("seller-name: " + $('.items-in-cart .product-item .seller-name').length);
-            console.log("seller: " + $('.items-in-cart .seller').length);
+            console.log("seller-name: " + $('.opc-sidebar .items-in-cart .product-item .seller-name').length);
+            console.log("seller: " + $('.opc-sidebar .items-in-cart .seller').length);
+            
+            $('.opc-sidebar .items-in-cart .product-item .seller-name').each(function(){
+                var sellerPerItem = $(this).html();
+                console.log("sellerPerItem: " + sellerPerItem);
+                $('.opc-sidebar .items-in-cart .seller').each(function(){
+                    var seller = $(this).html();
+                    console.log("seller: " + seller);
+                    if (sellerPerItem == seller) {
+                        $(sellerPerItem).closest('.product-item').insertAfter(seller);
+                    }
+                });
+            });
 
             return cmsPayments;
         }
