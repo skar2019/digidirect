b200e79c9a5b230bfb19d8abccea11c034732467
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-10-30 20:38:14 +0800

    Sellers Items List

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
@@ -10,8 +10,8 @@ define([
         isVisible: function () {
             
             console.log("cms-payments");
-            console.log("seller-name: " + $('.items-in-cart .product-item .seller-name').length);
-            console.log("seller: " + $('.items-in-cart .seller').length);
+            //console.log("seller-name: " + $('.items-in-cart .product-item .seller-name').length);
+            //console.log("seller: " + $('.items-in-cart .seller').length);
 
             return cmsPayments;
         }
