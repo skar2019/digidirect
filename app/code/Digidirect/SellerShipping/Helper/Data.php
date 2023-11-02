7c630e6ccbc89ff1e5f4f637e48c6ae32f90373e
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-02 14:32:23 +0800

    Sellers Shipping

--- app/code/Digidirect/SellerShipping/Helper/Data.php
+++ app/code/Digidirect/SellerShipping/Helper/Data.php
@@ -120,49 +120,6 @@ class Data extends AbstractHelper
             }
             //$this->logger->info('getProductId: ' . $product->getId());
         }
-        $this->logger->info('getSellers()');
-        
-        $sellerTotalShipping = 0;
-        $sellersArray = [];
-        
-        /*foreach($sellers as $seller) {
-            
-            $sellerShipping = 0;
-            $sellerTotal = 0;
-            
-            $this->logger->info('getSellers() sellers');
-            
-            foreach($items as $item) {
-                $product = $this->productFactory->create()->load($item->getProductId());
-                $this->logger->info('getFinalPrice: ' . $product->getFinalPrice());
-                $finalPrice = $product->getFinalPrice();
-                $itemSeller = $product->getAttributeText('marketplacer_seller');
-                    
-                if ($seller == $itemSeller) {
-                    $sellerTotal += $finalPrice;
-                }
-                
-                $this->logger->info('getSellers() items');
-            }
-            $this->logger->info($seller . ': ' . $sellerTotal);
-
-            if ($sellerTotal < 99) {
-                $sellerShipping = 10;
-            }
-            
-            $sellerTotalShipping += $sellerShipping;
-            
-            $sellerArray = [$seller,$sellerTotalShipping];
-            
-            //$this->logger->info(json_encode($sellerArray));
-            
-            if (!in_array($seller, $sellersArray))  {
-                //array_push($sellersArray, [$seller,$sellerTotalShipping]);
-                array_push($sellersArray, $seller);
-            }
-        }*/
-        
-        //$this->logger->info('sellers: ' . json_encode($sellersArray));
         
         return $sellers;
         
