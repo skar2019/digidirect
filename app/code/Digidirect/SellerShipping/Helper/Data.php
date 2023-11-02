b6076b07ca1b7b8fdf93db6497e8d23728c37e7a
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-02 17:43:14 +0800

    Seller Shipping

--- app/code/Digidirect/SellerShipping/Helper/Data.php
+++ app/code/Digidirect/SellerShipping/Helper/Data.php
@@ -88,7 +88,7 @@ class Data extends AbstractHelper
                     $sellerTotal += $finalPrice;
                 }
             }
-            $this->logger->info($seller . ': ' . $sellerTotal);
+            $this->logger->info('getSellerShipping ' . $seller . ': ' . $sellerTotal);
             
             if ($sellerTotal < 99) {
                 $sellerShipping = 10;
