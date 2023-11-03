dd49f0bf2c73c531637e2a9e812e94bd80c1540e
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-03 13:17:25 +0800

    Test Fix digiClub Session

--- app/code/Digidirect/DigiClub/Block/Customer.php
+++ app/code/Digidirect/DigiClub/Block/Customer.php
@@ -11,16 +11,9 @@ class Customer {
     protected $customerSession;
         
     public function __construct(
-        \Magento\Customer\Model\Session $customer,
         Session $session
     ) {        
-        $this->customer = $customer;
         $this->customerSession = $session;
-    }
-    
-    public function getCustomer() {
-        $customer = $this->customer;
-        return $customer;
     }
     
     public function getCustomerSession() {
