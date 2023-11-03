ba2d9b2c907b2b3ccda815c7813ecbfb6d664ce4
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-03 09:57:24 +0800

    Seller Shipping

--- app/code/Digidirect/SellerShipping/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    "map": {
-        "*": {
-            'Magento_Checkout/js/model/shipping-save-processor/default': 'Digidirect_SellerShipping/js/model/shipping-save-processor/default'
-        }
-    }
-};
