51cbdd69cd54c0b919391a0d83bf2dd180ec3637
ronde <ronde@DESKTOP-43FC5AG>
2023-11-03 14:17:28 +0800

    Test Fix digiClub Session

---
+++ app/code/Digidirect/DigiClub/Block/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Digidirect\Digiclub\Block;
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\View\Element\Template;
+
+class Customer extends Template {
+    
+    protected $customer;
+    
+    protected $customerSession;
+        
+    public function __construct(
+        Session $session
+    ) {        
+        $this->customerSession = $session;
+    }
+    
+    public function getCustomerSession() {
+        return $this->customerSession;
+    }
+    
+}
