2abdc3d964a3263f4d2beb20ade449e217deb9a1
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-03 18:54:34 +0800

    Test Fix digiClub Session

--- app/code/Digidirect/DigiClub/Block/Customer.php
+++ app/code/Digidirect/DigiClub/Block/Customer.php
@@ -4,21 +4,38 @@ namespace Digidirect\Digiclub\Block;
 
 use Magento\Customer\Model\Session;
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\Session\SessionManager;
 
 class Customer extends Template {
     
-    protected $customer;
-    
     protected $customerSession;
+    
+    protected $sessionManager;
+    
+    protected $logger;
         
     public function __construct(
-        Session $session
+        Session $session,
+        SessionManager $sessionManager,
+        \Psr\Log\LoggerInterface $logger
     ) {        
         $this->customerSession = $session;
+        $this->sessionManager = $sessionManager;
+        $this->logger = $logger;
     }
     
     public function getCustomerSession() {
+        $this->logger->info('account firstname: ' . $this->customerSession->getData("firstname"));
+        $this->logger->info('account lastname: ' . $this->customerSession->getData("lastname"));
+        $this->logger->info('account email: ' . $this->customerSession->getEmail());
+        $this->logger->info('account contact_number: ' . $this->customerSession->getData("contact_number"));
+        $this->logger->info('account dob: ' . $this->customerSession->getData("dob"));
         return $this->customerSession;
+    }
+    
+    public function getSessionManager() {
+        $this->logger->info('account Session ID: ' . $this->sessionManager->getSessionId());
+        return $this->sessionManager;
     }
     
 }
