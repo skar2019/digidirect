323c86f1224b578633510dd5a24bcd945e9d2274
ronde <ronde@DESKTOP-43FC5AG>
2023-11-03 16:41:26 +0800

    Test Fix digiClub Session

--- app/code/Digidirect/DigiClub/Block/Customer.php
+++ app/code/Digidirect/DigiClub/Block/Customer.php
@@ -13,20 +13,31 @@ class Customer extends Template {
     protected $customerSession;
     
     protected $sessionManager;
+    
+    protected $logger;
         
     public function __construct(
         Session $session,
-        SessionManager $sessionManager
+        SessionManager $sessionManager,
+        \Psr\Log\LoggerInterface $logger
     ) {        
         $this->customerSession = $session;
         $this->sessionManager = $sessionManager;
+        $this->logger = $logger;
     }
     
     public function getCustomerSession() {
+        $logger->info('account Session ID: ' . $this->customerSession->getSessionId());
+        $logger->info('account firstname: ' . $this->customerSession->getData("firstname"));
+        $logger->info('account lastname: ' . $this->customerSession->getData("lastname"));
+        $logger->info('account email: ' . $this->customerSession->getEmail());
+        $logger->info('account contact_number: ' . $this->customerSession->getData("contact_number"));
+        $logger->info('account dob: ' . $this->customerSession->getData("dob"));
         return $this->customerSession;
     }
     
     public function getSessionManager() {
+        $logger->info('account Session ID: ' . $this->sessionManager->getSessionId());
         return $this->sessionManager;
     }
     
