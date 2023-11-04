6e2a06a780f8f20a7d25e0086d8e289f99aa6aa9
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-04 11:04:09 +0800

    Test Fix Seller Shipping

--- app/code/Digidirect/SellerShipping/Plugin/Quote/Address/RatePlugin.php
+++ app/code/Digidirect/SellerShipping/Plugin/Quote/Address/RatePlugin.php
@@ -22,7 +22,7 @@ class RatePlugin
         $this->logger->info('afterImportShippingRate');
         if ($rate instanceof \Magento\Quote\Model\Quote\Address\RateResult\Method) {
             $this->logger->info('getCode: ' . $result->getCode());
-            if($result->getCode() == 'standard') {
+            if($result->getCode() == 'standard_standard') {
                 $result->setPrice($this->helperData->getDigiShipping());
                 $this->logger->info('standard afterImportShippingRate: ' . $this->helperData->getDigiShipping());
             }
