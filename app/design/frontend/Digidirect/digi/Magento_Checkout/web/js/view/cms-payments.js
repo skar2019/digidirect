8539d6abdbcff6a2eda58a75f8aea401c4f7d372
ronde <ronde@DESKTOP-43FC5AG>
2023-11-06 14:26:53 +0800

    Seller Shipping Module On Checkout

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
@@ -1,6 +1,7 @@
 define([
+    'jquery',
     'uiComponent'
-], function (Component) {
+], function ($, Component) {
     'use strict';
 
     var cmsPayments = window.cmsPayments;
@@ -8,6 +9,37 @@ define([
     return Component.extend({
         cmsData: cmsPayments ? cmsPayments.content : '',
         isVisible: function () {
+            
+            console.log("cms-payments");
+            console.log("seller-name: " + $('.opc-sidebar .items-in-cart .product-item .seller-name').length);
+            console.log("seller: " + $('.opc-sidebar .items-in-cart .seller').length);
+            
+            $('.opc-sidebar .seller-name').each(function(){
+                var thisSellerPerItem = $(this);
+                var sellerPerItem = $(this).html();
+                console.log("sellerPerItem: " + sellerPerItem);
+                
+                var subStrSpi = sellerPerItem.substring(
+                    sellerPerItem.indexOf("-->") + 3, 
+                    sellerPerItem.lastIndexOf("<!--")
+                );
+        
+                console.log("subStrSpi: " + subStrSpi);
+                
+                $('.opc-sidebar .seller').each(function(){
+                    var thisSeller = $(this);
+                    var seller = $(this).html();
+                    console.log("seller: " + seller);
+                    if (subStrSpi === seller) {
+                        /*if(thisSeller.parents().hasClass('opc-sidebar') && thisSellerPerItem.parents().hasClass('opc-sidebar')) {
+                            $(thisSellerPerItem).parent().insertAfter(thisSeller.parent());
+                        } else {
+                            
+                        }*/
+                    }
+                });
+            });
+
             return cmsPayments;
         }
     });
