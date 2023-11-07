ad57f3c23357825bd1c44104ab1f823636b9abad
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-07 14:05:55 +0800

    Test Seller Item Fix

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
@@ -14,30 +14,26 @@ define([
             console.log("seller-name: " + $('.opc-sidebar .items-in-cart .product-item .seller-name').length);
             console.log("seller: " + $('.opc-sidebar .items-in-cart .seller').length);
             
-            $('.opc-sidebar .seller-name').each(function(){
-                var thisSellerPerItem = $(this);
-                var sellerPerItem = $(this).html();
-                console.log("sellerPerItem: " + sellerPerItem);
-                
-                var subStrSpi = sellerPerItem.substring(
-                    sellerPerItem.indexOf("-->") + 3, 
-                    sellerPerItem.lastIndexOf("<!--")
-                );
-        
-                console.log("subStrSpi: " + subStrSpi);
-                
-                $('.opc-sidebar .seller').each(function(){
+            $('.opc-sidebar .seller').each(function(){
                     var thisSeller = $(this);
                     var seller = $(this).html();
                     console.log("seller: " + seller);
-                    if (subStrSpi === seller) {
-                        /*if(thisSeller.parents().hasClass('opc-sidebar') && thisSellerPerItem.parents().hasClass('opc-sidebar')) {
-                            $(thisSellerPerItem).parent().insertAfter(thisSeller.parent());
-                        } else {
-                            
-                        }*/
-                    }
-                });
+                    
+                    $('.opc-sidebar .seller-name').each(function(){
+                        var thisSellerPerItem = $(this);
+                        var sellerPerItem = $(this).html();
+                        console.log("sellerPerItem: " + sellerPerItem);
+
+                        var subStrSpi = sellerPerItem.substring(
+                            sellerPerItem.indexOf("-->") + 3, 
+                            sellerPerItem.lastIndexOf("<!--")
+                        );
+                        console.log("subStrSpi: " + subStrSpi);
+                        
+                        if (subStrSpi === seller) {
+                            thisSellerPerItem.parent().insertAfter(thisSeller.parent());
+                        }
+                    });
             });
 
             return cmsPayments;
