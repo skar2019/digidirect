1f7c599078872ecaabe7a1be4e4044c2b17ae1fd
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-07 19:03:03 +0800

    Seller Items

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
@@ -14,12 +14,12 @@ define([
             console.log("seller-name: " + $('.opc-sidebar .items-in-cart .product-item .seller-name').length);
             console.log("seller: " + $('.opc-sidebar .items-in-cart .seller').length);
             
-            $('#opc-sidebar .seller').each(function(){
+            $('.opc-block-summary .seller').each(function(){
                     var thisSeller = $(this);
                     var seller = $(this).html();
                     console.log("seller: " + seller);
                     
-                    $('#opc-sidebar .seller-name').each(function(){
+                    $('.opc-block-summary .seller-name').each(function(){
                         var thisSellerPerItem = $(this);
                         var sellerPerItem = $(this).html();
                         console.log("sellerPerItem: " + sellerPerItem);
