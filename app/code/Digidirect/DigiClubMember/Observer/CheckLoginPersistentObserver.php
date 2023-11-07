7e8ff0813dcd5e6d19a9a5d75ecbe021d6b4b92a
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-07 09:28:53 +0800

    Seller Arrangement

--- app/code/Digidirect/DigiClubMember/Observer/CheckLoginPersistentObserver.php
+++ app/code/Digidirect/DigiClubMember/Observer/CheckLoginPersistentObserver.php
@@ -20,19 +20,15 @@ class CheckLoginPersistentObserver implements ObserverInterface
     
     protected $urlInterface;
     
-    protected $pageConfig;
-    
 
     public function __construct(
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Framework\App\Response\RedirectInterface $redirect,
-        \Magento\Framework\UrlInterface $urlInterface,
-        \Magento\Framework\View\Page\Config $pageConfig
+        \Magento\Framework\UrlInterface $urlInterface
     ) {
         $this->_customerSession = $customerSession;
         $this->redirect = $redirect;
         $this->urlInterface = $urlInterface;
-        $this->pageConfig = $pageConfig;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
@@ -43,10 +39,9 @@ class CheckLoginPersistentObserver implements ObserverInterface
         if(!$this->_customerSession->isLoggedIn() && $routeName == 'digiclubmember') {
             $url = $this->urlInterface->getUrl('digiclubmember/customer/index');
             $login_url = $this->urlInterface
-                ->getUrl('customer/account/login',
+                ->getUrl('customer/account/login?digiclub=true',
                     array('referer' => base64_encode($url))
                 );
-            $this->pageConfig->addBodyClass('digiclub-redirect');
             $this->redirect->redirect($controller->getResponse(), $login_url);
         }
     }
