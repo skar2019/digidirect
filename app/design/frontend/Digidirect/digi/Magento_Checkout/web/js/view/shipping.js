311aca3d75f8a052718f0115be7a6ceebf56e809
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-08 12:02:08 +0800

    Seller Message

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/shipping.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/shipping.js
@@ -237,7 +237,10 @@ define([
         }),
         
         checkSellers: function () {
+            JSON.stringify(obj);
+            console.log('marketplacer_sellers: ' + JSON.stringify(marketplacer_sellers));
             console.log('marketplacer_sellers.length: ' + marketplacer_sellers.length);
+            console.log('marketplacer_sellers[0][0]: ' + marketplacer_sellers[0][0]);
             if (marketplacer_sellers.length == 1 && marketplacer_sellers[0][0] == 'General Seller') {
                 return false;
             } else {
