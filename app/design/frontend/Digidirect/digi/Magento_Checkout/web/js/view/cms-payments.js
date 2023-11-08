600d5820e45aec118c5e3114a391817a1f792400
ronde <ronde@DESKTOP-43FC5AG>
2023-11-08 14:32:01 +0800

    Seller Items On Checkout

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/view/cms-payments.js
@@ -14,28 +14,53 @@ define([
             console.log("seller-name: " + $('.opc-sidebar .items-in-cart .product-item .seller-name').length);
             console.log("seller: " + $('.opc-sidebar .items-in-cart .seller').length);
             
-            $('.opc-sidebar .seller-name').each(function(){
-                var thisSellerPerItem = $(this);
-                var sellerPerItem = $(this).html();
-                console.log("sellerPerItem: " + sellerPerItem);
-                
-                var subStrSpi = sellerPerItem.substring(
-                    sellerPerItem.indexOf("-->") + 3, 
-                    sellerPerItem.lastIndexOf("<!--")
-                );
-        
-                console.log("subStrSpi: " + subStrSpi);
-                
-                $('.opc-sidebar .seller').each(function(){
-                    var thisSeller = $(this);
-                    var seller = $(this).html();
-                    console.log("seller: " + seller);
+            var group = 0;
+            $('.items-in-cart').each(function(){
+                group += 1;
+                console.log('group: ' + group);
+                $(this).addClass('group-' + group);
+            });
+            
+            $('.items-in-cart.active.group-1 .seller').each(function(){
+                var thisSeller = $(this);
+                var seller = $(this).html();
+                console.log("seller: " + seller);
+
+                $('.items-in-cart.active.group-1 .seller-name').each(function(){
+                    var thisSellerPerItem = $(this);
+                    var sellerPerItem = $(this).html();
+                    console.log("sellerPerItem: " + sellerPerItem);
+
+                    var subStrSpi = sellerPerItem.substring(
+                        sellerPerItem.indexOf("-->") + 3, 
+                        sellerPerItem.lastIndexOf("<!--")
+                    );
+                    console.log("subStrSpi: " + subStrSpi);
+
                     if (subStrSpi === seller) {
-                        /*if(thisSeller.parents().hasClass('opc-sidebar') && thisSellerPerItem.parents().hasClass('opc-sidebar')) {
-                            $(thisSellerPerItem).parent().insertAfter(thisSeller.parent());
-                        } else {
-                            
-                        }*/
+                        thisSellerPerItem.parent().insertAfter(thisSeller.parent());
+                    }
+                });
+            });
+            
+            $('.items-in-cart.active.group-2 .seller').each(function(){
+                var thisSeller = $(this);
+                var seller = $(this).html();
+                console.log("seller: " + seller);
+
+                $('.items-in-cart.active.group-2 .seller-name').each(function(){
+                    var thisSellerPerItem = $(this);
+                    var sellerPerItem = $(this).html();
+                    console.log("sellerPerItem: " + sellerPerItem);
+
+                    var subStrSpi = sellerPerItem.substring(
+                        sellerPerItem.indexOf("-->") + 3, 
+                        sellerPerItem.lastIndexOf("<!--")
+                    );
+                    console.log("subStrSpi: " + subStrSpi);
+
+                    if (subStrSpi === seller) {
+                        thisSellerPerItem.parent().insertAfter(thisSeller.parent());
                     }
                 });
             });
