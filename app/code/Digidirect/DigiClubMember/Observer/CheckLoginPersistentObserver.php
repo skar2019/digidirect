595e10d840c82b5d26954c042756af772164f1b0
ronde <ronde@DESKTOP-43FC5AG>
2023-11-09 17:06:33 +0800

    digiClub Member Deals Redirect

--- app/code/Digidirect/DigiClubMember/Observer/CheckLoginPersistentObserver.php
+++ app/code/Digidirect/DigiClubMember/Observer/CheckLoginPersistentObserver.php
@@ -49,15 +49,16 @@ class CheckLoginPersistentObserver implements ObserverInterface
         $controller = $observer->getControllerAction();
         $routeName = $observer->getEvent()->getRequest()->getRouteName();
         $name = $observer->getEvent()->getRequest()->getFullActionName();
-        $this->logger->info('getCurrentUrl: ' . $this->urlInterface->getCurrentUrl());
         $this->logger->info('getBaseUrl: ' . $this->urlInterface->getBaseUrl());
+        $currentUrl = rtrim($this->urlInterface->getCurrentUrl(), '/');
+        $this->logger->info('currentUrl: ' . $currentUrl);
         
         if(!$this->_customerSession->isLoggedIn() && $routeName == 'digiclubmember') {
             $url = $this->urlInterface->getUrl('digiclubmember/customer/index');
             $login_url = $this->urlInterface->getUrl('customer/account/login', ['referer' => base64_encode($url), 'digiclub' => true]);
             $this->redirect->redirect($controller->getResponse(), $login_url);
 
-        } else if (!$this->_customerSession->isLoggedIn() && $this->urlInterface->getCurrentUrl() == $this->urlInterface->getBaseUrl().'/digiclub-member-deals') {
+        } else if (!$this->_customerSession->isLoggedIn() && $currentUrl == $this->urlInterface->getBaseUrl().'digiclub-member-deals') {
             $url = $this->urlInterface->getUrl('digiclub-member-deals');
             $page_url = $this->urlInterface->getUrl('customer/account/login', ['referer' => base64_encode($url)]);
             $this->redirect->redirect($controller->getResponse(), $page_url);
