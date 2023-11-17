0024b5de651efe4b354376f1266df586a4341f97
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-17 12:34:47 +0800

    Test digiDeals

--- app/code/Digidirect/DigiDeals/Block/Product/GridProduct.php
+++ app/code/Digidirect/DigiDeals/Block/Product/GridProduct.php
@@ -53,9 +53,9 @@ class GridProduct extends \Magento\Catalog\Block\Product\AbstractProduct
         //$collection->addAttributeToSelect('*')->addFinalPrice();
         //$collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
         //$collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-        //$collection->getSelect()->where("price_index.final_price < price");
+        //$collection->getSelect()->where("price_index.final_price < price")->limit(3);
         
-        return $collection;
+        return $collection->limit(3);
     }
 
 }
