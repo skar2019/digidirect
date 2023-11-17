1c2a3bb10859ef29f3e087d3f8846cf09a44eeea
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-17 11:54:29 +0800

    Test digiDeals

--- app/code/Digidirect/DigiDeals/Model/Layer.php
+++ app/code/Digidirect/DigiDeals/Model/Layer.php
@@ -59,9 +59,9 @@ class Layer extends \Magento\Catalog\Model\Layer
             $collection = $this->_productCollections[$this->getCurrentCategory()->getId()];
         } else {
             $collection = $this->collectionProvider->getCollection($this->getCurrentCategory());
-            $collection->addAttributeToSelect('*')->addFinalPrice();
-            $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
-            $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+            //$collection->addAttributeToSelect('*')->addFinalPrice();
+            //$collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
+            //$collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
             //$collection->getSelect()->where("price_index.final_price < price");
             $this->prepareProductCollection($collection);
             $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
