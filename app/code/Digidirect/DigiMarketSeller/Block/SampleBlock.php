aa5ef957a2c09b1553c1181ee8b6e165d3733fb6
Dev4 <jireh@kayweb.com.au>
2023-11-17 14:53:55 +0800

    Update digiMarketSeller Module

---
+++ app/code/Digidirect/DigiMarketSeller/Block/SampleBlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Digidirect\DigiMarketSeller\Block;
+
+class SampleBlock extends \Magento\Framework\View\Element\Template
+{
+    public function getWelcomeText()
+    {
+        return 'Hello World';
+    }
+}
