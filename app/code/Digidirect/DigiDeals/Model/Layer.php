1beb14ca74e7daeeeee0338915d1ebd66489a157
ronde <ronde@DESKTOP-43FC5AG>
2023-11-18 11:14:55 +0800

    Test digiDeals

--- app/code/Digidirect/DigiDeals/Model/Layer.php
+++ app/code/Digidirect/DigiDeals/Model/Layer.php
@@ -55,19 +55,15 @@ class Layer extends \Magento\Catalog\Model\Layer
     
     public function getProductCollection()
     {
-        if (isset($this->_productCollections[$this->getCurrentCategory()->getId()])) {
-            $collection = $this->_productCollections[$this->getCurrentCategory()->getId()];
-        } else {
-            $collection = $this->collectionProvider->getCollection($this->getCurrentCategory());
-            $collection->addAttributeToSelect('*')->addFinalPrice();
-            $collection->addCategoriesFilter(['in' => 17]);
-            $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
-            $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-            //$collection->getSelect()->where("price_index.final_price < price");
-            $this->prepareProductCollection($collection);
-            $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
-        }
-        
+        $collection = $this->collectionProvider->getCollection(17);
+        $collection->addAttributeToSelect('*')->addFinalPrice();
+        //$collection->addCategoriesFilter(['in' => 17]);
+        $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
+        $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+        //$collection->getSelect()->where("price_index.final_price < price");
+        $this->prepareProductCollection($collection);
+        $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
+
         return $collection;
     }
     
