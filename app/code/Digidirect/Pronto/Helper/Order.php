c1f58b5b7e246fa853fc661c2471accf6fb9b481
Clint <clint@kayweb.com.au>
2023-11-22 23:47:37 +0800

    seller cost

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -1920,6 +1920,8 @@ class Order extends AbstractHelper
                     if($sell == $seller)
                     {
                         $productSku = $sku;
+                        $costprice = ($price - ( $price * 0.1)); // ex gst
+                        $sellercost = $costprice - ($costprice * 0.1); //ex commission
                         $sellerdata['sales-order']['header']['set-on-status'] = "B";
                         $sellerdata['sales-order']['detail']['line'][$x]['line-type'] = 'SS';
                         $sellerdata['sales-order']['detail']['line'][$x]['stock-code'] = 'ZM00';//$productSku;
@@ -1930,7 +1932,7 @@ class Order extends AbstractHelper
                         $sellerdata['sales-order']['detail']['line'][$x]['backordered'] = $qty;
                         $sellerdata['sales-order']['detail']['line'][$x]['sol-disc-rate'] = $discperc;
                         $sellerdata['sales-order']['detail']['line'][$x]['sol-line-total-inc-tax'] = $total;
-                        $sellerdata['sales-order']['detail']['line'][$x]['item-cost'] = $price;
+                        $sellerdata['sales-order']['detail']['line'][$x]['item-cost'] = $sellercost;
                         $x++;
                         $producttotal += $total;
                     }
