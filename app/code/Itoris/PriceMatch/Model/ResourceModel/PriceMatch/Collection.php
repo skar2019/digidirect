c306fbcc6546a90597624d6a204e8a9ccde8c3a7
ronde <ronde@DESKTOP-43FC5AG>
2023-11-26 23:49:08 +0800

    Fix Price Match Issue

--- app/code/Itoris/PriceMatch/Model/ResourceModel/PriceMatch/Collection.php
+++ app/code/Itoris/PriceMatch/Model/ResourceModel/PriceMatch/Collection.php
@@ -149,7 +149,6 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
                 )',
                 ['product_name'=>'a_varchar.value']
             );
-
         return $this;
     }
 
