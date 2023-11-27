9c280c227f7ecc28f69401152bd7a3d1d6fa0c6c
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-11-27 13:53:08 +0800

    Test Fix Redirect

--- app/code/Digidirect/DigiClubMember/Observer/CheckLoginPersistentObserver.php
+++ app/code/Digidirect/DigiClubMember/Observer/CheckLoginPersistentObserver.php
@@ -70,9 +70,10 @@ class CheckLoginPersistentObserver implements ObserverInterface
             $url = $this->urlInterface->getUrl('digiclub-member-deals');
             $page_url = $this->urlInterface->getUrl('digiclubmember/customer/index', ['referer' => base64_encode($url)]);
             $this->redirect->redirect($controller->getResponse(), $page_url);
-        } else if ($this->_customerSession->isLoggedIn() && $currentUrl == $this->urlInterface->getBaseUrl().'digiclubmember/customer/index' && $isRedirect && $this->_customerSession->getCustomer()->getGroupId() == 10) {
+        } else if ($this->_customerSession->isLoggedIn() && $currentUrl == $this->urlInterface->getBaseUrl().'digiclubmember/customer/index/digiclub/1' && $this->_customerSession->getCustomer()->getGroupId() == 10) {
             $page_url = $this->urlInterface->getUrl('digiclub-member-deals');
             $this->redirect->redirect($controller->getResponse(), $page_url);
+            $this->logger->info('digiclub-member-deals');
         }
     }
 
