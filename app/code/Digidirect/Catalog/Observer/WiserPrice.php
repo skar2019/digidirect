7bf595b95d3b2b90585730ba3984e21dcd327ee0
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-12-01 13:15:16 +0800

    Test Fix digiProtect

--- app/code/Digidirect/Catalog/Observer/WiserPrice.php
+++ app/code/Digidirect/Catalog/Observer/WiserPrice.php
@@ -81,14 +81,14 @@ class WiserPrice implements ObserverInterface
                 $finalPrice = $price;
             }
             
-            //$options = $item->getProduct()->getTypeInstance(true)->getOrderOptions($item->getProduct());
+            $options = $item->getProduct()->getTypeInstance(true)->getOrderOptions($item->getProduct());
             
             /*foreach ($options as $option) {
                 $this->logger->info('Product Option: ' . json_encode($option));
             }*/
             
             $optionPrice = 0;
-            $options = $item->getProductOptions();   
+            //$options = $item->getProductOptions();   
             $this->logger->info('Product Options: ' . json_encode($options));
             
             if (isset($options['options']) && !empty($options['options'])) {        
