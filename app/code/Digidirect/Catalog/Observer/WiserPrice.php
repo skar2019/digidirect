7bcca65733c416770a79d09e62dda49633ba00fd
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-12-04 13:12:10 +0800

    Test Fix digiProtect

--- app/code/Digidirect/Catalog/Observer/WiserPrice.php
+++ app/code/Digidirect/Catalog/Observer/WiserPrice.php
@@ -95,13 +95,14 @@ class WiserPrice implements ObserverInterface
                     //$optionPrice = $option->getPrice();
                 }
             }*/
-            
-            foreach($this->_productOptions as $optionKey => $optionVal) {
-               foreach($optionVal->getValues() as $valuesKey => $valuesVal) {
-                   $this->logger->info('$valuesVal: ' . $valuesVal->getId().' '.$valuesVal->getTitle());
-                   //echo $valuesVal->getId().' '.$valuesVal->getTitle();
-               }
+        }
+        
+        foreach($this->_productOptions as $optionKey => $optionVal) {
+            foreach($optionVal->getValues() as $valuesKey => $valuesVal) {
+                $this->logger->info('$valuesVal: ' . $valuesVal->getId().' '.$valuesVal->getTitle());
+                //echo $valuesVal->getId().' '.$valuesVal->getTitle();
             }
+        }
         
         if (!$product->getData('added_by_rule_id')) {
             if ($wiserPrice > 1 && !empty($wiserPrice)) {
