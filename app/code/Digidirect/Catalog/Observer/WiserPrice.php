d59b0190a282c4bc7f835b2eceb4e75121d8a124
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-12-05 09:46:04 +0800

    Test Fix digiProtect

--- app/code/Digidirect/Catalog/Observer/WiserPrice.php
+++ app/code/Digidirect/Catalog/Observer/WiserPrice.php
@@ -106,7 +106,7 @@ class WiserPrice implements ObserverInterface
             foreach($customOptions as $optionKey => $optionVal) {
                 foreach($optionVal->getValues() as $valuesKey => $valuesVal) {
                     $this->logger->info('$valuesVal: ' . $valuesVal->getTitle(). ' ' .$valuesVal->getPrice());
-                    if ($selectedOption['info_buyRequest']['options']['label'] == "digiProtect") {
+                    if ($selectedOption['options']['label'] == "digiProtect") {
                         $digiProtectPrice = $valuesVal->getPrice();
                     }
                 }
