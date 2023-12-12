dcc65f1b54a1726da84c16de88d920ca816c0531
ronde <ronde@DESKTOP-43FC5AG>
2023-12-12 17:15:07 +0800

    Test digiDeals

--- app/code/Digidirect/DigiDeals/Block/Product/GridProduct.php
+++ app/code/Digidirect/DigiDeals/Block/Product/GridProduct.php
@@ -49,8 +49,11 @@ class GridProduct extends \Magento\Catalog\Block\Product\AbstractProduct
     
     public function getLoadedProductCollection()
     {
+        $defaultCategory = 2;
+        
         $collection = $this->_productCollectionFactory->create();
         $collection->addAttributeToSelect('*')->addFinalPrice();
+        $collection->addCategoriesFilter(['in' => $defaultCategory]);
         $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
         $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
         $collection->getSelect()->where("price_index.final_price < price");
