012e811ff626c0a0eb7e8da45c37bbfd9d6f3268
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-12-19 22:02:21 +0800

    Marketplacer Products Custom Listing Page

--- app/code/Digidirect/MarketplacerProducts/Model/Layer.php
+++ app/code/Digidirect/MarketplacerProducts/Model/Layer.php
@@ -70,7 +70,7 @@ class Layer extends \Magento\Catalog\Model\Layer
             $collection->addAttributeToSelect('*');
             $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
             $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-            $collection->addAttributeToFilter('marketplacer_seller', ['neq' => 'General Seller']);
+            $collection->addAttributeToFilter("marketplacer_seller", array("neq" => 20245));
             $this->prepareProductCollection($collection);
             $this->_productCollections[$defaultCategory] = $collection;
         }
