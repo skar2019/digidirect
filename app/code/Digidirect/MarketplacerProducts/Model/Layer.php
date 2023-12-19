40064e1b16af66d2f341ba389ed835dbbfdaad48
ronde <ronde@DESKTOP-43FC5AG>
2023-12-19 21:03:17 +0800

    Marketplacer Products Custom Listing Page

--- app/code/Digidirect/MarketplacerProducts/Model/Layer.php
+++ app/code/Digidirect/MarketplacerProducts/Model/Layer.php
@@ -70,7 +70,7 @@ class Layer extends \Magento\Catalog\Model\Layer
             $collection->addAttributeToSelect('*');
             $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
             $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
-            //$collection->addAttributeToFilter('marketplacer_seller', array('neq' => 'digiDirect'));
+            $collection->addFieldToFilter('marketplacer_seller', array('neq' => 'digiDirect'));
             $this->prepareProductCollection($collection);
             $this->_productCollections[$defaultCategory] = $collection;
         }
