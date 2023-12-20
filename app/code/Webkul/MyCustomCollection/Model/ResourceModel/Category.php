b4b1aec70d8efd1e49a18b4fe3bb57633f12c62c
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2023-12-20 11:59:22 +0800

    Test Webkul_MyCustomCollection Module

---
+++ app/code/Webkul/MyCustomCollection/Model/ResourceModel/Category.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Webkul Software.
+ *
+ * @category  Webkul
+ * @package   Webkul_MyCustomCollection
+ * @author    Webkul
+ * @copyright Copyright (c) Webkul Software Private Limited (https://webkul.com)
+ * @license   https://store.webkul.com/license.html
+ */
+namespace Webkul\MyCustomCollection\Model\ResourceModel;
+
+class Category extends \Magento\Catalog\Model\ResourceModel\Category
+{
+    public function getProductCount($category)
+    {
+        $collection = $category->getProductCollection();
+        $collection->addAttributeToSelect('*');
+        $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
+        $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+        return intval($collection->count());
+    }
+}
---
+++ app/code/Webkul/MyCustomCollection/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Webkul_MyCustomCollection',
+    __DIR__
+);
