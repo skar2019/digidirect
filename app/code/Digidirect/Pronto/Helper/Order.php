1eb743f11354cef2ad09da075e04889a2a32292e
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-01-03 07:44:52 +0800

    special characters issue

--- app/code/Digidirect/Pronto/Helper/Order.php
+++ app/code/Digidirect/Pronto/Helper/Order.php
@@ -683,6 +683,10 @@ class Order extends AbstractHelper
                 }
             }
 
+            $contactname = preg_replace('/[^A-Za-z0-9. -]/', '', $contactname);
+            $shipUnitNumber = preg_replace('/[^A-Za-z0-9. -]/', '', $shipUnitNumber);
+            $shipstreet = preg_replace('/[^A-Za-z0-9. -]/', '', $shipstreet);
+
             $data['sales-order']['header']['delivery-address']['line-1'] = $contactname;
             $data['sales-order']['header']['delivery-address']['line-2'] = $shipcompany;
             $data['sales-order']['header']['delivery-address']['line-3'] = $shipUnitNumber." ".$shipstreet;
