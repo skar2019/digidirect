313cc5f0983c3c1a75bbf65531b0fbf898c68853
ronde <ronde@DESKTOP-43FC5AG>
2024-01-03 21:14:24 +0800

    Test Fix digiDeals

--- app/code/Digidirect/DigiDeals/Model/Layer.php
+++ app/code/Digidirect/DigiDeals/Model/Layer.php
@@ -65,13 +65,13 @@ class Layer extends \Magento\Catalog\Model\Layer
         if (isset($this->_productCollections[$defaultCategory])) {
             $collection = $this->_productCollections[$defaultCategory];
         } else {
-            $category = $this->categoryRepo->get($defaultCategory, 1);
-            $collection = $this->collectionProvider->getCollection($category);
+            $collection = $this->collectionProvider->getCollection($this->getCurrentCategory());
             $collection->addAttributeToSelect('*');
+            $collection->addCategoriesFilter(['in' => $defaultCategory]);
             $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
             $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
             $collection->addMinimalPrice()->addFinalPrice();
-            $collection->getSelect()->where("price_index.final_price < price_index.price");
+            //$collection->getSelect()->where("price_index.final_price < price_index.price");
         }
         
         $this->prepareProductCollection($collection);
