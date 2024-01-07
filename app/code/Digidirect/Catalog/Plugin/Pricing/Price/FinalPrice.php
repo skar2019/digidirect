8a5dadabd7b1b679783ed202fcd4bd6ddaa108d9
ronde <ronde@DESKTOP-43FC5AG>
2024-01-07 14:34:16 +0800

    Test Fix digiProtect

--- app/code/Digidirect/Catalog/Plugin/Pricing/Price/FinalPrice.php
+++ app/code/Digidirect/Catalog/Plugin/Pricing/Price/FinalPrice.php
@@ -57,6 +57,29 @@ class FinalPrice
         } else {
             $result = $price;
         }
+        
+        $finalPrice = $result;
+        $digiProtectPrice = 0;
+
+        $selectedOption = $item->getProduct()->getTypeInstance(true)->getOrderOptions($product);
+        $this->logger->info('$selectedOption: ' . json_encode($selectedOption));
+
+        $customOptions = $this->_productOptions->getProductOptionCollection($product);
+        foreach($customOptions as $optionKey => $optionVal) {
+            foreach($optionVal->getValues() as $valuesKey => $valuesVal) {
+                $this->logger->info('$valuesVal: ' . $valuesVal->getTitle(). ' ' .$valuesVal->getPrice());
+                if (isset($selectedOption['options'])) {
+                    $digiProtectPrice = $valuesVal->getPrice();
+                }
+            }
+        }
+
+        $this->logger->info('$finalPrice: ' . $finalPrice);
+        $this->logger->info('$digiProtectPrice: ' . $digiProtectPrice);
+
+        $finalProductPrice = $finalPrice + $digiProtectPrice;
+        
+        $result = $finalProductPrice;
             
         return $result;
     }
