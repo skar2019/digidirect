861ee55648e6c006e8aa16a2d021a21efed0c54d
ronde <ronde@DESKTOP-43FC5AG>
2024-01-07 18:56:05 +0800

    Test Fix digiProtect

--- app/code/Digidirect/Catalog/Observer/WiserPrice.php
+++ app/code/Digidirect/Catalog/Observer/WiserPrice.php
@@ -71,7 +71,25 @@ class WiserPrice implements ObserverInterface
         if ($wiserPrice == 0 || empty($wiserPrice)) {
             $finalProductPrice = $finalPrice;
         } else {
-            if ($basePrice > $wiserPrice) {
+            
+            $digiProtectPrice = 0;
+
+            $selectedOption = $item->getProduct()->getTypeInstance(true)->getOrderOptions($item->getProduct());
+            $this->logger->info('$selectedOption: ' . json_encode($selectedOption));
+
+            $customOptions = $this->_productOptions->getProductOptionCollection($product);
+            foreach($customOptions as $optionKey => $optionVal) {
+                foreach($optionVal->getValues() as $valuesKey => $valuesVal) {
+                    $this->logger->info('$valuesVal: ' . $valuesVal->getTitle(). ' ' .$valuesVal->getPrice());
+                    if (isset($selectedOption['options'])) {
+                        $digiProtectPrice = $valuesVal->getPrice();
+                    }
+                }
+            }
+            
+            $wiserPlusDigiProtect = $wiserPrice + $digiProtectPrice;
+            
+            if ($finalPrice > $wiserPlusDigiProtect) {
                 if ($wiserPrice > 1 && !empty($wiserPrice)) {
                     if ($wiserPrice < $price) {
                         if ((in_array($sku, $discount2)) && $isDigiClub) {
@@ -90,21 +108,6 @@ class WiserPrice implements ObserverInterface
                 } else {
                     $finalPrice = $price;
                 }
-                
-                $digiProtectPrice = 0;
-
-                $selectedOption = $item->getProduct()->getTypeInstance(true)->getOrderOptions($item->getProduct());
-                $this->logger->info('$selectedOption: ' . json_encode($selectedOption));
-
-                $customOptions = $this->_productOptions->getProductOptionCollection($product);
-                foreach($customOptions as $optionKey => $optionVal) {
-                    foreach($optionVal->getValues() as $valuesKey => $valuesVal) {
-                        $this->logger->info('$valuesVal: ' . $valuesVal->getTitle(). ' ' .$valuesVal->getPrice());
-                        if (isset($selectedOption['options'])) {
-                            $digiProtectPrice = $valuesVal->getPrice();
-                        }
-                    }
-                }
 
                 $this->logger->info('$finalPrice: ' . $finalPrice);
                 $this->logger->info('$digiProtectPrice: ' . $digiProtectPrice);
@@ -113,7 +116,7 @@ class WiserPrice implements ObserverInterface
                 
             } else {
                 
-                $finalProductPrice = $price;
+                $finalProductPrice = $finalPrice;
             }
             
         }
