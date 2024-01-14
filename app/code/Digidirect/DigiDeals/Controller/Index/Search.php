ea84dccf6499f1c00ebd036ae9adb4f09a7af200
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-01-14 21:12:34 +0800

    Test Custom Page Inner Search

--- app/code/Digidirect/DigiDeals/Controller/Index/Search.php
+++ app/code/Digidirect/DigiDeals/Controller/Index/Search.php
@@ -3,22 +3,34 @@
 namespace Digidirect\DigiDeals\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\Result\JsonFactory;
 
 class Search extends Action {
+    /**
+     * @var JsonFactory
+     */
+    protected $_resultJsonFactory;
     
     public function __construct(
-        \Magento\Framework\App\Action\Context $context
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
     ) {
+        $this->_resultJsonFactory = $resultJsonFactory;
         parent::__construct($context);
     }   
 
     public function execute() {
-
+        
+        $result = $this->_resultJsonFactory->create();
+        
         if ($this->getRequest()->getParam('innerSearchInput')) {
-            return 'Test JSON!';
+            $result->setData(['output' => $this->getRequest()->getParam('innerSearchInput')]);
+            return $result;
         } else {
-            return 'No data!';
+            $result->setData(['output' => 'No data!']);
+            return $result;
         }
+        
     }
 
 }
