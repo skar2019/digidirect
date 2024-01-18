e45bebbeeaf87962b80ba66510320f710c938d88
Dev4 <jireh@kayweb.com.au>
2024-01-18 11:12:02 +0800

    test js on Magento_Company

--- app/design/frontend/Digidirect/digi/Magento_Company/web/js/default.js
+++ app/design/frontend/Digidirect/digi/Magento_Company/web/js/default.js
@@ -1,1 +1,17 @@
-alert("Requisition");
+require(['jquery', 'owlcarousel'],
+    function ($) {
+            alert("test");
+            $('.requisition-list-button').attr( 'id', 'requisition_id' );
+
+        // $(document).ready(function() {
+        //     $('.product-carousel').owlCarousel({
+        //         loop:false,
+        //         nav:false,
+        //         items:1,
+        //         dots:true,
+        //         autoplay:true,
+        //         autoplayTimeout:3000
+        //     })
+        // });
+    }
+);
