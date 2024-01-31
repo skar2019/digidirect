c0b94b192de05bba8b89d1a91249a71dec6fe228
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-01-31 09:10:20 +0800

    Test Fix Free Gift Issue

--- app/code/Digidirect/FreeGift/Model/MiniCartItem.php
+++ app/code/Digidirect/FreeGift/Model/MiniCartItem.php
@@ -2,25 +2,27 @@
 
 namespace Digidirect\FreeGift\Model;
 
+use Magento\Framework\App\ObjectManager;
+use Magento\Catalog\Model\Product\Configuration\Item\ItemResolverInterface;
+
 class MiniCartItem extends \Magento\Checkout\CustomerData\DefaultItem
 {
-
     public function __construct(
         \Magento\Catalog\Helper\Image $imageHelper,
         \Magento\Msrp\Helper\Data $msrpHelper,
         \Magento\Framework\UrlInterface $urlBuilder,
         \Magento\Catalog\Helper\Product\ConfigurationPool $configurationPool,
         \Magento\Checkout\Helper\Data $checkoutHelper,
-        \Magento\Catalog\Helper\Output $helper,
-        \Magento\Catalog\Model\Product $productModel
+        \Magento\Framework\Escaper $escaper = null,
+        ItemResolverInterface $itemResolver = null
     ) {
         $this->configurationPool = $configurationPool;
         $this->imageHelper = $imageHelper;
         $this->msrpHelper = $msrpHelper;
         $this->urlBuilder = $urlBuilder;
         $this->checkoutHelper = $checkoutHelper;
-        $this->helper = $helper;
-        $this->productModel = $productModel;
+        $this->escaper = $escaper ?: ObjectManager::getInstance()->get(\Magento\Framework\Escaper::class);
+        $this->itemResolver = $itemResolver ?: ObjectManager::getInstance()->get(ItemResolverInterface::class);
     }
 
     /**
@@ -29,27 +31,31 @@ class MiniCartItem extends \Magento\Checkout\CustomerData\DefaultItem
     protected function doGetItemData()
     {
         $imageHelper = $this->imageHelper->init($this->getProductForThumbnail(), 'mini_cart_product_thumbnail');
-        $product = $this->productModel->load($this->item->getProduct()->getId());
+        $productName = $this->escaper->escapeHtml($this->item->getProduct()->getName());
+        
         return [
             'options' => $this->getOptionList(),
             'qty' => $this->item->getQty() * 1,
             'item_id' => $this->item->getId(),
             'configure_url' => $this->getConfigureUrl(),
             'is_visible_in_site_visibility' => $this->item->getProduct()->isVisibleInSiteVisibility(),
-            'product_name' => $this->item->getProduct()->getName(),
+            'product_id' => $this->item->getProduct()->getId(),
+            'product_name' => $productName,
+            'product_sku' => $this->item->getProduct()->getSku(),
             'product_url' => $this->getProductUrl(),
             'product_has_url' => $this->hasProductUrl(),
             'product_price' => $this->checkoutHelper->formatPrice($this->item->getCalculationPrice()),
+            'product_price_value' => $this->item->getCalculationPrice(),
             'product_image' => [
                 'src' => $imageHelper->getUrl(),
                 'alt' => $imageHelper->getLabel(),
                 'width' => $imageHelper->getWidth(),
                 'height' => $imageHelper->getHeight(),
             ],
-            'product_custom_attribute' => $this->helper->productAttribute($product, $product->getCustomAttribute(), 'custom_attribute'),
+            'freegift_rule_id' => 15,
             'canApplyMsrp' => $this->msrpHelper->isShowBeforeOrderConfirm($this->item->getProduct())
                 && $this->msrpHelper->isMinimalPriceLessMsrp($this->item->getProduct()),
-            'freegift_rule_id' => 15,
+            'message' => $this->item->getMessage(),
         ];
     }
 }
