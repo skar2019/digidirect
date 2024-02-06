b36f26dbf1117c1bb45030c8613ed689aeb833f5
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-06 10:07:06 +0800

    Test Pagination

--- app/design/frontend/Digidirect/digi/Magento_Checkout/requirejs-config.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/requirejs-config.js
@@ -6,6 +6,7 @@ var config = {
             'digidirectStoreCheckout': 'Magento_Checkout/js/dist/common/store',
             'accordionStep': 'Magento_Checkout/js/accordion-step',
             'qantasLoader': 'Magento_Checkout/js/qantas',
+            'clickAndCollect': 'Digidirect_Locator/js/view/list'
         }
     },
     config: {
