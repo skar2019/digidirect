f40b8aa615e5b0133814ded7f2af3e914876e827
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-08 11:09:05 +0800

    Test Product API Override

--- app/code/Digidirect/Catalog/Plugin/ProductRepository.php
+++ app/code/Digidirect/Catalog/Plugin/ProductRepository.php
@@ -7,16 +7,26 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class ProductRepository
 {
+    
+    protected $logger;
     /**
      * @param ProductRepositoryInterface $subject
      * @param ProductInterface $result
      * @return ProductInterface
      */
+    
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+    }
+    
     public function afterGet(
         ProductRepositoryInterface $subject,
         ProductInterface $result
     ) {
         // Modify SKU before returning the result
+        $this->logger->info('Test Product API Override!');
         $result->setSku(substr($result->getSku(), 0, 3));
 
         return $result;
