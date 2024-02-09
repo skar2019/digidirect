957e169a06be5f0b75e163a4ce37ee2e5cb6df08
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-09 11:08:20 +0800

    Test Disable Digidirect_Qantas Module

--- app/etc/config.php
+++ app/etc/config.php
@@ -609,7 +609,7 @@ return [
         'Digidirect_PaSalesForceProductRecommendation' => 1,
         'Digidirect_ProductOverlay' => 1,
         'Digidirect_Pronto' => 1,
-        'Digidirect_Qantas' => 0,
+        'Digidirect_Qantas' => 1,
         'Digidirect_QuickView' => 1,
         'Digidirect_ReadytoPickup' => 0,
         'Digidirect_RecommendedProducts' => 1,
