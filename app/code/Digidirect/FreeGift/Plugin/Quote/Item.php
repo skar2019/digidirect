a7629586af21814edb682defb453961a7be7fd27
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-13 09:39:52 +0800

    Test Fix Free Gift

--- app/code/Digidirect/FreeGift/Plugin/Quote/Item.php
+++ app/code/Digidirect/FreeGift/Plugin/Quote/Item.php
@@ -42,6 +42,7 @@ class Item
         $this->logger->info('$value: ' . $value);
         $this->logger->info('isFreeGiftItem: ' . $this->_giftItem->isFreeGiftItem($subject));
         if ($this->_giftItem->isFreeGiftItem($subject)) {
+            $this->logger->info('This is free gift!');
             return [0];
         }
         return [$value];
