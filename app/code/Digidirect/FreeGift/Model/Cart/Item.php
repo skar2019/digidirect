2970054118a5d90a12d382e4ec066e14b2b809dd
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-16 21:59:15 +0800

    Test Fix Free Gift

--- app/code/Digidirect/FreeGift/Model/Cart/Item.php
+++ app/code/Digidirect/FreeGift/Model/Cart/Item.php
@@ -107,10 +107,16 @@ class Item extends QuoteItem
      */
     public function isFreeGiftItem($item)
     {
-        if (in_array($this->_appState->getAreaCode(), [Area::AREA_ADMIN, Area::AREA_ADMINHTML])) {
+        /*if (in_array($this->_appState->getAreaCode(), [Area::AREA_ADMIN, Area::AREA_ADMINHTML])) {
+            return false;
+        }*/
+        
+        if ($this->getRuleId($item)) {
+            return $this->getRuleId($item) !== null;
+        } else {
             return false;
         }
-        return $this->getRuleId($item) !== null;
+        
     }
 
     /**
