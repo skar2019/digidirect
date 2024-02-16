6fa4fbd04dbdee886d5af371ed6851376e7b57d3
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-16 12:45:09 +0800

    Test Fix Free Gift

---
+++ app/code/Digidirect/Catalog/Plugin/Quote/Item.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Digidirect\Catalog\Plugin\Quote;
+
+use Magento\Quote\Model\Quote\Item as QuoteItem;
+use Magento\Catalog\Model\Product;
+use Digidirect\FreeGift\Model\Cart\Item as CartItem;
+
+class Item
+{
+    /**
+     * @var CartItem
+     */
+    protected $_giftItem;
+    
+    
+    protected $logger;
+
+    /**
+     * Item constructor.
+     *
+     * @param CartItem $giftItem
+     */
+    public function __construct
+    (   
+        CartItem $giftItem,
+        \Psr\Log\LoggerInterface $logger
+    )
+    {
+        $this->_giftItem = $giftItem;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param QuoteItem $subject
+     * @param mixed $value
+     * @return array
+     */
+    public function beforeSetPrice(QuoteItem $subject, $value)
+    {
+        $this->logger->info('beforeSetPrice!');
+        $this->logger->info('$value: ' . $value);
+        $this->logger->info('isFreeGiftItem: ' . $this->_giftItem->isFreeGiftItem($subject));
+        if ($this->_giftItem->isFreeGiftItem($subject)) {
+            $this->logger->info('This is free gift!');
+            return [0];
+        }
+        return [$value];
+    }
+}
