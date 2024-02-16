3f55e94ccb59274b4244a7634dc007cd3d8dbc78
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-16 12:24:19 +0800

    Test Free Gift

--- app/etc/config.php
+++ app/etc/config.php
@@ -565,7 +565,7 @@ return [
         'Digidirect_BestsellersProducts' => 1,
         'Digidirect_Blog' => 1,
         'Digidirect_InfiniteScroll' => 0,
-        'Digidirect_Catalog' => 0,
+        'Digidirect_Catalog' => 1,
         'Digidirect_LayeredNavigation' => 0,
         'Digidirect_Checkout' => 1,
         'Digidirect_CheckoutFields' => 1,
