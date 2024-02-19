1e6d276480f2a5b2eb2f33ccdfa835015f7f83b3
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-02-19 12:05:20 +0800

    Temporary Fix For digiDeals

--- app/code/Digidirect/DigiDeals/Model/Layer.php
+++ app/code/Digidirect/DigiDeals/Model/Layer.php
@@ -68,6 +68,7 @@ class Layer extends \Magento\Catalog\Model\Layer
             $category = $this->categoryRepo->get($defaultCategory, 1);
             $collection = $this->collectionProvider->getCollection($category);
             $collection->addAttributeToSelect('*');
+            $collection->addCategoriesFilter(['in' => 17]);
             $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
             $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
             $collection->addMinimalPrice()->addFinalPrice();
