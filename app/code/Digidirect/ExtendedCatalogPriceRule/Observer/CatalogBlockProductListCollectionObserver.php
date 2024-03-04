a96c4495721922ec249ec7629a232e8e15e3e7ae
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-03-04 20:29:14 +0800

    Test Fix Pagination Issue

--- app/code/Digidirect/ExtendedCatalogPriceRule/Observer/CatalogBlockProductListCollectionObserver.php
+++ app/code/Digidirect/ExtendedCatalogPriceRule/Observer/CatalogBlockProductListCollectionObserver.php
@@ -37,10 +37,10 @@ class CatalogBlockProductListCollectionObserver implements ObserverInterface
             return;
         }
 
-        $productIds = $productCollection->getAllIds();
-        $ruleData = $this->helper->getExtendedRulesDataForView($productIds);
-        if (empty($ruleData)) {
-            return;
+        foreach ($productCollection as $item) {
+            if (!empty($ruleData[$item->getId()])) {
+                $item->setData('show_msg_extended_rule', $ruleData[$item->getId()]);
+            }
         }
     }
 }
