4efaec0bb9fb39b068f4c2b7ba668be02add52bb
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-03-05 09:02:10 +0800

    Test Fix Pagination Issue

--- app/code/Digidirect/ExtendedCatalogPriceRule/Observer/CatalogBlockProductListCollectionObserver.php
+++ app/code/Digidirect/ExtendedCatalogPriceRule/Observer/CatalogBlockProductListCollectionObserver.php
@@ -33,6 +33,20 @@ class CatalogBlockProductListCollectionObserver implements ObserverInterface
     public function execute(Observer $observer)
     {
         $productCollection = $observer->getEvent()->getCollection();
-        return $productCollection;
+        if (!$productCollection instanceof \Magento\Catalog\Model\ResourceModel\Product\Collection) {
+            return;
+        }
+
+        $productIds = $productCollection->getAllIds();
+        $ruleData = $this->helper->getExtendedRulesDataForView($productIds);
+        if (empty($ruleData)) {
+            return;
+        }
+
+        foreach ($productCollection as $item) {
+            if (!empty($ruleData[$item->getId()])) {
+                $item->setData('show_msg_extended_rule', $ruleData[$item->getId()]);
+            }
+        }
     }
 }
