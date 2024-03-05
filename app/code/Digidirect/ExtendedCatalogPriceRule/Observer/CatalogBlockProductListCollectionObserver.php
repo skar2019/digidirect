93a6a4d79371a94119a5153fd129c26235aa5a5a
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-03-05 21:57:03 +0800

    Revert

--- app/code/Digidirect/ExtendedCatalogPriceRule/Observer/CatalogBlockProductListCollectionObserver.php
+++ app/code/Digidirect/ExtendedCatalogPriceRule/Observer/CatalogBlockProductListCollectionObserver.php
@@ -33,7 +33,20 @@ class CatalogBlockProductListCollectionObserver implements ObserverInterface
     public function execute(Observer $observer)
     {
         $productCollection = $observer->getEvent()->getCollection();
-        $productCollection->addAttributeToSelect('*');
-        return $productCollection;
+        if (!$productCollection instanceof \Magento\Catalog\Model\ResourceModel\Product\Collection) {
+            return;
+        }
+
+        $productIds = $productCollection->getAllIds();
+        $ruleData = $this->helper->getExtendedRulesDataForView($productIds);
+        if (empty($ruleData)) {
+            return;
+        }
+
+        foreach ($productCollection as $item) {
+            if (!empty($ruleData[$item->getId()])) {
+                $item->setData('show_msg_extended_rule', $ruleData[$item->getId()]);
+            }
+        }
     }
 }
