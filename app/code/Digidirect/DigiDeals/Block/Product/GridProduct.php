61cea08ccd5f626515724bcfcc8f596907d45a4d
Rondel15 <51977349+Rondel15@users.noreply.github.com>
2024-03-19 10:03:12 +0800

    Update digiDeals Listing

--- app/code/Digidirect/DigiDeals/Block/Product/GridProduct.php
+++ app/code/Digidirect/DigiDeals/Block/Product/GridProduct.php
@@ -53,7 +53,7 @@ class GridProduct extends \Magento\Catalog\Block\Product\AbstractProduct
 
         $collection = $this->_productCollectionFactory->create();
         $collection->addAttributeToSelect('*');
-        $collection->addCategoriesFilter(['in' => 17]);
+        $collection->addCategoriesFilter(['in' => $defaultCategory]);
         $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
         $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
         $collection->addMinimalPrice()->addFinalPrice();
