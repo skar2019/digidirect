e606ce839679ad8b7166129386dca6360f54dc53
rondel <rondel@localhost>
2024-04-06 21:04:44 +0800

    Test digiMarket Randomize

--- app/code/Digidirect/MarketplacerProducts/Model/Layer.php
+++ app/code/Digidirect/MarketplacerProducts/Model/Layer.php
@@ -67,10 +67,11 @@ class Layer extends \Magento\Catalog\Model\Layer
         } else {
             $category = $this->categoryRepo->get($defaultCategory, 1);
             $collection = $this->collectionProvider->getCollection($category);
-            $collection->addAttributeToSelect('*')->orderRand();
+            $collection->addAttributeToSelect('*');
             $collection->addAttributeToFilter('visibility', \Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH);
             $collection->addAttributeToFilter('status', \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
             $collection->addAttributeToFilter("marketplacer_seller", array("neq" => 20329));
+            $collection->getSelect()->orderRand();
             $this->prepareProductCollection($collection);
             $this->_productCollections[$defaultCategory] = $collection;
         }
