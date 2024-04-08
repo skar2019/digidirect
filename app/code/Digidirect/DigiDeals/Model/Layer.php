046b7bebf139e1e0d9ef08ec8b2512c9d582def8
rondel <rondel@localhost>
2024-04-08 08:55:15 +0800

    Test Fix digiDeals

--- app/code/Digidirect/DigiDeals/Model/Layer.php
+++ app/code/Digidirect/DigiDeals/Model/Layer.php
@@ -59,7 +59,8 @@ class Layer extends \Magento\Catalog\Model\Layer
     
     public function getProductCollection()
     {
-        $defaultCategory = 2;
+        //$defaultCategory = 2;
+        $defaultCategory = 17;
         $productIdsArray = [];
         
         if (isset($this->_productCollections[$defaultCategory])) {
