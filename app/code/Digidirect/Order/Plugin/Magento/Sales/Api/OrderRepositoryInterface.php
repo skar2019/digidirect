7c126a464e3fcd65b1e0814e663326952d4daac5
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-04-11 08:31:09 +0800

    unit_number to rest api

---
+++ app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderRepositoryInterface.php
@@ -0,0 +1,50 @@
+<?php
+namespace Vendorname\ModuleName\Plugin\Magento\Sales\Api;
+
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderAddressExtensionInterfaceFactory;
+use Magento\Sales\Api\Data\OrderAddressExtension;
+use Magento\Sales\Api\Data\OrderAddressInterface;
+
+class OrderRepositoryInterface
+{
+    /**
+     * @var OrderAddressExtensionInterfaceFactory
+     */
+    private $addressExtensionInterfaceFactory;
+
+    public function __construct(
+        OrderAddressExtensionInterfaceFactory $addressExtensionInterfaceFactory
+    ) {
+        $this->addressExtensionInterfaceFactory = $addressExtensionInterfaceFactory;
+    }
+
+    public function afterGet(
+        \Magento\Sales\Api\OrderRepositoryInterface $subject,
+        OrderInterface $order
+    ) {
+        /**
+         * @var OrderAddressInterface  $billingAddress
+         */
+        $billingAddress = $order->getBillingAddress();
+
+        $billingAddressExtensionAttributes = (null !== $billingAddress->getExtensionAttributes())?
+            $billingAddress->getExtensionAttributes():
+            $this->addressExtensionInterfaceFactory->create();
+        $billingAddressExtensionAttributes->setUnitNumber($billingAddress->getUnitNumber());
+
+        $billingAddress->setExtensionAttributes($billingAddressExtensionAttributes);
+
+        if (!$order->getIsVirtual()) {
+            $shippingAddress = $order->getShippingAddress();
+            $shippingAddressExtensionAttributes = (null!== $shippingAddress->getExtensionAttributes())?
+                $shippingAddress->getExtensionAttributes():
+                $this->addressExtensionInterfaceFactory->create();
+
+            $shippingAddressExtensionAttributes->setUnitNumber($shippingAddress->getUnitNumber());
+            $shippingAddress->setExtensionAttributes($shippingAddressExtensionAttributes);
+        }
+
+        return $order;
+    }
+}
