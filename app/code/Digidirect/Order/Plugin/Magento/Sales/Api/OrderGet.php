8648df2db0c48a1eca61964f609674f6c6275559
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-04-15 13:10:54 +0800

    Unit Number on extension attributes

---
+++ app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderGet.php
@@ -0,0 +1,54 @@
+<?php
+namespace Digidirect\Order\Plugin\Magento\Sales\Api;
+
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\ResourceModel\Order\Collection;
+
+class OrderGet
+{
+    protected $orderExtensionFactory;
+    protected $logger;
+    public function __construct(
+        OrderExtensionFactory $orderExtensionFactory,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->orderExtensionFactory = $orderExtensionFactory;
+        $this->logger = $logger;
+    }
+
+    public function afterGet(
+        OrderRepositoryInterface $subject,
+        OrderInterface $resultOrder
+    ) {
+        $this->logger->info('Test order API Override!');
+
+        $extensionAttributes = $resultOrder->getExtensionAttributes();
+        if ($extensionAttributes && $extensionAttributes->getUnitNumber()) {
+            return $resultOrder;
+        }
+
+        $initialShippingAddressUnitNumber = $resultOrder->getShippingAddress()->getUnitNumber();
+        $this->logger->info('Test order API Override! -'.$initialShippingAddressUnitNumber);
+        /** @var \Magento\Sales\Api\Data\OrderExtension $orderExtension */
+
+        $orderExtension = $extensionAttributes ? $extensionAttributes : $this->orderExtensionFactory->create();
+        $orderExtension->setUnitNumber($initialShippingAddressUnitNumber);
+        $resultOrder->setExtensionAttributes($orderExtension);
+
+        return $resultOrder;
+
+    }
+
+    public function afterGetList(
+        OrderRepositoryInterface $subject,
+        Collection $resultOrder
+    ) {
+        /** @var  $order */
+        foreach ($resultOrder->getItems() as $order) {
+            $this->afterGet($subject, $order);
+        }
+        return $resultOrder;
+    }
+}
