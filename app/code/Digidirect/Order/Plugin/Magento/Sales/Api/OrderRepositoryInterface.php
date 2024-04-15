44df6ab103346a9e5605b2e8c18060c19693a008
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-04-15 10:52:26 +0800

    webapi rest

--- app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderRepositoryInterface.php
+++ app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderRepositoryInterface.php
@@ -13,15 +13,12 @@ class OrderRepositoryInterface
      */
     private $addressExtensionInterfaceFactory;
     protected $logger;
-    protected $orderExtension;
     public function __construct(
         OrderAddressExtensionInterfaceFactory $addressExtensionInterfaceFactory,
-        \Magento\Sales\Api\Data\OrderExtension $orderExtension,
         \Psr\Log\LoggerInterface $logger
     ) {
         $this->addressExtensionInterfaceFactory = $addressExtensionInterfaceFactory;
         $this->logger = $logger;
-        $this->orderExtension = $orderExtension;
     }
 
     public function afterGet(
@@ -32,18 +29,9 @@ class OrderRepositoryInterface
          * @var OrderAddressInterface  $billingAddress
          */
         $this->logger->info('Test order API Override!');
-        //Set unit number for Shipping Address
-
         $initialShippingAddressUnitNumber = $order->getShippingAddress()->getUnitNumber();
         $this->logger->info('Test order API Override! -'.$initialShippingAddressUnitNumber);
-        $extensionAttributes= $order->getExtensionAttributes();
-        if ($extensionAttributes === null)
-        {
-            $extensionAttributes = $this->orderExtension;
-        }
-        $extensionAttributes->setData('unit_number',$initialShippingAddressUnitNumber);
 
-        $order->setExtensionAttributes($extensionAttributes);
         return $order;
     }
 }
