5d97edafcaa0799fbd0728712e610c70746c3b3c
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-04-16 10:26:11 +0800

    Unit number on street

--- app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderGet.php
+++ app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderGet.php
@@ -9,12 +9,15 @@ use Magento\Sales\Model\ResourceModel\Order\Collection;
 class OrderGet
 {
     protected $orderExtensionFactory;
+    protected $repositoryAddress;
     protected $logger;
     public function __construct(
         OrderExtensionFactory $orderExtensionFactory,
+        \Magento\Sales\Model\Order\AddressRepository $repositoryAddress,
         \Psr\Log\LoggerInterface $logger
     ) {
         $this->orderExtensionFactory = $orderExtensionFactory;
+        $this->repositoryAddress= $repositoryAddress;
         $this->logger = $logger;
     }
 
@@ -24,21 +27,34 @@ class OrderGet
     ) {
         $this->logger->info('Test order API Override!');
 
-        $extensionAttributes = $resultOrder->getExtensionAttributes();
-        if ($extensionAttributes && $extensionAttributes->getUnitNumber()) {
-            return $resultOrder;
-        }
+//        $extensionAttributes = $resultOrder->getExtensionAttributes();
+//        if ($extensionAttributes && $extensionAttributes->getUnitNumber()) {
+//            return $resultOrder;
+//        }
 
         $initialShippingAddressUnitNumber = $resultOrder->getShippingAddress()->getUnitNumber();
-        $this->logger->info('Test order API Override! -'.$initialShippingAddressUnitNumber);
+        $street = $resultOrder->getShippingAddress()->getStreet();
+        $newstreet = $initialShippingAddressUnitNumber . " ". $street[0];
+
+        $shipAddress = $resultOrder->getShippingAddress();
+        $initialShippingAddressUnitNumber = $resultOrder->getShippingAddress()->getUnitNumber();
+        if(!empty($initialShippingAddressUnitNumber))
+        {
+            $initialShippingAddressUnitNumber = str_replace("unit_number", "", $initialShippingAddressUnitNumber);
+            $shippingAddressUnitNumber = str_replace("\n", "", $initialShippingAddressUnitNumber);
+            $shipAddress->setStreet(array($newstreet));
+            //$shipAddress->setUnitNumber($shippingAddressUnitNumber);
+        }
+
+        $this->repositoryAddress->save($shipAddress);
+        $this->logger->info('Test order API Override! -'.$newstreet);
         /** @var \Magento\Sales\Api\Data\OrderExtension $orderExtension */
 
-        $orderExtension = $extensionAttributes ? $extensionAttributes : $this->orderExtensionFactory->create();
-        $orderExtension->setUnitNumber($initialShippingAddressUnitNumber);
-        $resultOrder->setExtensionAttributes($orderExtension);
+//        $orderExtension = $extensionAttributes ? $extensionAttributes : $this->orderExtensionFactory->create();
+//        $orderExtension->setUnitNumber($initialShippingAddressUnitNumber);
+//        $resultOrder->setExtensionAttributes($orderExtension);
 
         return $resultOrder;
-        //redeploy
 
     }
 
