9fc9bc0df9868560fcc8cc5d41be7cb349dccf77
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-04-17 13:38:56 +0800

    Test Disable Digidirect_ProductOverlay'

--- app/etc/config.php
+++ app/etc/config.php
@@ -607,7 +607,7 @@ return [
         'Digidirect_OnSaleProducts' => 1,
         'Digidirect_Order' => 1,
         'Digidirect_PaSalesForceProductRecommendation' => 1,
-        'Digidirect_ProductOverlay' => 1,
+        'Digidirect_ProductOverlay' => 0,
         'Digidirect_Pronto' => 1,
         'Digidirect_Qantas' => 1,
         'Digidirect_QuickView' => 1,
