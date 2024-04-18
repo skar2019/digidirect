641d220ce32bef8424ea023f9aba7e3811d9241d
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-04-18 09:59:54 +0800

    Fixed repeating unit number

--- app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderGet.php
+++ app/code/Digidirect/Order/Plugin/Magento/Sales/Api/OrderGet.php
@@ -34,14 +34,22 @@ class OrderGet
 
         $initialShippingAddressUnitNumber = $resultOrder->getShippingAddress()->getUnitNumber();
         $street = $resultOrder->getShippingAddress()->getStreet();
-        $newstreet = $initialShippingAddressUnitNumber . " ". $street[0];
+        $strstring = $street[0];
+        if (str_contains($strstring, $initialShippingAddressUnitNumber)) {
+            $newstreet = $street[0];
+        }
+        else
+        {
+            $newstreet = $initialShippingAddressUnitNumber . " ". $street[0];
+        }
 
         $shipAddress = $resultOrder->getShippingAddress();
-        $initialShippingAddressUnitNumber = $resultOrder->getShippingAddress()->getUnitNumber();
+        //$initialShippingAddressUnitNumber = $resultOrder->getShippingAddress()->getUnitNumber();
         if(!empty($initialShippingAddressUnitNumber))
         {
             $initialShippingAddressUnitNumber = str_replace("unit_number", "", $initialShippingAddressUnitNumber);
             $shippingAddressUnitNumber = str_replace("\n", "", $initialShippingAddressUnitNumber);
+
             $shipAddress->setStreet(array($newstreet));
             //$shipAddress->setUnitNumber($shippingAddressUnitNumber);
         }
