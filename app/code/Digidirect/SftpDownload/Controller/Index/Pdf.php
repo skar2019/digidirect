d40e58f1ee5ac6a793735e0aefd6e26e18bd7f3e
rondel <rondel@localhost>
2024-05-21 14:53:38 +0800

    Test Digidirect_SftpDownload Module

--- app/code/Digidirect/SftpDownload/Controller/Index/Pdf.php
+++ app/code/Digidirect/SftpDownload/Controller/Index/Pdf.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Digidirect\StfpDownload\Controller\Index;
+namespace Digidirect\SftpDownload\Controller\Index;
 
 use Magento\Framework\Filesystem\Io\Sftp;
 use Magento\Framework\Filesystem\Io\File;
@@ -41,11 +41,11 @@ class Pdf extends \Magento\Framework\App\Action\Action
 
             try {
                 $this->sftp->open($sftpConfig);
-                $this->sftp->read('sftp://magentosftp@119.82.149.212:6999/MAGENTO/'.$fileName, $this->file->read($filePath));
+                $this->sftp->read('sftp://magentosftp@119.82.149.212:6999/MAGENTO/'.$fileName, $filePath);
                 $this->sftp->close();
-                return true;
+                echo "Success!";
             } catch (\Exception $e) {
-                echo "Error: " . $e->getMessage();
+                //echo "Error: " . $e->getMessage();
                 return false;
             }
             
