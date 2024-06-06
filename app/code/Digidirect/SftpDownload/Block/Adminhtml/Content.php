5055708adbc16df6c959b7769d2bb0a9dfb4376e
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-07 07:47:22 +0800

    Test SFTP Module On Backend

--- app/code/Digidirect/SftpDownload/Block/Adminhtml/Content.php
+++ app/code/Digidirect/SftpDownload/Block/Adminhtml/Content.php
@@ -2,24 +2,28 @@
 
 namespace Digidirect\SftpDownload\Block\Adminhtml;
 
+use Magento\Framework\View\Element\Template;
 
-class Content extends \Magento\Framework\View\Element\Template {
-    
-    protected $directoryList;
-    
-    protected $driverFile;
+class Content extends Template {
     
     protected $logger;
     
     public function __construct(
+        Template\Context $context, 
         \Magento\Framework\Filesystem\DirectoryList $directoryList,
         \Magento\Framework\Filesystem\Driver\File $driverFile,
-        \Psr\Log\LoggerInterface $logger
+        \Psr\Log\LoggerInterface $logger,
+        array $data = []    
     )
     {
         $this->directoryList = $directoryList;
         $this->driverFile = $driverFile;
         $this->logger = $logger;
+        parent::__construct($context, $data);
+    }
+    
+    protected function _prepareLayout() {
+        return parent::_prepareLayout();
     }
     
     public function testEcho() {
