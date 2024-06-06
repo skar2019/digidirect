9d8f4e91645f3ed2cc8db386a530f06670324a9c
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-06 13:31:15 +0800

    Test File

---
+++ app/code/Digidirect/SftpDownload/Block/Adminhtml/Content.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Digidirect\SftpDownload\Block\Adminhtml;
+
+
+class Content {
+    
+    public  function __construct(
+        \Magento\Framework\Filesystem\DirectoryList $directoryList,
+        \Magento\Framework\Filesystem\Driver\File $driverFile,
+        LoggerInterface $logger)
+    {
+        $this->directoryList = $directoryList;
+        $this->driverFile = $driverFile;
+        $this->logger = $logger;
+    }
+    
+    public function testEcho() {
+        echo 'Send PDF!';
+    }
+
+    public function getFileContents() {
+        $paths = [];
+        try {
+            $path = $this->directoryList->getPath(DirectoryList::VAR_DIR). '/export/email/digi_website_au_customers_10_05_2022_025147.csv';
+            //read just that single directory
+            $contents =  $this->driverFile->fileGetContents($path);
+        } catch (FileSystemException $e) {
+            $this->logger->error($e->getMessage());
+        }
+
+        return $contents;
+    }
+    
+}
