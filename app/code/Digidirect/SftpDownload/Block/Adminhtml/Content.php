eeafd7be53101000fcc1a8e480c2c1fca95fdb7c
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-07 08:06:32 +0800

    Test SFTP Module On Backend

--- app/code/Digidirect/SftpDownload/Block/Adminhtml/Content.php
+++ app/code/Digidirect/SftpDownload/Block/Adminhtml/Content.php
@@ -30,4 +30,17 @@ class Content extends Template {
         echo 'Send PDF!';
     }
     
+    public function getFileContents() {
+        $paths = [];
+        try {
+            $path = $this->directoryList->getPath(DirectoryList::VAR_DIR). '/export/email/digi_website_au_customers_10_05_2022_025147.csv';
+            //read just that single directory
+            $contents =  $this->driverFile->fileGetContents($path);
+        } catch (FileSystemException $e) {
+            $this->logger->error($e->getMessage());
+        }
+
+        return $contents;
+    }
+    
 }
