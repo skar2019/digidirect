ab086cb00536af026f7a6b7d85d4389f5e57ba02
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-09 13:16:54 +0800

    Test SFTP Module

--- app/code/Digidirect/SftpDownload/Controller/Adminhtml/Index/Send.php
+++ app/code/Digidirect/SftpDownload/Controller/Adminhtml/Index/Send.php
@@ -4,25 +4,63 @@ namespace Digidirect\SftpDownload\Controller\Adminhtml\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory; 
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Send extends Action {
     
     protected $_resultJsonFactory;
     
+    protected $transportBuilder;
+    
+    protected $storeManager;
+    
+    protected $logger;
+    
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Psr\Log\LoggerInterface $logger,
+        TransportBuilder $transportBuilder,
+        StoreManagerInterface $storeManager,
     ) {
         $this->_resultJsonFactory = $resultJsonFactory;
+        $this->transportBuilder = $transportBuilder;
+        $this->storeManager = $storeManager;
+        $this->logger = $logger;
         parent::__construct($context);
     }   
 
     public function execute() {
         
         $result = $this->_resultJsonFactory->create();
-        if ($this->getRequest()->getParam('email')) {
-            $result->setData(['output' => $this->getRequest()->getParam('email')]);
+        $customerEmail = $this->getRequest()->getParam('email');
+        
+        $store = $this->storeManager->getStore();
+        
+        if ($customerEmail) {
+            $result->setData(['output' => $customerEmail]);
             return $result;
+        }
+        
+        $transport = $this->transportBuilder->setTemplateIdentifier(
+            'send_pdf_email_template'
+            )->setTemplateOptions(
+                ['area' => 'adminhtml', 'store' => $store->getId()]
+            )->addTo(
+                $customerEmail, $customerEmail
+            /*)->setTemplateVars(
+                $templateParams*/
+            )->setFrom(
+                'general'
+            )->addBcc(
+                'rondel.d@digidirect.com.au' 
+            )->getTransport();
+
+        try {
+            $transport->sendMessage();
+        } catch (\Exception $e) {
+            $this->logger->critical($e->getMessage());
         }
         
     }
