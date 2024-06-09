40d38be343e1bc953d8df2873fac493e4ac21e4b
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-09 12:18:07 +0800

    Test SFTP Module

--- app/code/Digidirect/SftpDownload/Controller/Adminhtml/Index/Send.php
+++ app/code/Digidirect/SftpDownload/Controller/Adminhtml/Index/Send.php
@@ -7,14 +7,19 @@ use Magento\Framework\Controller\ResultFactory;
 
 class Send extends Action {
     
+    protected $_resultJsonFactory;
+    
     public function __construct(
-        \Magento\Framework\App\Action\Context $context
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
     ) {
+        $this->_resultJsonFactory = $resultJsonFactory;
         parent::__construct($context);
     }   
 
     public function execute() {
         
+        $result = $this->_resultJsonFactory->create();
         if ($this->getRequest()->getParam('email')) {
             $result->setData(['output' => $this->getRequest()->getParam('email')]);
             return $result;
