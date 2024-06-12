e3f09215e3ac4b7bf10abf46873a94927508567b
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-06-12 13:37:42 +0800

    setsourceitems

--- app/code/Digidirect/Pronto/Helper/Product.php
+++ app/code/Digidirect/Pronto/Helper/Product.php
@@ -3242,7 +3242,7 @@ class Product extends AbstractHelper
                         }
                     }
                 }
-
+                //redeploy
                 //default source, dapat lagi meron
                 $sourceItem = $this->sourceItemFactory->create();
                 $sourceItem->setSourceCode('default');
