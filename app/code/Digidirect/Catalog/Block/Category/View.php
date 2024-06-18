70def95dd067f1ad1bfb87366c1f5d809c8f2c81
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-18 20:06:00 +0800

    Test Fix Canonical

--- app/code/Digidirect/Catalog/Block/Category/View.php
+++ app/code/Digidirect/Catalog/Block/Category/View.php
@@ -86,7 +86,7 @@ class View extends \Magento\Framework\View\Element\Template implements \Magento\
             }
             if ($this->_categoryHelper->canUseCanonicalTag()) {
                 $this->pageConfig->addRemotePageAsset(
-                    $this->getUrl('*/*/*', ['_current' => true, '_use_rewrite' => true]),
+                    'canonical-category',
                     'canonical',
                     ['attributes' => ['rel' => 'canonical']]
                 );
