180fb9d0d3062aab3086a65f050f50eb3270868d
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-27 12:28:32 +0800

    Test Fix Canonical

--- app/code/ZV/SeoCompatible/Helper/Canonical.php
+++ app/code/ZV/SeoCompatible/Helper/Canonical.php
@@ -60,17 +60,12 @@ class Canonical extends AbstractHelper
                     );
                 } elseif ($this->cmsPage->getIdentifier() == "find") {
                     $url = $this->urlInterface->getCurrentUrl();
-                    $this->logger->info('$url find: ' . $url);
                     $urlComponents = parse_url($url);
-                    
-                    $canonical = $urlComponents['scheme'] . '://' . $urlComponents['host'] . $urlComponents['path'];
-                    $this->logger->info('$canonical find: ' . $canonical);
                     
                     if (!empty($urlComponents['query'])) {
                         $this->pageConfig->setRobots("NOINDEX,NOFOLLOW");
-                        return $this->createLink($canonical);
                     }
-                    return $this->createLink($canonical);
+                    return $this->createLink("https://www.digidirect.com.au/find");
                     
                 } else {
                     return $this->createLink(
