baf45ae490ec35d1390da3a821c0f16a11b03e8c
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-06-27 12:48:07 +0800

    Test Canonical

--- app/code/ZV/SeoCompatible/Helper/Canonical.php
+++ app/code/ZV/SeoCompatible/Helper/Canonical.php
@@ -53,7 +53,6 @@ class Canonical extends AbstractHelper
             if ($this->cmsPage->getId()) {
                 
                 //$this->logger->info('$this->cmsPage->getIdentifier() ' . $this->cmsPage->getIdentifier());
-
                 if ($this->cmsPage->getIdentifier() == "home") {
                     return $this->createLink(
                          rtrim($this->scopeConfig->getValue('web/secure/base_url'), '/')
