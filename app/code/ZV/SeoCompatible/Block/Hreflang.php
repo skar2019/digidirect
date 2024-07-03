07547d0f1e6d2b9a818df943d53bdb05b2a8cbed
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-07-03 19:01:26 +0800

    Test Hreflang Default

---
+++ app/code/ZV/SeoCompatible/Block/Hreflang.php
@@ -0,0 +1,32 @@
+<?php
+namespace ZV\SeoCompatible\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Helper\Data as CatalogHelper;
+use Magento\Framework\Registry;
+
+class Hreflang extends Template
+{
+    protected $registry;
+    protected $catalogHelper;
+
+    public function __construct(
+        Template\Context $context,
+        CatalogHelper $catalogHelper,
+        Registry $registry,
+        array $data = []
+    ) {
+        $this->catalogHelper = $catalogHelper;
+        $this->registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    public function getCurrentUrl()
+    {
+        $product = $this->registry->registry('current_product');
+        if ($product) {
+            return $this->catalogHelper->getProductUrl($product);
+        }
+        return $this->_urlBuilder->getCurrentUrl();
+    }
+}
