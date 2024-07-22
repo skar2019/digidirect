e41ebc4489257d281edf1a8cc3c364b57c9ff602
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-07-22 14:15:44 +0800

    digiClub Competition

--- app/code/Digidirect/DigiClubCompetition/Controller/Index/Index.php
+++ app/code/Digidirect/DigiClubCompetition/Controller/Index/Index.php
@@ -1,7 +1,10 @@
 <?php
 namespace Digidirect\DigiClubCompetition\Controller\Index;
 
-class Index extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Mail\Template\TransportBuilder;
+
+class Index extends Action
 {
     protected $_resultPageFactory;
 
@@ -12,15 +15,55 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        TransportBuilder $transportBuilder
     ) {
         parent::__construct($context);
         $this->_resultPageFactory = $resultPageFactory;
+        $this->transportBuilder = $transportBuilder;
     }
 
     public function execute() {
         $resultPage = $this->_resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->set("Competition");
         return $resultPage;
+
+
+        $post = $this->getRequest()->getPostValue();
+        
+        // Get post values
+        // $email = $this->getRequest()->getParam('email');
+        $message = $this->getRequest()->getParam('message');
+
+        // Send Mail functionality starts from here 
+        $from = "jirehcapao@gmail.com";
+        // $from = $email;
+        $to = array("jireh.c@digidirect.com.au","community@digidirect.com.au");
+        // $bcc = "orders@kayweb.com.au";
+        $nameTo = "Digidirect";
+        $body = "
+        <div>
+            <p>Message: ".$message."</p>
+        </div>";
+
+        $email = new \Zend_Mail();
+        $email->setSubject("digiClub Competition Form"); 
+        $email->setBodyHtml($body);     // use it to send html data
+        //$email->setBodyText($body);   // use it to send simple text data
+        $email->setFrom($from, $nameFrom);
+        $email->addTo($to, $nameTo);
+        // $email->addBcc($bcc);
+        $email->send();
+
+        // $objectManager = \Magento\Framework\App\ObjectManager::getInstance();       
+        // $data = $objectManager->create('Digidirect\CollaborateForm\Model\CollaborateForm');
+        // $data->setData($post);
+        // $data->save();
+
+//        echo "success";
+        /* echo "hello";
+        exit; */
+
+        $this->messageManager->addSuccess(__('Form successfully submitted'));
     }
 }
