22c817165cd7954881808c01ddb13c81b049ca07
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-07-24 07:37:18 +0800

    revert zip

---
+++ app/code/Zip/ZipPayment/Logger/Logger.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Zip\ZipPayment\Logger;
+
+class Logger extends \Monolog\Logger
+{
+
+}
---
+++ app/code/Zip/ZipPayment/Model/Config/Source/Region.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Zip\ZipPayment\Model\Config\Source;
+
+/**
+ * @copyright 2020 Zip Co Limited
+ * @link      https://zip.co
+ */
+class Region implements \Magento\Framework\Option\ArrayInterface
+{
+    protected $_availableCountries = ["au","gb","mx","nz","ca","us","ae","sg","za"];
+    protected $countryInformationAcquirer;
+
+    public function __construct(
+        \Magento\Directory\Api\CountryInformationAcquirerInterface $countryInformationAcquirer
+    ) {
+        $this->countryInformationAcquirer = $countryInformationAcquirer;
+    }
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $countries = $this->countryInformationAcquirer->getCountriesInfo();
+         // Get all countries:
+ 
+        $specificCountries = [];
+ 
+        foreach ($countries as $country) {
+            $countryCode = strtolower($country->getTwoLetterAbbreviation());
+            if (in_array($countryCode, $this->_availableCountries)) {
+                $specificCountries[] = [
+                    'value' => $countryCode,
+                    'label' => $country->getFullNameEnglish()
+                ];
+            }
+        }
+        // you can use array_column() instead of the above code
+        $label = array_column($specificCountries, 'label');
+        // Sort the country with label ascending
+        array_multisort($label, SORT_ASC, $specificCountries);
+        return $specificCountries;
+    }
+}
