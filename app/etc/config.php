4d38fad7e4599a858c0870fbada7cfee585f0423
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-07-31 10:27:43 +0800

    csp report only

--- app/etc/config.php
+++ app/etc/config.php
@@ -824,5 +824,27 @@ return [
                 'en_US'
             ]
         ]
+    ],
+    'csp' => [
+        'mode' => [
+            'storefront_checkout_index_index' => [
+                'report_only' => '1'
+            ],
+            'admin_sales_order_create_index' => [
+                'report_only' => '1'
+            ]
+        ],
+        'policies' => [
+            'storefront_checkout_index_index' => [
+                'scripts' => [
+                    'inline' => '1'
+                ]
+            ],
+            'admin_sales_order_create_index' => [
+                'scripts' => [
+                    'inline' => '1'
+                ]
+            ]
+        ]
     ]
 ];
