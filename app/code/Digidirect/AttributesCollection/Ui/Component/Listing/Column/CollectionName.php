ade90814e5a29a1e3c58851a4cf544daf44b2eb1
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-07-31 11:49:31 +0800

    Sales Order Grid (Benedict)

---
+++ app/code/Digidirect/AttributesCollection/Ui/Component/Listing/Column/CollectionName.php
@@ -0,0 +1,43 @@
+<?php
+namespace Digidirect\AttributesCollection\Ui\Component\Listing\Column;
+
+use \Magento\Sales\Api\OrderRepositoryInterface;
+use \Magento\Framework\View\Element\UiComponent\ContextInterface;
+use \Magento\Framework\View\Element\UiComponentFactory;
+use \Magento\Ui\Component\Listing\Columns\Column;
+use \Magento\Framework\Api\SearchCriteriaBuilder;
+
+class CollectionName extends Column
+{
+    protected $_orderRepository;
+    protected $_searchCriteria;
+
+    public function __construct(ContextInterface $context, UiComponentFactory $uiComponentFactory, OrderRepositoryInterface $orderRepository, SearchCriteriaBuilder $criteria, array $components = [], array $data = [])
+    {
+        $this->_orderRepository = $orderRepository;
+        $this->_searchCriteria  = $criteria;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+
+                $order  = $this->_orderRepository->get($item["entity_id"]);
+                $status = $order->getData("pronto_order_number");
+
+                // if($status){
+                //     $message = 'Yes';
+                // }
+                // else{
+                //     $message = 'No';
+                // }
+
+                $item[$this->getData('name')] = $status;
+            }
+        }
+
+        return $dataSource;
+    }
+}
