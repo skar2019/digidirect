3e14de4702dd63a1368693ea14e087ac1953714d
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-07-31 20:53:01 +0800

    PA API On PDP

--- app/code/Digidirect/ParticularAudienceAPI/Block/Widget/Homepage/BlueRightSide.php
+++ app/code/Digidirect/ParticularAudienceAPI/Block/Widget/Homepage/BlueRightSide.php
@@ -112,10 +112,10 @@ class BlueRightSide extends \Magento\Framework\View\Element\Template implements 
     }
 
     function console_log($output, $with_script_tags = true) {
-        $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
-            ');';
+        $js_code = "console.log(" . json_encode($output, JSON_HEX_TAG) .
+            ");";
         if ($with_script_tags) {
-            $js_code = '<script>' . $js_code . '</script>';
+            $js_code = "<script>" . $js_code . "</script>";
         }
         echo $js_code;
     }
