6421c00d36a4c32b2f3d8f038b08a0aa5db4d729
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-07-31 10:42:47 +0800

    remvoe csp

--- app/etc/config.php
+++ app/etc/config.php
@@ -327,7 +327,7 @@ return [
         'Magento_Contact' => 1,
         'Magento_Cookie' => 1,
         'Magento_Cron' => 1,
-        'Magento_Csp' => 1,
+        'Magento_Csp' => 0,
         'Magento_CurrencySymbol' => 1,
         'Magento_CustomAttributeManagement' => 1,
         'Magento_AdvancedCheckout' => 1,
