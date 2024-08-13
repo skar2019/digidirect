b085b7c369e809fe17f0d3755e770f4126728e7f
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-08-13 13:35:22 +0800

    PA API On PDP

--- app/code/Digidirect/ParticularAudienceAPI/Controller/Index/FrequentlyBoughtWith.php
+++ app/code/Digidirect/ParticularAudienceAPI/Controller/Index/FrequentlyBoughtWith.php
@@ -4,57 +4,61 @@ namespace Digidirect\ParticularAudienceAPI\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\LocalizedException;
 
-class FrequentlyBoughtWith extends Action implements HttpGetActionInterface {
+class FrequentlyBoughtWith extends Action {
     
-   private $checkoutSession;
+    private $checkoutSession;
 
-   private $cartRepository;
+    private $cartRepository;
 
-   private $productRepository;
+    private $productRepository;
 
-   private $json;
+    private $json;
 
-   private $configurableType;
+    private $configurableType;
 
-   public function __construct(
-       Context $context,
-       \Magento\Framework\Serialize\Serializer\Json $json,
-       \Magento\Checkout\Model\SessionFactory $checkoutSession,
-       \Magento\Quote\Api\CartRepositoryInterface $cartRepository,
-       \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-       \Magento\ConfigurableProduct\Model\Product\Type\Configurable $configurableType
-   ) {
-       $this->checkoutSession = $checkoutSession;
-       $this->cartRepository = $cartRepository;
-       $this->productRepository = $productRepository;
-       $this->json = $json;
-       $this->configurableType = $configurableType;
-       parent::__construct($context);
-   }
+    protected $logger;
 
-   /**
+    public function __construct(
+        Context $context,
+        \Magento\Framework\Serialize\Serializer\Json $json,
+        \Magento\Checkout\Model\SessionFactory $checkoutSession,
+        \Magento\Quote\Api\CartRepositoryInterface $cartRepository,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\ConfigurableProduct\Model\Product\Type\Configurable $configurableType,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->cartRepository = $cartRepository;
+        $this->productRepository = $productRepository;
+        $this->json = $json;
+        $this->configurableType = $configurableType;
+        $this->logger = $logger;
+        parent::__construct($context);
+    }
+
+    /**
     * @return ResultInterface
     * @throws LocalizedException
     */
     public function execute()
     {
-         $productIds = $this->getRequest()->getParam('productIds');
+        $productIds = $this->getRequest()->getParam('productIds');
 
-         $session = $this->checkoutSession->create();
-         $quote = $session->getQuote();
+        $session = $this->checkoutSession->create();
+        $quote = $session->getQuote();
 
-         foreach($productIds as $item) {
-             $product = $this->productRepository->getById($item);
-             $quote->addProduct($product, 1);
-         }
+        foreach($productIds as $item) {
+            $this->logger->info("item: " . $item);
+            $product = $this->productRepository->getById($item);
+            $quote->addProduct($product, 1);
+        }
 
-         $this->cartRepository->save($quote);
-         $session->replaceQuote($quote)->unsLastRealOrderId();
+        $this->cartRepository->save($quote);
+        $session->replaceQuote($quote)->unsLastRealOrderId();
 
-         return true;
+        return true;
     }
-}
+    }
