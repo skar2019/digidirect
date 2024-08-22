9dc4c0297e2db9f2094dd8651a11781a6352ea44
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-08-22 18:26:20 +0800

    PA API Widget On PDP

--- app/code/Digidirect/ParticularAudienceAPI/Block/TestPdp.php
+++ app/code/Digidirect/ParticularAudienceAPI/Block/TestPdp.php
@@ -9,6 +9,7 @@ class TestPdp extends \Magento\Framework\View\Element\Template
     protected $_registry;
   
     public function __construct(
+        \Magento\Backend\Block\Template\Context $context, 
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\Registry $registry,
         array $data = []
