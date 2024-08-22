47d0fd89e192b3c4c5b6286d0d506802bc6b832a
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-08-22 16:42:41 +0800

    PA API Widget On PDP

--- app/code/Digidirect/ParticularAudienceAPI/Controller/Index/ProductPage.php
+++ app/code/Digidirect/ParticularAudienceAPI/Controller/Index/ProductPage.php
@@ -20,6 +20,8 @@ class ProductPage extends Action implements HttpPostActionInterface {
 
     protected $jsonSerializer;
     
+    protected $_registry;
+    
     public function __construct(
         Context $context,
         \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
@@ -27,12 +29,14 @@ class ProductPage extends Action implements HttpPostActionInterface {
         \Magento\Variable\Model\Variable $variable,
         \Magento\Framework\HTTP\Client\Curl $curl,
         \Magento\Framework\Serialize\Serializer\Json $jsonSerializer,
+        \Magento\Framework\Registry $registry
     ) {
         $this->_resultJsonFactory = $resultJsonFactory;
         $this->logger = $logger;
         $this->variable = $variable;
         $this->curl = $curl;
         $this->jsonSerializer = $jsonSerializer;
+        $this->_registry = $registry;
         parent::__construct($context);
     }
 
@@ -44,9 +48,16 @@ class ProductPage extends Action implements HttpPostActionInterface {
     {
         $result = $this->_resultJsonFactory->create();
         $customerId = $this->getRequest()->getParam('customerId');
+        $currentProductId = $this->getCurrentProduct()->getId();
         
         $variableData = $this->variable->loadByCode('pa_bearer_token');
         $bearerToken = $variableData->getValue('text');
+        
+        if ($currentProductId) {
+            $refIdParam = "&refId=".$currentProductId;
+        } else {
+            $refIdParam = "";
+        }
         
         if ($customerId) {
             $customerIdParam = "&customerId=".$customerId;
@@ -54,7 +65,7 @@ class ProductPage extends Action implements HttpPostActionInterface {
             $customerIdParam = "";
         }
 
-        $getRecommendationsUrl = "https://api-recs.particularaudience.com/3.0/recommendations?currentUrl=https://www.digidirect.com.au/pa-digi-products-pdp&expandProductDetails=true".$customerIdParam;
+        $getRecommendationsUrl = "https://api-recs.particularaudience.com/3.0/recommendations?currentUrl=https://www.digidirect.com.au/pa-digi-products-pdp&expandProductDetails=true".$refIdParam.$customerIdParam;
         //$this->logger->info("getRecommendationsUrl: " . $getRecommendationsUrl);ß
         $this->curl->addHeader("Content-Type", "application/json");
         $this->curl->addHeader("Authorization", "Bearer " . $bearerToken);
@@ -65,5 +76,9 @@ class ProductPage extends Action implements HttpPostActionInterface {
 
         $result->setData($getRecommendationsResultJson);
         return $result;
+    }
+    
+    public function getCurrentProduct(){         
+        return $this->_registry->registry('current_product');
     }
 }
