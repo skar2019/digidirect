2cc0deed1fadf2c3726339e7ae01e68638b2cfcb
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-09-07 00:54:38 +0800

    Test PA Events API

---
+++ app/code/Digidirect/ParticularAudienceAPI/view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+/* eslint no-unused-vars: [1] */
+
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/minicart': {
+                'ParticularAudienceAPI/js/view/minicart-mixin': true
+            }
+        }
+    }
+};
