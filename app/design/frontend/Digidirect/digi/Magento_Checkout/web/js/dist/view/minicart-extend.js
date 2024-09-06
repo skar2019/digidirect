956db29a6818ce2bb62182fda690c44d47c8703f
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-09-07 00:11:31 +0800

    Test PA Events API

--- app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/dist/view/minicart-extend.js
+++ app/design/frontend/Digidirect/digi/Magento_Checkout/web/js/dist/view/minicart-extend.js
@@ -90,6 +90,60 @@ define(['module', 'exports', 'jquery', 'ko', 'digidirectStoreCheckout', 'mCustom
         
         checkoutEvent: function checkoutEvent() {
             
+            /*function getCookie(name) {
+                const value = `; ${document.cookie}`;
+                const parts = value.split(`; ${name}=`);
+                if (parts.length === 2) return parts.pop().split(';').shift();
+            }
+
+            var customerId = getCookie('PAC');
+            var sessionId = getCookie('pa_session_id');
+            var currentUrl = window.location.href;
+
+            var configCustomerId;
+            var configSessionId;
+
+            configCustomerId = customerId;
+            configSessionId = sessionId;*/
+            
+            console.log("Test Trigger On checkoutEvent()!");
+            var widgetId = '631dfdd4-dc01-ef11-abf3-02bf4bf6447c';
+
+            var date = new Date();
+            var now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
+                    date.getUTCDate(), date.getUTCHours(),
+                    date.getUTCMinutes(), date.getUTCSeconds());
+            var products = [];
+
+            _jquery("#mini-cart .product-item").each(function() {
+
+                var productItem;
+
+                var refId = _jquery(this).find("[pa-option-label='refId']").attr("pa-option-value"); 
+                var quantity = _jquery(this).find(".cart-item-qty").attr("data-item-qty"); 
+                var routeId = _jquery(this).find("[pa-option-label='routeId']").attr("pa-option-value"); 
+                var widgetId = _jquery(this).find("[pa-option-label='widgetId']").attr("pa-option-value"); 
+
+                productItem = {"refId": refId, "quantity": quantity, "routeId": routeId, "widgetId": widgetId};
+                products.push(productItem);
+
+            });
+
+            const checkoutData = {
+                //customerId: configCustomerId,
+                //sessionId: configSessionId,
+                events: [
+                    {
+                        currentUrl: currentUrl,
+                        eventTime: date.toISOString(),
+                        products: products,
+                        widgetId: widgetId,
+                        routeId: routeId
+                    }
+                ]
+            };
+
+            console.log("checkoutData", JSON.stringify(checkoutData));
         }
     };
 
