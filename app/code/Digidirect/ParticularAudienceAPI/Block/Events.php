a7a821f5075c70077ec0122b1b43524a2c0edd3e
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-09-06 21:20:57 +0800

    Test PA Events API

--- app/code/Digidirect/ParticularAudienceAPI/Block/Events.php
+++ app/code/Digidirect/ParticularAudienceAPI/Block/Events.php
@@ -7,19 +7,23 @@ class Events extends \Magento\Framework\View\Element\Template
     protected $logger;
     
     protected $_registry;
+    
+    protected $_urlInterface;
   
     public function __construct(
         \Magento\Backend\Block\Template\Context $context, 
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\Registry $registry,
+        \Magento\Framework\UrlInterface $urlInterface, 
         array $data = []
     ) {        
         $this->logger = $logger;
         $this->_registry = $registry;
+        $this->_urlInterface = $urlInterface;
         parent::__construct($context, $data);
     }
     
-    public function getCurrentProduct(){         
-        return $this->_registry->registry('current_product');
+    public function getCurrentUrl() {
+        return rtrim($this->_urlInterface->getCurrentUrl(), '/');
     }
 }
