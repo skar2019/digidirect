7d923862ad7a70020dac66ab26b9f696166c38be
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-09-07 14:06:26 +0800

    Test PA Events API

--- app/code/Digidirect/ParticularAudienceAPI/view/frontend/web/js/view/minicart-mixin.js
+++ app/code/Digidirect/ParticularAudienceAPI/view/frontend/web/js/view/minicart-mixin.js
@@ -12,7 +12,7 @@ define([
                 
                 console.log("checkoutEvent On JS Mixin");
                 
-                function getCookie(name) {
+                /*function getCookie(name) {
                     const value = `; ${document.cookie}`;
                     const parts = value.split(`; ${name}=`);
                     if (parts.length === 2) return parts.pop().split(';').shift();
@@ -68,7 +68,7 @@ define([
                 
                 if (products) {
                     console.log("checkoutData", JSON.stringify(checkoutData));
-                }
+                }*/
                 
             }
         });
