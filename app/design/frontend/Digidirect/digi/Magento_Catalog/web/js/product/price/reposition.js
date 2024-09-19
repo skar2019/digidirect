443cc783526912ff38e98d551386b555c5d661ae
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-09-19 14:36:43 +0800

    Test Fix digiRent Rendering On PDP

--- app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/web/js/product/price/reposition.js
@@ -26,9 +26,16 @@ define([
         $('#product-addtocart-button').removeAttr("title");
     });
     
+    $('.studio19-wrapper').attr("style", "display:none !important;");
     $('.s19-price-per-period').insertBefore($('.s19-component'));
     $('.s19-apply-btn').insertBefore($('.s19-learn-btn'));
     $('.s19-actions').insertAfter($('.s19-min-period'));
+    
+    setTimeout(function() {
+        if ($('.studio19-wrapper').children().length > 0) {
+            $('.studio19-wrapper').removeAttr('style');
+        }
+    }, 2000);
     
     var win = $(this); //this = window
     if (win.width() >= 768) {
