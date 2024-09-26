cc44e119a1abcce7cfe22f61312c88954e357c70
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-09-26 18:51:32 +0800

    Test Overdose SEO H1 Fix

--- app/code/Digidirect/Theme/Block/Html/Title.php
+++ app/code/Digidirect/Theme/Block/Html/Title.php
@@ -117,12 +117,15 @@ class Title extends Template
         try {
             $currentCategory = $this->_registry->registry('current_category');
             if ($currentCategory) {
+                $this->logger->info("currentCategory is true!");
                return $currentCategory; 
             }
         }
         catch(Exception $e) {
             return false;
+            $this->logger->info("currentCategory is false!");
         }
         return false;
+        $this->logger->info("currentCategory is false!");
     }
 }
