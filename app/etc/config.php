ad12ea0c5489943449b3b5a85918aa6a17e960e7
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-09-27 10:10:01 +0800

    store

--- app/etc/config.php
+++ app/etc/config.php
@@ -67,6 +67,14 @@ return [
                 'root_category_id' => '2',
                 'default_store_id' => '13',
                 'code' => 'retail_store'
+            ],
+            10 => [
+                'group_id' => '10',
+                'website_id' => '7',
+                'name' => 'Retail Stores Store',
+                'root_category_id' => '2',
+                'default_store_id' => '19',
+                'code' => 'retail_stores_store'
             ]
         ],
         'stores' => [
