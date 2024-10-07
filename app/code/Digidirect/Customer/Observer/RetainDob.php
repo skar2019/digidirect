e02fb29670cabc2115b4ab6fe237afd3130ed20f
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-10-07 14:34:37 +0800

    Test Fix On Date Of Birth Field

--- app/code/Digidirect/Customer/Observer/RetainDob.php
+++ app/code/Digidirect/Customer/Observer/RetainDob.php
@@ -8,8 +8,7 @@ use Magento\Framework\Event\ObserverInterface;
 /**
  *  Revert Dob value in case Customer already set the Dob value before.
  */
-class RetainDob implements ObserverInterface
-{
+class RetainDob implements ObserverInterface {
     /**
      * Revert Dob value in case Customer already set the Dob value before.
      *
@@ -21,9 +20,7 @@ class RetainDob implements ObserverInterface
         /** @var  $customer \Magento\Customer\Model\Customer */
         $customer = $observer->getEvent()->getCustomer();
 
-        if ($customer->getOrigData('dob')
-            && $customer->getData('dob') !== $customer->getOrigData('dob')
-        ) {
+        if ($customer->getOrigData('dob') && $customer->getData('dob') !== $customer->getOrigData('dob')) {
             $customer->setData('dob', $customer->getOrigData('dob'));
         }
 
