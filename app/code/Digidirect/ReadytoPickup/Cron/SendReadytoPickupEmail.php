44608f36e10e0f4323e82d355fa1058c07b11d8a
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-10-09 12:03:33 +0800

    Test Fix Ready To Pickup Email Issue

--- app/code/Digidirect/ReadytoPickup/Cron/SendReadytoPickupEmail.php
+++ app/code/Digidirect/ReadytoPickup/Cron/SendReadytoPickupEmail.php
@@ -1,33 +1,27 @@
 <?php
+
 namespace Digidirect\ReadytoPickup\Cron;
 
 use Psr\Log\LoggerInterface;
 use Digidirect\ReadytoPickup\Helper\ReadytoPickup;
 
-class SendReadytoPickupEmail
-{
-    /**
-     * @var LoggerInterface
-     */
+class SendReadytoPickupEmail {
+    
     protected $logger;
-
-    /**
-     * @var Inventory
-     */
+    
     protected $helper;
     
-    public function __construct(
-            LoggerInterface $logger,
-            ReadytoPickup $helper)
-    {
+    public function __construct (
+        LoggerInterface $logger,
+        ReadytoPickup $helper
+    ){
         $this->logger = $logger;
         $this->helper = $helper;
     }
 
-    public function execute()
-    {
-        $this->helper->sendReadytoPickupEmail();
-
+    public function execute() {
+        $this->logger->info("send_readytopickup_email");
+        //$this->helper->sendReadytoPickupEmail();
     }
     
 }
