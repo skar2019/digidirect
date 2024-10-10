9e66f92a66788a83624230126ffee155af192d53
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-10-10 12:08:14 +0800

    final price debug

--- app/code/Digidirect/Pronto/Helper/ProductEntHelper.php
+++ app/code/Digidirect/Pronto/Helper/ProductEntHelper.php
@@ -698,9 +698,9 @@ class ProductEntHelper extends AbstractHelper
             if($seller == '20329' || $seller == 20329)
             {
 
-                $final_price = $product->getData('final_price');
+                $finalprice4 = $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue(); //tested 10-10-2024
 
-                $product->setCustomAttribute('custom_final_price', $final_price);
+                $product->setCustomAttribute('custom_final_price', $finalprice4);
                 $this->productRepository->save($product);
 
             }
@@ -722,14 +722,17 @@ class ProductEntHelper extends AbstractHelper
             if($seller == '20329' || $seller == 20329)
             {
                 echo $product->getSku();
+                echo "<br /> \n";
                 $final_price = $product->getData('final_price');
                 echo "final price 1: ".$final_price;
                 $finalprice2 = $product->getPriceInfo()->getPrice('final_price')->getValue();
                 echo "<br /> \n";
                 echo "final price 2: ".$finalprice2;
+
                 $finalprice3 = $product->getFinalPrice();
                 echo "<br /> \n";
                 echo "final price 3: ".$finalprice3;
+
                 $finalprice4 = $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
                 echo "<br /> \n";
                 echo "final price 4: ".$finalprice4;
