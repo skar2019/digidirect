2907b7500eb89817d8d7297bae62e8e2f009e12f
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-10-18 11:29:45 +0800

    Test PA Optimization

---
+++ app/code/Digidirect/ParticularAudienceAPI/Block/Widget/TestPAOptimized.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Digidirect\ParticularAudienceAPI\Block\Widget;
+
+use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+use Magento\Framework\Stdlib\Cookie\PublicCookieMetadata;
+use Magento\Framework\Stdlib\CookieManagerInterface;
+
+
+class TestPAOptimized extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
+{
+    
+    public const PA_CUSTOMER_ID = "pa_customer_id";
+    
+    public const PA_SESSION_ID = "pa_session_id";
+    
+    protected $_cookieManager;
+    
+    protected $_cookieMetadataFactory;
+    
+    protected $_sessionManager;
+    
+    protected $_objectManager;
+    
+    protected $_remoteAddressInstance;
+    
+    protected $_template = 'Digidirect_ParticularAudienceAPI::widget/test-pa-optimized.phtml';
+    
+    protected $variable;
+  
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,  
+        CookieManagerInterface $cookieManager,
+        CookieMetadataFactory $cookieMetadataFactory,
+        SessionManagerInterface $sessionManager,
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Variable\Model\Variable $variable,
+        array $data = []
+    ) {        
+        $this->_cookieManager = $cookieManager;
+        $this->_cookieMetadataFactory = $cookieMetadataFactory;
+        $this->_sessionManager = $sessionManager;
+        $this->_objectManager = $objectManager;
+        $this->_remoteAddressInstance = $this->_objectManager->get(
+            'Magento\Framework\HTTP\PhpEnvironment\RemoteAddress'
+        );
+        $this->variable = $variable;
+        parent::__construct($context, $data);
+    }
+    
+    public function getCookie($cookie) {
+        return $this->_cookieManager->getCookie($cookie);
+    }
+    
+    public function setCookie($cookie, $value) {
+        $metadata = $this->_cookieMetadataFactory
+            ->createPublicCookieMetadata()
+            ->setPath($this->_sessionManager->getCookiePath())
+            ->setDomain($this->_sessionManager->getCookieDomain());
+
+        $this->_cookieManager->setPublicCookie(
+            $cookie,
+            $value,
+            $metadata
+        );
+    }
+    
+    public function getBearerToken() {
+        $variableData = $this->variable->loadByCode('pa_bearer_token');
+        $bearerToken = $variableData->getValue('text');
+        return $bearerToken;
+    }
+    
+    public function getConfig() {
+        
+        $customerId = $this->getCookie(self::PA_CUSTOMER_ID);
+        $sessionId = $this->getCookie(self::PA_SESSION_ID);
+        
+        if (!$customerId || !$sessionId) {
+            $bearerToken = $this->getBearerToken();
+            if ($customerId) {
+                $customerIdParam = "?customerId=".$customerId;
+            } else {
+                $customerIdParam = "";
+            }
+
+            $getConfigUrl = "https://api-recs.particularaudience.com/3.0/config".$customerIdParam;
+            $this->curl->addHeader("Content-Type", "application/json");
+            $this->curl->addHeader("Authorization", "Bearer " . $bearerToken);
+            $this->curl->get($getConfigUrl);
+
+            $getConfigResult = $this->curl->getBody();
+            $getConfigResultJson = $this->jsonSerializer->unserialize($getConfigResult);
+
+            $this->setCookie(self::PA_CUSTOMER_ID, $getConfigResultJson['payload']['customerId']);
+            $this->setCookie(self::PA_SESSION_ID, $getConfigResultJson['payload']['session']['id']);
+            
+            return $getConfigResultJson;
+        }
+        
+        return $customerId;
+        
+    }
+    
+}
