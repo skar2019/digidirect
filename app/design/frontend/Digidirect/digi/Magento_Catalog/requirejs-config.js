efed5b5d54f833fb77f1b018cb2eaef88a90f2e2
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-10-25 14:32:29 +0800

    Test Optimize Page

--- app/design/frontend/Digidirect/digi/Magento_Catalog/requirejs-config.js
+++ app/design/frontend/Digidirect/digi/Magento_Catalog/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     config: {
         mixins: {
             'Magento_Catalog/js/product/list/toolbar': {
-                'Magento_Catalog/js/product/list/toolbar-mixin': true
+                'Magento_Catalog/js/product/list/toolbar-mixin': false
             }
         }
     },
