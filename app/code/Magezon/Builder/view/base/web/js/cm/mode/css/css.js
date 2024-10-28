b20914c2a2cfa7d9bd203415ecace5eb0a481d9b
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-10-28 22:34:13 +0800

    Revert

--- app/code/Magezon/Builder/view/base/web/js/cm/mode/css/css.js
+++ app/code/Magezon/Builder/view/base/web/js/cm/mode/css/css.js
@@ -668,7 +668,7 @@ CodeMirror.defineMode("css", function(config, parserConfig) {
     "small", "small-caps", "small-caption", "smaller", "soft-light", "solid", "somali",
     "source-atop", "source-in", "source-out", "source-over", "space", "space-around", "space-between", "space-evenly", "spell-out", "square",
     "square-button", "start", "static", "status-bar", "stretch", "stroke", "sub",
-    "subpixel-antialiased", "super", "sw-resize", "symbolic", "symbols", "system-ui", "table",
+    "subpixel-antialiased", "super", "sw-resize", "symbolic", "symbols", "Poppins", "table",
     "table-caption", "table-cell", "table-column", "table-column-group",
     "table-footer-group", "table-header-group", "table-row", "table-row-group",
     "tamil",
