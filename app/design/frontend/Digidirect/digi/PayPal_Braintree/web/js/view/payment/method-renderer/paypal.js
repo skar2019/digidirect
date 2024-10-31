5ff414508ddad8b7e23e1fa3a59c2fbada2fe1be
Clint Angelo 'Tec' Mercado <clint.tec.mercado@gmail.com>
2024-10-31 11:16:03 +0800

    check undefined

--- app/design/frontend/Digidirect/digi/PayPal_Braintree/web/js/view/payment/method-renderer/paypal.js
+++ app/design/frontend/Digidirect/digi/PayPal_Braintree/web/js/view/payment/method-renderer/paypal.js
@@ -553,11 +553,10 @@ define([
          */
         getShippingAddress: function () {
             var address = quote.shippingAddress();
-            console.log(address);
 
             return {
                 recipientName: address.firstname + ' ' + address.lastname,
-                line1: address.street[0],
+                line1: typeof address.street[0] === 'undefined' ? ' ' : address.street[0],
                 line2: typeof address.street[2] === 'undefined' ? address.street[1] : address.street[1] + ' ' + address.street[2],
                 city: address.city,
                 countryCode: address.countryId,
