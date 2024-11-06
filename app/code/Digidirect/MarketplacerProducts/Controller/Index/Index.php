d7c7bc80365fb4fa00e2cb714c210f3b020b4202
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-11-06 14:39:33 +0800

    digiMarket Meta

--- app/code/Digidirect/MarketplacerProducts/Controller/Index/Index.php
+++ app/code/Digidirect/MarketplacerProducts/Controller/Index/Index.php
@@ -21,8 +21,8 @@ class Index extends \Magento\Framework\App\Action\Action
 
     public function execute() {
         $resultPage = $this->_resultPageFactory->create();
-        $resultPage->getConfig()->getTitle()->set("Buy & Sell Pre-Owned Photography Gear at digiMarket now!");
-        $resultPage->getConfig()->setDescription("Discover digiMarket for buying and selling pre-owned camera gear. Great deals on quality equipment. Explore digiDirect's marketplace today!");
+        $resultPage->getConfig()->getTitle()->set("Explore digiMarket for a wide range of quality gear and equipment.");
+        $resultPage->getConfig()->setDescription("Explore digiMarket for a wide range of quality gear and equipment. Shop now and enjoy our selection of unique finds at digiDirect’s marketplace today!");
         return $resultPage;
     }
 }
