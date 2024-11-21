dfc4fc7c2eee5766ce556ed42ea36aaf132834b7
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-11-21 23:50:44 +0800

    Test PA

--- app/code/Digidirect/ParticularAudienceAPI/Block/Widget/TestPA.php
+++ app/code/Digidirect/ParticularAudienceAPI/Block/Widget/TestPA.php
@@ -5,12 +5,16 @@ namespace Digidirect\ParticularAudienceAPI\Block\Widget;
 
 class TestPA extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
 {
+    protected $customer;
+    
     protected $_template = 'Digidirect_ParticularAudienceAPI::widget/test-pa.phtml';
   
     public function __construct(
-        \Magento\Backend\Block\Template\Context $context,    
+        \Magento\Backend\Block\Template\Context $context, 
+        \Magento\Customer\Model\Session $customerSession,   
         array $data = []
     ) {        
+        $this->customer = $customerSession;
         parent::__construct($context, $data);
     }
 }
