269ab6e47004ad2bc1c645576e4be9e97f1afa11
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-11-21 23:29:23 +0800

    Test PA

--- app/code/Digidirect/ParticularAudienceAPI/Block/Widget/TestPA.php
+++ app/code/Digidirect/ParticularAudienceAPI/Block/Widget/TestPA.php
@@ -5,18 +5,12 @@ namespace Digidirect\ParticularAudienceAPI\Block\Widget;
 
 class TestPA extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
 {
-    protected $variable;
-    
-    protected $_urlInterface;
-    
     protected $_template = 'Digidirect_ParticularAudienceAPI::widget/test-pa.phtml';
   
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,    
-        \Magento\Framework\UrlInterface $urlInterface, 
         array $data = []
     ) {        
-        $this->_urlInterface = $urlInterface;
         parent::__construct($context, $data);
     }
 }
