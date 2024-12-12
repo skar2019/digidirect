25c13569653aafd2583d4344452de0bcd7453e44
Rondel Dalumpines <rondel.d@digidirect.com.au>
2024-12-12 17:16:21 +0800

    Test digiDeals Products Category

--- app/code/Digidirect/DigiDeals/Controller/Index/Products.php
+++ app/code/Digidirect/DigiDeals/Controller/Index/Products.php
@@ -8,23 +8,47 @@ class Products extends \Magento\Framework\App\Action\Action
     
     protected $logger;
     
+    protected $categoryFactory;
+    
+    protected $categoryLinkRepository;
+    
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Psr\Log\LoggerInterface $logger,
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollection
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollection,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        \Magento\Catalog\Api\CategoryLinkRepositoryInterface $categoryLinkRepository
     )
     {
         $this->logger = $logger;
         $this->_productCollection= $productCollection;
+        $this->categoryFactory = $categoryFactory;
+        $this->categoryLinkRepository = $categoryLinkRepository;
         parent::__construct($context);
     }
 
     public function execute()
     {
-        $productCollection = $this->getProductCollections();
-        foreach ($productCollection as $product) {
-            print_r($product->getData('sku') . ',');
+        $categoryId = 4080; //digiDeals
+        $category = $this->categoryFactory->create()->load($categoryId);
+        $categoryProducts = $category->getProductCollection()->addAttributeToSelect('sku');
+
+        foreach ($categoryProducts as $product) {
+            $sku = $product->getSku();
+            try {
+                $isProductUnassigned = $this->categoryLinkRepository->deleteByIds($categoryId, $sku);
+            } catch (Exception $ex) {
+                $this->logger->info("digiDeals, " . $ex->getMessage());
+                continue;
+            }
         }
+        
+        $categoryLinkRepository->assignProductToCategories(148264, $categoryId);
+        /*$productCollection = $this->getProductCollections();
+        foreach ($productCollection as $product) {
+            //print_r($product->getData('sku') . ',');
+            $categoryLinkRepository->assignProductToCategories($product->getData('sku'), $categoryId);
+        }*/
     }
     
     public function getProductCollections()
