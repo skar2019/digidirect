a7aa6deaf11a3782852a8ab1a7e1c7008036cc4d
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-01-13 13:07:45 +0800

    Test Category Column On Product Grid

---
+++ app/code/Digidirect/Catalog/Model/Category/CategoryList.php
@@ -0,0 +1,26 @@
+<?php
+namespace Digidirect\Catalog\Model\Category;
+
+class CategoryList implements \Magento\Framework\Option\ArrayInterface
+{
+    protected  $collectionFactory;
+    protected $_categoryCollectionFactory;
+    public function __construct(
+    \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collectionFactory
+    ) {
+        $this->_categoryCollectionFactory = $collectionFactory;
+    }
+    public function toOptionArray($addEmpty = true)
+    {
+        $collection = $this->_categoryCollectionFactory->create();
+        $collection->addAttributeToSelect("name");
+        $options = [];
+        if ($addEmpty) {
+            $options[] = ["label" => __('-- Please Select a Category --'), "value" => ''];
+        }
+        foreach ($collection as $category) {
+            $options[] = ['label' => $category->getName(), 'value' => $category->getId()];
+        }
+        return $options;
+    }
+}
