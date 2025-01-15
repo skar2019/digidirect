65c483c1448670f15a4405f4e646bd5e87cf56a1
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-01-15 11:51:57 +0800

    Test Fix Category Column For Admin Product Grid

--- app/etc/config.php
+++ app/etc/config.php
@@ -775,7 +775,6 @@ return [
         'WeltPixel_GA4' => 0,
         'ZV_SeoCompatible' => 1,
         'Zendesk_Zendesk' => 0,
-        'Zip_ZipPayment' => 1,
-        'Digidirect_ProductGridExport' => 0
+        'Zip_ZipPayment' => 1
     ]
 ];
