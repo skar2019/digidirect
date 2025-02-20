5de1be073f75a1a829160f535f4d73cacdda5ff8
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-02-21 00:26:21 +0800

    Redeploy

--- app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
+++ app/code/Digidirect/CollectStoreLocator/Observer/StoreLocator/AddExtraInfoToStoreLocatorItems.php
@@ -86,6 +86,7 @@ class AddExtraInfoToStoreLocatorItems implements ObserverInterface
 
             $id = $storeData['entity_id'];
             $qty = 0;
+            //$items[$key]['click_and_collect'] = true;
 
             if (empty($places[$id])) {
                 continue;
