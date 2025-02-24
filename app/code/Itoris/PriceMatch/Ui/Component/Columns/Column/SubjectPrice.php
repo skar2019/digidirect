134a49a69e08ffdc9ecedc8c8b6a17947ee3a6f5
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-02-24 19:26:24 +0800

    Test Fix Price Match

--- app/code/Itoris/PriceMatch/Ui/Component/Columns/Column/SubjectPrice.php
+++ app/code/Itoris/PriceMatch/Ui/Component/Columns/Column/SubjectPrice.php
@@ -53,7 +53,12 @@ class SubjectPrice extends \Magento\Ui\Component\Listing\Columns\Column
         if (isset($dataSource['data']['items'])) {
             $fieldName = $this->getData('name');
             foreach ($dataSource['data']['items'] as & $item) {
-                $fPrice = $item['final_price'];
+                $fPrice = $this->priceCurrency->format(
+                    $item['final_price'],
+                    false,
+                    \Magento\Framework\Pricing\PriceCurrencyInterface::DEFAULT_PRECISION,
+                    $item['store_id']
+                );
 
                 if (isset($item[$fieldName])) {
                     $item[$fieldName] = $fPrice;
