f0d171b19308ab27c4688a4eef5defcb9d4afb85
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-03-10 09:33:24 +0800

    SEO Issue Fixes (Meta Description & Robots)

---
+++ app/code/Digidirect/Catalog/Observer/OverrideMetaTags.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Digidirect\Catalog\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class OverrideMetaTags implements ObserverInterface {
+
+    public function execute(\Magento\Framework\Event\Observer $observer){
+        $product = $observer->getProduct();
+        if (!$product->getMetaDescription()) {
+            $description = html_entity_decode($product->getDescription());
+            $product->setMetaDescription(substr(htmlentities(strip_tags($description)), 0, 160));
+        }
+        //$product->setMetaDescription(strip_tags($product->getMetaDescription()));
+    }
+}
