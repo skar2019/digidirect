c9a05a8010279396a0b853830f4c7de98f51bdf8
clintmercado <clint@kayweb.com.au>
2025-03-12 23:47:10 +0800

    getOriginalPrice

--- app/code/Digidirect/Pronto/Helper/TestPronto.php
+++ app/code/Digidirect/Pronto/Helper/TestPronto.php
@@ -1434,7 +1434,7 @@ class TestPronto extends AbstractHelper
                 $skus = array();
                 $productSku = "";
                 $digiProtect = "";
-                $price = (double) $item->getBasePriceInclTax();
+                $price = (double) $item->getOriginalPrice();//$item->getBasePriceInclTax();
                 $qty = (double) $item->getQtyOrdered();
                 $discount = (double) $item->getDiscountAmount();
                 echo "discount - " .$discount ." <br/>";
