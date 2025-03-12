28cf42d0c0d644c18ce2df9af23d7792f4b3d2f7
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-03-12 19:48:38 +0800

    Test SEO Fix For Blog Tag Pages

--- app/code/Digidirect/Blog/Block/TagPost.php
+++ app/code/Digidirect/Blog/Block/TagPost.php
@@ -15,10 +15,10 @@ class TagPost extends Blog implements IdentityInterface
     {
         $collection = parent::prepareCollection();
         //
-        if ($this->getTag()->getId()) {
+        /*if ($this->getTag()->getId()) {
             $collection->addFilterByTagId($this->getTag()->getId());
             $this->arrowHelper->setTagCondition($this->getTag()->getId());
-        }
+        }*/
         return $collection;
     }
 
