11a8931423f706d12ae9c1abffa2c13bc309d03a
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-05-06 10:44:24 +0800

    Update digiMarket Internal Search Placeholder

--- app/design/frontend/Digidirect/digi/Algolia_AlgoliaSearch/web/js/instantsearch.js
+++ app/design/frontend/Digidirect/digi/Algolia_AlgoliaSearch/web/js/instantsearch.js
@@ -476,9 +476,18 @@ define([
              * searchBox
              * Docs: https://www.algolia.com/doc/api-reference/widgets/search-box/js/
              **/
+            
+            const currentURL = window.location.href;
+            
+            var searchPlaceholder = algoliaConfig.translations.searchFor;
+            
+            if (currentURL == "https://www.digidirect.com.au/digimarket") {
+                searchPlaceholder = "Shop over 10s and 1000s of products now!";
+            }
+            
             allWidgetConfiguration.searchBox = {
                 container  : instant_selector,
-                placeholder: algoliaConfig.translations.searchFor,
+                placeholder: searchPlaceholder,
                 showSubmit : false,
                 queryHook  : (inputValue, search) => {
                     if (
