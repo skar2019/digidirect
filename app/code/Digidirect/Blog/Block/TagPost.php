3995b54460abf5c479be4f241a0127b986269aa1
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-05-20 19:48:32 +0800

    Test Blog Tags Fix

--- app/code/Digidirect/Blog/Block/TagPost.php
+++ app/code/Digidirect/Blog/Block/TagPost.php
@@ -20,8 +20,8 @@ class TagPost extends Blog implements IdentityInterface
         
         $collection = parent::prepareCollection();
         //
-        $collection->addFilterByTagId(9);
-        $this->arrowHelper->setTagCondition(9);
+        $collection->addFilterByTagId($this->getTag()->getId());
+        $this->arrowHelper->setTagCondition($this->getTag()->getId());
         return $collection;
     }
 
