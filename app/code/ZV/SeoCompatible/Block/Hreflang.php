d4e75e172150797316387c5a1b77ecefa382d542
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-05-26 21:34:23 +0800

    Test SEO Fix (hreflang Issue)

--- app/code/ZV/SeoCompatible/Block/Hreflang.php
+++ app/code/ZV/SeoCompatible/Block/Hreflang.php
@@ -39,7 +39,7 @@ class Hreflang extends Template
 
             parse_str($urlComponents['query'], $params);
 
-            if (!empty($params['p']) || !empty($params['page'])) {
+            if (!empty($params['p'])) {
 
                 if (count($params) == 1) {
                     $this->pageConfig->setRobots("INDEX,FOLLOW");
@@ -49,12 +49,6 @@ class Hreflang extends Template
                     $page = ''; 
                 } else {
                     $page = '?p=' . $params['p']; 
-                }
-
-                if ($params['page'] == 1) {
-                    $page = ''; 
-                } else {
-                    $page = '?page=' . $params['page']; 
                 }
 
                 $canonical = $urlComponents['scheme'] . '://' . $urlComponents['host'] . $urlComponents['path'] . $page;
