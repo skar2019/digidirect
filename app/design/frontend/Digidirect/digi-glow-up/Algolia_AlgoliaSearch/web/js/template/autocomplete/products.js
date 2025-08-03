f0c6fe9eb81b0660265c18e36f3ebc9b6470d03b
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-08-03 12:59:50 +0800

    Glow Up Theme

---
+++ app/design/frontend/Digidirect/digi-glow-up/Algolia_AlgoliaSearch/web/js/template/autocomplete/products.js
@@ -0,0 +1,164 @@
+define([], function () {
+    return {
+
+        ////////////////////
+        //  Template API  //
+        ////////////////////
+
+        getNoResultHtml: function ({html}) {
+            return html`<p>${algoliaConfig.translations.noResults}</p>`;
+        },
+
+        getHeaderHtml: function () {
+            return "";
+        },
+
+        getItemHtml: function ({item, components, html}) {
+            return html`<a class="algoliasearch-autocomplete-hit"
+                           href="${item.url}"
+                           data-objectId="${item.objectID}"
+                           data-position="${item.position}"
+                           data-index="${item.__autocomplete_indexName}"
+                           data-queryId="${item.__autocomplete_queryID}">
+                <div class="thumb"><img src="${item.thumbnail_url || ''}" alt="${item.name || ''}"/></div>
+                <div class="info">
+                    ${this.safeHighlight(components, item, "name")}
+                    <div class="algoliasearch-autocomplete-category">
+                        ${this.getColorHtml(item, components, html)}
+                        ${this.getCategoriesHtml(item, components, html)}
+                    </div>
+
+                    ${this.getPricingHtml(item, html)}
+                </div>
+            </a>`;
+        },
+
+        getFooterHtml: function ({html, ...resultDetails}) {
+            return html`<div id="autocomplete-products-footer">
+                ${this.getFooterSearchLinks(html, resultDetails)}
+            </div>`;
+        },
+
+        ////////////////////
+        // Helper methods //
+        ////////////////////
+
+        getColorHtml: function(item, components, html) {
+            const highlight = this.safeHighlight(components, item, "color");
+            
+            return highlight 
+                ? html`<span class="color">color: ${highlight}</span>`
+                : "";
+        },
+
+        getCategoriesHtml: function(item, components, html) {
+            const highlight = this.safeHighlight(components, item, "categories_without_path", false);
+
+            return highlight 
+                ? html`<span>in ${highlight}</span>`
+                : "";
+        },
+
+        getOriginalPriceHtml: (item, html, priceGroup) => {
+            if (item['price'][algoliaConfig.currencyCode][priceGroup + '_original_formated'] == null) return "";
+
+            return html`<span class="before_special"> ${item['price'][algoliaConfig.currencyCode][priceGroup + '_original_formated']} </span>`;
+        },
+
+        getTierPriceHtml: (item, html, priceGroup) => {
+            if (item['price'][algoliaConfig.currencyCode][priceGroup + '_tier_formated'] == null) return "";
+
+            return html`<span class="tier_price"> As low as <span class="tier_value">${item['price'][algoliaConfig.currencyCode][priceGroup + '_tier_formated']}</span></span>`;
+        },
+
+        getPricingHtml: function(item, html) {
+            if (item['price'] == undefined) return "";
+
+            const priceGroup =  algoliaConfig.priceGroup || 'default';
+            
+            const formatter = new Intl.NumberFormat('en-AU', {
+                style: 'currency',
+                currency: 'AUD',
+                minimumFractionDigits: 2
+            });
+            
+            //custom_final_price
+            /*if (item['custom_final_price']) {
+                //Set price to custom_final_price
+                if ((item['custom_final_price'] < item['price']['AUD']['default']) && item['custom_final_price'] != 0) {
+                    item.customFinalPrice =  formatter.format(item['custom_final_price']);
+                    hasCustomFinalPrice = true;
+                }
+
+                //Set price to default
+                //item.customFinalPrice =  formatter.format(item['price']['AUD']['default']);
+                //hasCustomFinalPrice = false;
+            }*/
+            
+            //wiser_price
+            if (item['wiser_price']) {
+                //Set price to wiser_price
+                var wiserDiscount = item['price']['AUD']['default'] - item['wiser_price'];
+                if ((item['wiser_price'] < item['price']['AUD']['default']) && item['wiser_price'] != 0) {
+                    item.customFinalPrice =  formatter.format(item['wiser_price']);
+                    return html `<div className="algoliasearch-autocomplete-price">
+                            <span className="after_special custom_final_price">
+                                ${formatter.format(item['wiser_price'])}
+                            </span>
+                        </div>`;
+                } else {
+                    item.customFinalPrice =  formatter.format(item['price']['AUD']['default']);
+                }
+            }
+            
+            return html `<div className="algoliasearch-autocomplete-price">
+                    <span className="after_special ${item['price'][algoliaConfig.currencyCode][priceGroup + '_original_formated'] != null ? 'promotion' : ''}">
+                        ${item['price'][algoliaConfig.currencyCode][priceGroup + '_formated']}
+                    </span>
+                </div>`;
+        },
+
+        getFooterSearchCategoryLinks: (html, resultDetails) => {
+            if (resultDetails.allCategories == undefined || resultDetails.allCategories.length === 0) return "";
+
+            return html ` ${algoliaConfig.translations.orIn}
+                ${resultDetails.allCategories.map((list, index) =>
+                    index === 0 ? html` <span><a href="${list.url}">${list.name}</a></span>` : html`, <span><a href="${list.url}">${list.name}</a></span>`
+                )}
+            `;
+        },
+
+        getFooterSearchLinks: function(html, resultDetails)  {
+            if (resultDetails.nbHits === 0) return "";
+
+            return html`${algoliaConfig.translations.seeIn} <span><a href="${resultDetails.allDepartmentsUrl}">${algoliaConfig.translations.allDepartments}</a></span> (${resultDetails.nbHits})
+                ${this.getFooterSearchCategoryLinks(html, resultDetails)}
+            `;
+        },
+
+        // TODO: Refactor to external lib
+        safeHighlight: function(components, hit, attribute, strict = true) {
+            const highlightResult = hit._highlightResult[attribute];
+
+            if (!highlightResult) return '';
+
+            if (strict
+                &&
+                (
+                    (Array.isArray(highlightResult)) && !highlightResult.find(hit => hit.matchLevel !== 'none')
+                    ||
+                    highlightResult.value === ''
+                )
+            ) {
+                return '';
+            }
+
+            try {
+                return components.Highlight({ hit, attribute });
+            } catch (e) {
+                return '';
+            }
+        }
+
+    };
+});
