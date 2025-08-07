a3d2edba04779a986dc4167b12cbcd6a74e2aa8c
lakshyami <lakshyami@i4tlabs.io>
2025-08-07 14:38:56 +0530

    DGU-39 - digiServices => Added specific class per cards

--- app/code/Digidirect/GlowupHome/Setup/Patch/Data/AddDigiServicesBlock.php
+++ app/code/Digidirect/GlowupHome/Setup/Patch/Data/AddDigiServicesBlock.php
@@ -45,7 +45,7 @@ class AddDigiServicesBlock implements DataPatchInterface
 
     <div class="digiservices-grid">
 
-      <div class="digiservice-card">
+      <div class="digiservice-card digiservice-card-rent">
         <img src="{{media url='wysiwyg/glowup-digiservices/1.png'}}" alt="digiRent" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Rent</span></h1><a href="#" class="btn">Learn More</a></div>
@@ -53,7 +53,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-      <div class="digiservice-card">
+      <div class="digiservice-card digiservice-card-seconds">
         <img src="{{media url='wysiwyg/glowup-digiservices/4.png'}}" alt="digiSeconds" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Seconds</span></h1><a href="#" class="btn orange">shop now</a></div>
@@ -61,7 +61,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-    <div class="digiservice-card">
+    <div class="digiservice-card digiservice-card-trade">
         <img src="{{media url='wysiwyg/glowup-digiservices/7.png'}}" alt="Trade up program" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">Trade</span><span class="digi-black-text" >Up</span><span class="digi-black-text small">Program</span></h1><a href="#" class="btn orange">Trade Now</a></div>
@@ -69,7 +69,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-      <div class="digiservice-card">
+      <div class="digiservice-card digiservice-card-life">
         <img src="{{media url='wysiwyg/glowup-digiservices/10.png'}}" alt="digiLife" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Life</span></h1><a href="#" class="btn orange">Join</a></div>
@@ -77,7 +77,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-     <div class="digiservice-card">
+     <div class="digiservice-card digiservice-card-print">
         <img src="{{media url='wysiwyg/glowup-digiservices/2.png'}}" alt="digiPrint" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Print</span></h1><a href="#" class="btn">shop now</a></div>
@@ -85,7 +85,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-<div class="digiservice-card">
+<div class="digiservice-card digiservice-card-click-collect">
         <img src="{{media url='wysiwyg/glowup-digiservices/5.png'}}" alt="click & collect" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-black-text" >Click & Collect</span></h1><a href="#" class="btn orange">Learn More</a></div>
@@ -93,7 +93,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
- <div class="digiservice-card">
+ <div class="digiservice-card digiservice-card-club">
         <img src="{{media url='wysiwyg/glowup-digiservices/8.png'}}" alt="digiClub" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Club</span></h1><a href="#" class="btn orange">join</a></div>
@@ -101,7 +101,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
- <div class="digiservice-card">
+ <div class="digiservice-card digiservice-card-events">
         <img src="{{media url='wysiwyg/glowup-digiservices/11.png'}}" alt="Events" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-black-text" >Events</span></h1><a href="#" class="btn orange">Explore</a></div>
@@ -109,7 +109,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-      <div class="digiservice-card">
+      <div class="digiservice-card digiservice-card-market">
         <img src="{{media url='wysiwyg/glowup-digiservices/3.png'}}" alt="digiMarket" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Market</span></h1><a href="#" class="btn orange">Explore</a></div>
@@ -117,7 +117,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-      <div class="digiservice-card">
+      <div class="digiservice-card digiservice-card-direct">
         <img src="{{media url='wysiwyg/glowup-digiservices/6.png'}}" alt="digiDirect" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Direct</span><span class="digi-black-text small">Business</span></h1><a href="#" class="btn orange">Enter now</a></div>
@@ -125,7 +125,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-      <div class="digiservice-card">
+      <div class="digiservice-card digiservice-card-protect">
         <img src="{{media url='wysiwyg/glowup-digiservices/9.png'}}" alt="digiProtect" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">digi</span><span class="digi-black-text" >Protect</span></h1><a href="#" class="btn orange">Learn More</a></div>
@@ -133,7 +133,7 @@ class AddDigiServicesBlock implements DataPatchInterface
         </div>
       </div>
 
-      <div class="digiservice-card">
+      <div class="digiservice-card digiservice-card-price">
         <img src="{{media url='wysiwyg/glowup-digiservices/12.png'}}" alt="PriceMatch" />
         <div class="digicard-body">
           <div class="digicard-header"><h1><span class="digi-orange-text">Price</span><span class="digi-black-text" >Match</span></h1><a href="#" class="btn orange">Learn More</a></div>
