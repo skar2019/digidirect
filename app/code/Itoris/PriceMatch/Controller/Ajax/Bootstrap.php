71bb9f1ff5b90617fd377f0d83eb6b9e2b3fbbfb
clintmercado <clint@kayweb.com.au>
2025-08-26 00:19:04 +0800

    $finalPrice = $product->getFinalPrice();

--- app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
+++ app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
@@ -71,14 +71,13 @@ class Bootstrap extends \Magento\Framework\App\Action\Action
         $product = $this->productRepository->getById($productId,false, $storeId);
 
         $resultJson = $this->resultJsonFactory->create();
-        $finalPrice = $product->getPriceInfo()->getPrice('final_price')->getValue();
+        $finalPrice = $product->getFinalPrice();
         $wiserPrice = $product->getData('wiser_price');
-        //$customFinalPrice = $product->getData('custom_final_price');
         //$discountWiserPrice = number_format((float)$finalPrice - $wiserPrice, 2, '.', ''); //round($finalPrice - $wiserPrice, 2);
 
 
         if ($wiserPrice == 0 || empty($wiserPrice)) {
-            $finalPrice = $product->getPriceInfo()->getPrice('final_price')->getValue();
+            $finalPrice = $product->getFinalPrice();
         } else {
             if($wiserPrice < $finalPrice) {
                 $finalPrice = $wiserPrice;
