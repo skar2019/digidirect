ef5b3b01d94d3c93445020041ee2a8a1b56385fb
clintmercado <clint@kayweb.com.au>
2025-08-26 10:47:29 +0800

    conditional wiser price

--- app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
+++ app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
@@ -72,17 +72,17 @@ class Bootstrap extends \Magento\Framework\App\Action\Action
 
         $resultJson = $this->resultJsonFactory->create();
         $finalPrice = $product->getFinalPrice();
-//        $wiserPrice = $product->getData('wiser_price');
-//        //$discountWiserPrice = number_format((float)$finalPrice - $wiserPrice, 2, '.', ''); //round($finalPrice - $wiserPrice, 2);
-//
-//
-//        if ($wiserPrice == 0 || empty($wiserPrice)) {
-//            $finalPrice = $product->getFinalPrice();
-//        } else {
-//            if($wiserPrice < $finalPrice) {
-//                $finalPrice = $wiserPrice;
-//            }
-//        }
+        $wiserPrice = $product->getData('wiser_price');
+        //$discountWiserPrice = number_format((float)$finalPrice - $wiserPrice, 2, '.', ''); //round($finalPrice - $wiserPrice, 2);
+
+
+        if ($wiserPrice == 0 || empty($wiserPrice)) {
+            $finalPrice = $product->getFinalPrice();
+        } else {
+            if($wiserPrice < $finalPrice) {
+                $finalPrice = $wiserPrice;
+            }
+        }
         $response = [
             'product_name' => $product->getName(),
             'final_price' => $finalPrice,//$product->getFinalPrice(),
