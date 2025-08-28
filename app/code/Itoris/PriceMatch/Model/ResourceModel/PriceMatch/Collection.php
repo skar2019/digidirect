f486141527004cd63008391502e33fe59e1c728b
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-08-28 11:56:19 +0800

    Test Fix Pricematch

--- app/code/Itoris/PriceMatch/Model/ResourceModel/PriceMatch/Collection.php
+++ app/code/Itoris/PriceMatch/Model/ResourceModel/PriceMatch/Collection.php
@@ -86,13 +86,7 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
 
             $byRequest = \Zend_Json_Decoder::decode( $item['by_request'] );
             $simpleProduct = $this->configurableProduct->getProductByAttributes($byRequest, $product);
-            $phpTableArray[] = [
-                'product_id'=>$item['product_id'], 
-                'child_product_id'=>$simpleProduct->getId(), 
-                'by_request'=>$item['by_request'], 
-                'product_name'=>$simpleProduct->getName(),
-                
-            ];
+            $phpTableArray[] = ['product_id'=>$item['product_id'], 'child_product_id'=>$simpleProduct->getId(), 'by_request'=>$item['by_request'], 'product_name'=>$simpleProduct->getName()];
         }
 
         $this->getConnection()->query("CREATE TEMPORARY TABLE IF NOT EXISTS {$this->getTable('itoris_pm_temporary')} (
@@ -129,18 +123,14 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
             )->joinLeft(
                 ['ent'=>$this->getTable('catalog_product_entity')],
                 "pm.product_id = ent.entity_id",
-                ['product_sku'=>'ent.sku']
+                []
             )->join(
                 ['price_index'=>$this->getTable('catalog_product_index_price')],
                 "(
                     (temporary_it.child_product_id IS NOT NULL AND price_index.entity_id = temporary_it.child_product_id) OR
                     (temporary_it.child_product_id IS NULL AND price_index.entity_id = pm.product_id)
                 ) AND price_index.website_id = st.website_id AND price_index.customer_group_id = 0",
-                []
-            )->joinLeft(
-                ['cpev'=>$this->getTable('catalog_product_entity_varchar')],
-                "ent.row_id = cpev.row_id AND cpev.attribute_id=1415",
-                ['final_price'=>'IF(isnull(pm.old_price),IF(cpev.value IS NOT NULL AND cpev.value != 0, IF(price_index.final_price < cpev.value, price_index.final_price, cpev.value),price_index.final_price), pm.old_price)']
+                ['final_price'=>'IF(isnull(pm.old_price),price_index.final_price,pm.old_price)']
             )->join(
                 ['a_varchar'=>$this->getTable('catalog_product_entity_varchar')],
                 "a_varchar.{$indexColumn} = ent.{$indexColumn} AND a_varchar.attribute_id = ".$attrNameId.' AND  a_varchar.store_id = (
--- app/code/Itoris/PriceMatch/Ui/DataProvider/Admin/Edit/DataProvider.php
+++ app/code/Itoris/PriceMatch/Ui/DataProvider/Admin/Edit/DataProvider.php
@@ -159,7 +159,8 @@ class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvi
 
         $product = $this->productRepository->getById($item['product_id']);
         $price = $product->getCustomAttribute('custom_final_price');
-        $finalPrice = $price->getValue();
+        //$finalPrice = $price->getValue();
+        $finalPrice = $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
 
         //$wiserPrice = $product->getData('wiser_price');
 
