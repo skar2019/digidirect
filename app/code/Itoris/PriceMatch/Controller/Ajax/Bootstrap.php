66194bcf0ca87176bb109efdb71cf4d9a4cf3623
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-08-28 16:09:21 +0800

    Test Fix Pricematch

--- app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
+++ app/code/Itoris/PriceMatch/Controller/Ajax/Bootstrap.php
@@ -68,21 +68,23 @@ class Bootstrap extends \Magento\Framework\App\Action\Action
         $productId = $this->_request->getParam('id');
         $customer  = $this->customerSession->getCustomer();
 
-        $product = $this->productRepository->getById($productId,false, $storeId);
+        $product = $this->productRepository->getById($productId);
 
         $resultJson = $this->resultJsonFactory->create();
-        $finalPrice = $product->getFinalPrice();
+        $finalPrice = $product->getPriceInfo()->getPrice('final_price')->getValue();
         $wiserPrice = $product->getData('wiser_price');
         //$discountWiserPrice = number_format((float)$finalPrice - $wiserPrice, 2, '.', ''); //round($finalPrice - $wiserPrice, 2);
 
-
-        if ($wiserPrice == 0 || empty($wiserPrice)) {
-            $finalPrice = $product->getFinalPrice();
-        } else {
+        $this->logger->info("sku: ". $product->getData('sku'));
+        $this->logger->info("wiserPrice: ". $wiserPrice);
+        $this->logger->info("finalPrice: ". $finalPrice);
+        
+        if ($wiserPrice != 0 || !empty($wiserPrice)) {
             if($wiserPrice < $finalPrice) {
                 $finalPrice = $wiserPrice;
             }
         }
+        
         $response = [
             'product_name' => $product->getName(),
             'final_price' => $finalPrice,//$product->getFinalPrice(),
