413110118f4b142213ab2330ac520350862bc56e
clintmercado <clint@kayweb.com.au>
2025-09-29 11:14:23 +0800

    discount glitch

--- app/code/Digidirect/Pronto/Helper/TestPronto.php
+++ app/code/Digidirect/Pronto/Helper/TestPronto.php
@@ -1465,9 +1465,18 @@ class TestPronto extends AbstractHelper
                 $todiscount = $price * $qty;
                 $total = ($price * $qty) - $discount;
                 $discperc = 0;
-                if($price > 0)
+                if($coupon != "")
                 {
-                    $discperc = (($discount / $price) * 100) / $qty;
+                    $discperc = 0;
+                    if(str_contains($coupon, 'PMC-'))
+                    {
+                        $data['sales-order']['header']['rep'] = "PMC";
+                    }
+
+                    if($price > 0)
+                    {
+                        $discperc = (($discount / $price) * 100) / $qty; // in pronto, discount % are mutliplied by qty, so here we divide it
+                    }
                 }
                 echo "discperc - " .$discperc ." <br/>";
                 if($coupon != "")
