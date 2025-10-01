434e39f605e0a2d001d955bf197e3b842601cb19
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-10-01 20:54:30 +0800

    Custom Remove Item Confirmation

--- app/design/frontend/Digidirect/digi-glow-up/requirejs-config.js
+++ app/design/frontend/Digidirect/digi-glow-up/requirejs-config.js
@@ -1,12 +1,5 @@
 var config = {
     deps: [
         'js/custom' // your existing custom file
-    ],
-    config: {
-        mixins: {
-            'Magento_Checkout/js/sidebar': {
-                'Magento_Theme/js/sidebar-mixin': true
-            }
-        }
-    }
+    ]
 };
