8ec8071834286fdeb8ef0aa444ebc4655c51d588
clintmercado <clint@kayweb.com.au>
2025-10-07 20:55:30 +0800

    sticky footer menu

--- app/design/frontend/Digidirect/digi-glow-up/Magento_Theme/web/js/custom-footer.js
+++ app/design/frontend/Digidirect/digi-glow-up/Magento_Theme/web/js/custom-footer.js
@@ -1,4 +1,13 @@
 require(['jquery'], function($) {
+
+    function toggleMobileMenu() {
+        // Trigger Magento's mobile menu
+        $('[data-action="toggle-nav"]').trigger('click');
+    }
+
+    // Make function globally available
+    window.toggleMobileMenu = toggleMobileMenu;
+
     $(document).ready(function() {
         $('.page-footer').hover(
             function () {
@@ -8,5 +17,15 @@ require(['jquery'], function($) {
               $(this).find('.footer-black-gradient').css('opacity', '1');
             }
         );
+
+        var currentUrl = window.location.pathname;
+        $('.footer-nav-item').each(function() {
+            var href = $(this).attr('href');
+            if (href && currentUrl.indexOf(href) > -1 && href !== '/') {
+                $('.footer-nav-item').removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+
     });
 });
