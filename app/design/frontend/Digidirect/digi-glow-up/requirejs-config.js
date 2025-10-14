d0e069b80d5d72b83f451f0ef05f700ceae05b8b
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-10-14 12:59:07 +0800

    UI/UX, Fix Minicart Content

--- app/design/frontend/Digidirect/digi-glow-up/requirejs-config.js
+++ app/design/frontend/Digidirect/digi-glow-up/requirejs-config.js
@@ -1,11 +1,4 @@
 var config = {
-  config: {
-    mixins: {
-      'Magento_Checkout/js/view/minicart': {
-        'Magento_Checkout/js/view/minicart-custom': true
-      }
-    }
-  },
   deps: [
     'js/custom'
   ]
