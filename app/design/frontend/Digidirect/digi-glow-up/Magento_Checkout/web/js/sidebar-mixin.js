3619dc1be30df3cc68f201c5cee206f8d07851e1
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-10-16 19:45:22 +0800

    UI/UX, Minicart Fixes

--- app/design/frontend/Digidirect/digi-glow-up/Magento_Checkout/web/js/sidebar-mixin.js
+++ app/design/frontend/Digidirect/digi-glow-up/Magento_Checkout/web/js/sidebar-mixin.js
@@ -1,40 +1,35 @@
 define([
-  'jquery',
-  'Magento_Ui/js/modal/confirm'
+    'jquery',
+    'Magento_Ui/js/modal/confirm'
 ], function ($, confirm) {
-  'use strict';
+    'use strict'
 
-  return function (target) {
-    return $.extend(target, {
-      _removeItem: function (id) {
-        const self = this;
+    return function (Component) {
+        return Component.extend({
+            _removeItem: function (id) {
+                var self = this
 
-        confirm({
-          content: $.mage.__('Are you sure you would like to remove this item?'),
-          buttons: [
-            {
-              text: $.mage.__('No, Keep it'),
-              class: 'action-secondary action-dismiss',
-              click: function (event) {
-                this.closeModal(event);
-              }
-            },
-            {
-              text: $.mage.__('Yes, Remove it'),
-              class: 'action-primary action-accept',
-              click: function () {
-                self._removeItemAfterConfirm(id);
-                this.closeModal(true);
-              }
+                confirm({
+                    content: $.mage.__('Are you sure you would like to remove this item from your cart?'),
+                    buttons: [
+                        {
+                            text: $.mage.__('No, keep it'),
+                            class: 'action-secondary action-dismiss',
+                            click: function (event) {
+                                this.closeModal(event)
+                            }
+                        },
+                        {
+                            text: $.mage.__('Yes, remove it'),
+                            class: 'action-primary action-accept',
+                            click: function () {
+                                self._super(id) // call original remove logic
+                                this.closeModal(true)
+                            }
+                        }
+                    ]
+                })
             }
-          ]
-        });
-      },
-
-      _removeItemAfterConfirm: function (id) {
-        // call the original removal logic
-        this._super(id);
-      }
-    });
-  };
-});
+        })
+    }
+})
