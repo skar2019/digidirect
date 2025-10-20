93c0c2845794b3796164d4e6144354971dcdc52a
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-10-20 11:53:18 +0800

    UI/UX, Algolia Landing Page Banner (digiDeals)

--- app/code/Digidirect/Algolia/Observer/AddLandingPageHandle.php
+++ app/code/Digidirect/Algolia/Observer/AddLandingPageHandle.php
@@ -4,41 +4,36 @@ namespace Digidirect\Algolia\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\App\RequestInterface;
+use Psr\Log\LoggerInterface;
 
 class AddLandingPageHandle implements ObserverInterface
 {
-    /**
-     * @var RequestInterface
-     */
     protected $request;
     protected $logger;
 
     public function __construct(
         RequestInterface $request,
-        \Psr\Log\LoggerInterface $logger,
-    ){
+        LoggerInterface $logger
+    ) {
         $this->request = $request;
         $this->logger = $logger;
     }
 
-    /**
-     * Add dynamic layout handle for specific Algolia Merchandising Landing Pages.
-     */
     public function execute(Observer $observer)
     {
-        // Check if this is an Algolia Merchandising landing page
-        if ($this->request->getFullActionName() === 'algolia_landingpage_view') {
+        $fullAction = $this->request->getFullActionName();
+        $this->logger->info('Algolia Observer fired: ' . $fullAction);
+
+        if (strpos($fullAction, 'algolia_landingpage') !== false) {
             $layout = $observer->getData('layout');
-
-            // Example: "/deals" or "/brands/canon"
             $path = trim($this->request->getPathInfo(), '/');
-
-            // Remove .html or trailing slashes, make handle safe
             $path = preg_replace('/(\.html$|\/$)/', '', $path);
             $slugHandle = preg_replace('/[^a-z0-9_]+/i', '_', strtolower($path));
-            $this->logger->info('handle, ' . 'algolia_landingpage_view_' . $slugHandle);
-            // Example handle: algolia_landingpage_view_deals
-            $layout->getUpdate()->addHandle('algolia_landingpage_view_' . $slugHandle);
+
+            $handle = 'algolia_landingpage_view_' . $slugHandle;
+            $this->logger->info('Adding handle: ' . $handle);
+
+            $layout->getUpdate()->addHandle($handle);
         }
     }
 }
