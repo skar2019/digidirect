1c77350873700ab6555a834b9627b2e67dfc9361
clintmercado <clint@kayweb.com.au>
2025-10-22 23:09:13 +0800

    requirejs

--- app/design/frontend/Digidirect/digi-glow-up/Magento_Catalog/requirejs-config.js
+++ app/design/frontend/Digidirect/digi-glow-up/Magento_Catalog/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     config: {
         mixins: {
-            'Magento_Catalog/js/add-to-cart': {
+            'Magento_Catalog/js/catalog-add-to-cart': {
                 'Magento_Catalog/js/add-to-cart-mixin': true
             }
         }
