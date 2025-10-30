617d19097f7a81fd34b8ad7773d23f9b3f506320
lakshyami <lakshyami@i4tlabs.io>
2025-10-30 13:47:57 +0530

    Reference note adjustments and DOB fixes

--- app/design/frontend/Digidirect/digi-glow-up/Magento_Customer/web/js/my-account.js
+++ app/design/frontend/Digidirect/digi-glow-up/Magento_Customer/web/js/my-account.js
@@ -18,12 +18,17 @@ require(['jquery',
 
             if ($('.form-edit-account .customer-dob #dob').val()) {
                 $('.form-edit-account .field-dob').attr('style', 'width: 49% !important;');
+                $('.form-edit-account .customer-dob').attr('style', 'opacity: 0.6;');
+                $('.form-edit-account .customer-dob').attr('style', 'cursor: default;');
             } else {
                 $('input[name="dob"]').after('<button class="ui-datepicker-trigger-glowup icon-dob"></button>');
             }
 
             if (!$('input[name="digiclub-dob"]').val()) {
                 $('input[name="digiclub-dob"]').after('<button class="ui-datepicker-trigger-glowup icon-dob-digiclub"></button>');
+            } else {
+                $('.form-edit-account input[name="digiclub-dob"]').attr('style', 'opacity: 0.6;');
+                $('.form-edit-account input[name="digiclub-dob"]').attr('style', 'cursor: default;');
             }
 
             selectors.forEach(({ input, icon }) => {
