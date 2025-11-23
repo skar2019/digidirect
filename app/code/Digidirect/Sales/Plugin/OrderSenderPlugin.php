379b2f032b0926fb641878b2c80f166c73f61f3e
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-11-24 03:18:04 +0800

    Test Fix C&C Email Confirmation

--- app/code/Digidirect/Sales/Plugin/OrderSenderPlugin.php
+++ app/code/Digidirect/Sales/Plugin/OrderSenderPlugin.php
@@ -7,26 +7,16 @@ use Magento\Sales\Model\Order;
 class OrderSenderPlugin
 {
     /**
-     * Force custom email template ID for order emails
+     * Override template for Click & Collect orders
      *
      * @param OrderSender $subject
      * @param callable $proceed
      * @param Order $order
-     * @param bool $forceSyncMode
-     * @return bool
      */
-    public function aroundSend(OrderSender $subject, callable $proceed, Order $order, $forceSyncMode = false)
+    public function aroundPrepareTemplate(OrderSender $subject, callable $proceed, Order $order)
     {
-        // Force template ID 69
-        $templateId = 69;
-
-        // Set template ID in OrderIdentity container
-        $orderIdentity = $subject->getTemplateContainer();
-        if ($orderIdentity) {
-            $orderIdentity->setTemplateId($templateId);
-        }
-
-        // Continue sending email
-        return $proceed($order, $forceSyncMode);
+        $subject->getTemplateContainer()->setTemplateId(69);
+        // Continue with original method
+        $proceed($order);
     }
 }
