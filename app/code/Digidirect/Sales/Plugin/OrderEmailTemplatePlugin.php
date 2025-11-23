d7d709ac36d334297872e0f6a461196feee5bb69
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-11-23 21:57:30 +0800

    Test Fix C&C Email Confirmation

--- app/code/Digidirect/Sales/Plugin/OrderEmailTemplatePlugin.php
+++ app/code/Digidirect/Sales/Plugin/OrderEmailTemplatePlugin.php
@@ -3,15 +3,28 @@ namespace Digidirect\Sales\Plugin;
 
 use Magento\Sales\Model\Order\Email\Sender\OrderSender;
 use Magento\Sales\Model\Order;
+use Psr\Log\LoggerInterface;
 
 class OrderEmailTemplatePlugin
 {
-    public function beforeSend(OrderSender $subject, Order $order, $forceSyncMode = null)
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
     {
+        $this->logger = $logger;
+    }
+
+    public function beforeSend(
+        OrderSender $subject,
+        Order $order,
+        $forceSyncMode = null
+    ) {
         $shippingMethod = $order->getShippingMethod();
+        $this->logger->debug('OrderEmailTemplatePlugin: ' . $shippingMethod);
 
         if ($shippingMethod === 'collect_collect') {
             $subject->getTemplateContainer()->setTemplateId(69);
+            $this->logger->debug('Template set to 69');
         }
 
         return [$order, $forceSyncMode];
