2ba9b7b5a67f6af629d91689c16488591b32776b
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-11-24 07:17:12 +0800

    Test Fix C&C Email Confirmation

--- app/code/Digidirect/Sales/Observer/ChangeOrderEmailTemplateObserver.php
+++ app/code/Digidirect/Sales/Observer/ChangeOrderEmailTemplateObserver.php
@@ -4,19 +4,43 @@ namespace Digidirect\Sales\Observer;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Model\Order;
+use Magento\Email\Model\Template\Config as TemplateConfig;
+use Psr\Log\LoggerInterface;
 
 class ChangeOrderEmailTemplateObserver implements ObserverInterface
 {
     /**
-     * Force Click & Collect orders to use a different email template
+     * @var TemplateConfig
+     */
+    protected $templateConfig;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Constructor
+     *
+     * @param TemplateConfig $templateConfig
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        TemplateConfig $templateConfig,
+        LoggerInterface $logger
+    ) {
+        $this->templateConfig = $templateConfig;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Execute observer
      *
      * @param Observer $observer
      */
     public function execute(Observer $observer)
     {
-        /** @var \Magento\Framework\DataObject $transport */
         $transport = $observer->getData('transportObject');
-
         if (!$transport) {
             return;
         }
@@ -26,13 +50,31 @@ class ChangeOrderEmailTemplateObserver implements ObserverInterface
             return;
         }
 
-        /*$shippingMethod = $order->getShippingMethod();
-        if (strpos(strtolower($shippingMethod), 'clickandcollect') !== false) {
-            // Force template ID
-            $identityContainer = $observer->getData('sender')->getIdentityContainer();
-            $identityContainer->setTemplateId(69);
-        }*/
+        $shippingMethod = $order->getShippingMethod();
+
         $identityContainer = $observer->getData('sender')->getIdentityContainer();
-        $identityContainer->setTemplateId(69);
+
+        $templateId = 69; // Your Click & Collect template ID
+
+        // Check if template exists for this store
+        $storeId = $order->getStoreId();
+        $templateConfig = $this->templateConfig->getTemplateByConfigPath('sales_email/order/template', $storeId);
+
+        if ($templateId) {
+            $identityContainer->setTemplateId($templateId);
+            $identityContainer->setStore($order->getStore());
+
+            $this->logger->info('Click & Collect email template applied', [
+                'order_id' => $order->getIncrementId(),
+                'shipping_method' => $shippingMethod,
+                'template_id' => $templateId
+            ]);
+        } else {
+            $this->logger->warning('Click & Collect template ID not found', [
+                'order_id' => $order->getIncrementId(),
+                'shipping_method' => $shippingMethod,
+                'template_id' => $templateId
+            ]);
+        }
     }
 }
