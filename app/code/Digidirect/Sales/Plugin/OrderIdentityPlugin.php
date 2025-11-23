4759b307438087ad5a8cb6cae35015eecc57d3db
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-11-24 00:26:52 +0800

    Test Fix C&C Email Confirmation

---
+++ app/code/Digidirect/Sales/Plugin/OrderIdentityPlugin.php
@@ -0,0 +1,23 @@
+<?php
+namespace Digidirect\Sales\Plugin;
+
+use Magento\Sales\Model\Order\Email\Container\OrderIdentity;
+use Magento\Sales\Model\Order;
+
+class OrderIdentityPlugin
+{
+    public function aroundGetTemplateId(OrderIdentity $subject, callable $proceed)
+    {
+        $order = $subject->getTemplateVars()['order'] ?? null;
+
+        if ($order instanceof Order) {
+            $shippingMethod = $order->getShippingMethod();
+
+            if ($shippingMethod === 'collect_collect') {
+                return 69; // your custom template ID
+            }
+        }
+
+        return $proceed();
+    }
+}
