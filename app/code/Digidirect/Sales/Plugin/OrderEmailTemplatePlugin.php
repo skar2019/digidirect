89e67a52ad0de22a2eb54ae79f9fca3c5f9623bc
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-11-23 21:16:25 +0800

    Test Fix C&C Email Confirmation

--- app/code/Digidirect/Sales/Plugin/OrderEmailTemplatePlugin.php
+++ app/code/Digidirect/Sales/Plugin/OrderEmailTemplatePlugin.php
@@ -2,18 +2,18 @@
 namespace Digidirect\Sales\Plugin;
 
 use Magento\Sales\Model\Order\Email\Sender\OrderSender;
+use Magento\Sales\Model\Order;
 
 class OrderEmailTemplatePlugin
 {
-    public function aroundPrepareTemplate(OrderSender $subject, callable $proceed, $order)
+    public function beforeSend(OrderSender $subject, Order $order, $forceSyncMode = null)
     {
-        // First, call the original method to initialize template container
-        $proceed($order);
-
-        // Now safely override template
         $shippingMethod = $order->getShippingMethod();
+
         if ($shippingMethod === 'collect_collect') {
             $subject->getTemplateContainer()->setTemplateId(69);
         }
+
+        return [$order, $forceSyncMode];
     }
 }
