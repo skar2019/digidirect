48052a6f6eb38d0c9e358c57ede61c2e0ed606f0
clintmercado <clint@kayweb.com.au>
2025-11-24 11:02:01 +0800

    plugin and sequence

--- app/code/Digidirect/Sales/Plugin/Sales/Model/Order/Email/Sender/OrderSenderPlugin.php
+++ app/code/Digidirect/Sales/Plugin/Sales/Model/Order/Email/Sender/OrderSenderPlugin.php
@@ -7,20 +7,9 @@ use Psr\Log\LoggerInterface;
 
 class OrderSenderPlugin
 {
-    /**
-     * @var Template
-     */
     protected $templateContainer;
-
-    /**
-     * @var LoggerInterface
-     */
     protected $logger;
 
-    /**
-     * @param Template $templateContainer
-     * @param LoggerInterface $logger
-     */
     public function __construct(
         Template $templateContainer,
         LoggerInterface $logger
@@ -30,45 +19,45 @@ class OrderSenderPlugin
     }
 
     /**
-     * Plugin to modify template based on shipping method
-     *
-     * @param \Magento\Sales\Model\Order\Email\Sender\OrderSender $subject
-     * @param callable $proceed
-     * @param Order $order
-     * @param bool $forceSyncMode
-     * @return bool
+     * Before send
      */
-    public function aroundSend(
-        \Magento\Sales\Model\Order\Email\Sender\OrderSender $subject,
-        callable $proceed,
+    public function beforeSend(
+        $subject,
         Order $order,
-                                                            $forceSyncMode = false
+        $forceSyncMode = false
     ) {
-        $this->logger->info('=== Digidirect Order Sender Plugin: Start ===');
+        $this->logger->info('========================================');
+        $this->logger->info('DIGIDIRECT PLUGIN TRIGGERED: beforeSend');
+        $this->logger->info('Subject Class: ' . get_class($subject));
         $this->logger->info('Order ID: ' . $order->getId());
-        $this->logger->info('Order Increment ID: ' . $order->getIncrementId());
+        $this->logger->info('========================================');
 
+        $this->modifyTemplate($order);
+        return [$order, $forceSyncMode];
+    }
+
+    /**
+     * Modify template based on shipping method
+     */
+    protected function modifyTemplate(Order $order)
+    {
         try {
-            // Get shipping method from order
             $shippingMethod = $order->getShippingMethod();
 
-            $this->logger->info('Shipping Method: ' . ($shippingMethod ?: 'NULL'));
+            $this->logger->info('Digidirect Plugin - Order Increment ID: ' . $order->getIncrementId());
+            $this->logger->info('Digidirect Plugin - Shipping Method: ' . ($shippingMethod ?: 'NULL'));
+            $this->logger->info('Digidirect Plugin - Current Template ID: ' . $this->templateContainer->getTemplateId());
 
-            // Check if shipping method is store pickup/collection
             if ($shippingMethod === 'collect_collect') {
-                $this->logger->info('Switching to collection template (ID: 69)');
+                $this->logger->info('Digidirect Plugin - CONDITION MATCHED! Switching to template 69');
                 $this->templateContainer->setTemplateId(69);
+                $this->logger->info('Digidirect Plugin - New Template ID: ' . $this->templateContainer->getTemplateId());
             } else {
-                $this->logger->info('Using default template (not collection method)');
+                $this->logger->info('Digidirect Plugin - Condition not matched, keeping default template');
             }
         } catch (\Exception $e) {
-            $this->logger->error('Digidirect Order Sender Plugin Error: ' . $e->getMessage());
-            $this->logger->error('Stack trace: ' . $e->getTraceAsString());
+            $this->logger->error('Digidirect Plugin Error: ' . $e->getMessage());
+            $this->logger->error('Stack Trace: ' . $e->getTraceAsString());
         }
-
-        $this->logger->info('=== Digidirect Order Sender Plugin: End ===');
-
-        // Call the original send method
-        return $proceed($order, $forceSyncMode);
     }
 }
