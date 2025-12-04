0e25b71a3ef9a9b4f16bf7482f5504090f10312d
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-12-04 22:03:52 +0800

    Test Fix Disable ATC Button Immediately On Click

--- app/design/frontend/Digidirect/digi-glow-up/web/js/custom.js
+++ app/design/frontend/Digidirect/digi-glow-up/web/js/custom.js
@@ -2159,64 +2159,6 @@ function updateCartAjax($input, newQty) {
             }
         });
     });
-    
-    //Disable Add To Cart Button Immediately On Click
-    $(document).on('mousedown', 'button[type="submit"].tocart', function(e) {
-        var $button = $(this);
-
-        // Don't do anything if already loading
-        if ($button.hasClass('loading')) {
-            return;
-        }
-
-        // Visual feedback IMMEDIATELY - but WITHOUT pointer-events: none
-        $button.css({
-                   'opacity': '0.6',
-                   'cursor': 'not-allowed'
-               })
-               .addClass('loading');
-
-        // Change text immediately
-        var $span = $button.find('span');
-        $span.data('original-text', $span.text());
-        $span.text('Adding...');
-    });
-
-    // Prevent double clicks while loading
-    $(document).on('click', 'button[type="submit"].tocart', function(e) {
-        var $button = $(this);
-
-        // If loading and first click already done, prevent it
-        if ($button.hasClass('loading') && $button.data('first-click-done')) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-            return false;
-        }
-
-        // If loading but first click not done yet, mark it as done and allow it
-        if ($button.hasClass('loading') && !$button.data('first-click-done')) {
-            $button.data('first-click-done', true);
-        }
-    });
-
-    // Re-enable after AJAX completes
-    $(document).ajaxComplete(function(event, xhr, settings) {
-        if (settings.url && settings.url.indexOf('checkout/cart/add') !== -1) {
-            $('button.tocart.loading').each(function() {
-                var $button = $(this);
-                var $span = $button.find('span');
-
-                $button.css({
-                           'opacity': '1',
-                           'cursor': 'pointer'
-                       })
-                       .removeClass('loading')
-                       .removeData('first-click-done');
-
-                $span.text($span.data('original-text') || 'Add to Cart');
-            });
-        }
-    });
 
   })
 })
