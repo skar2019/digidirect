2971e725e5266dfbb02f598adfb117cea961acf1
Rondel Dalumpines <rondel.d@digidirect.com.au>
2025-12-07 08:44:56 +0800

    Test Promotion Theme

--- app/design/frontend/Digidirect/digi-glow-up-promotion/Magento_PurchaseOrder/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'Magento_PurchaseOrder/template/checkout/billing-address/details.html':
-                'Magento_PurchaseOrder/template/checkout/billing-address/details.html'
-        }
-    }
-};
