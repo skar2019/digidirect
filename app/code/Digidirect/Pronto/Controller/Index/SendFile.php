b543153ce2c9baeeb44f5d9f6ac373ad5c4ff938
clintmercado <clint@kayweb.com.au>
2025-12-17 07:59:14 +0800

    test send file

--- app/code/Digidirect/Pronto/Controller/Index/SendFile.php
+++ app/code/Digidirect/Pronto/Controller/Index/SendFile.php
@@ -1,7 +1,7 @@
 <?php
 namespace Digidirect\Pronto\Controller\Index;
 
-use Digidirect\Pronto\Helper\SftpMWaveSender;
+use Digidirect\Pronto\Helper\Sftpwisersender;
 
 class SendFile extends \Magento\Framework\App\Action\Action
 {
@@ -9,7 +9,7 @@ class SendFile extends \Magento\Framework\App\Action\Action
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        SftpMWaveSender $helper)
+        Sftpwisersender $helper)
     {
         $this->helper = $helper;
         return parent::__construct($context);
@@ -18,7 +18,6 @@ class SendFile extends \Magento\Framework\App\Action\Action
     public function execute()
     {
 
-
-        $this->helper->sendExtraData();
+        $this->helper->sendFile();
     }
 }
