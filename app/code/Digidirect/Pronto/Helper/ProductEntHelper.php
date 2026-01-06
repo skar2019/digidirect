e168200879ea330986fd7573a2411076fe31ff32
clintmercado <clint@kayweb.com.au>
2026-01-06 14:10:03 +0800

    update wiser data cron

--- app/code/Digidirect/Pronto/Helper/ProductEntHelper.php
+++ app/code/Digidirect/Pronto/Helper/ProductEntHelper.php
@@ -307,14 +307,8 @@ class ProductEntHelper extends AbstractHelper
                     }
                 }
 
-                $final_price = $product->getPriceInfo()->getPrice('final_price')->getValue();
-                $final_price2 = $product->getFinalPrice();
                 $final_price3 = $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
-//                $product->setCustomAttribute('custom_final_price', $final_price3);
-//                $this->productRepository->save($product);
-//            echo "final price ".$final_price."<br/>";
-//            echo "final price2 ".$final_price2."<br/>";
-//            echo $product->getSku()." final price3 ".$final_price3."<br/>";
+
                 $stockC = "Other";
                 $stockcondition = $product->getCustomAttribute('stock_condition');
 
@@ -934,14 +928,8 @@ class ProductEntHelper extends AbstractHelper
                 $avgcost = number_format($avgcost, 2, '.', '');
 
 
-                $final_price = $product->getPriceInfo()->getPrice('final_price')->getValue();
-                $final_price2 = $product->getFinalPrice();
                 $final_price3 = $product->getPriceInfo()->getPrice('final_price')->getAmount()->getValue();
-//                $product->setCustomAttribute('custom_final_price', $final_price3);
-//                $this->productRepository->save($product);
-//            echo "final price ".$final_price."<br/>";
-//            echo "final price2 ".$final_price2."<br/>";
-//            echo $product->getSku()." final price3 ".$final_price3."<br/>";
+
                 $stockC = "Other";
                 $stockcondition = $product->getCustomAttribute('stock_condition');
 
