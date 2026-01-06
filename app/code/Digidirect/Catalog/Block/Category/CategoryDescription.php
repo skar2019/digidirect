2a6f897080e6bad04476654817f1776a2db2adcb
Rondel Dalumpines <rondel.d@digidirect.com.au>
2026-01-06 12:17:38 +0800

    Test Fix PLP SEO Description

---
+++ app/code/Digidirect/Catalog/Block/Category/CategoryDescription.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Digidirect\YourModule\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Framework\Registry;
+
+class CategoryDescription extends Template
+{
+    protected $registry;
+    protected $categoryRepository;
+
+    public function __construct(
+        Template\Context $context,
+        Registry $registry,
+        CategoryRepositoryInterface $categoryRepository,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->registry = $registry;
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function getCategory()
+    {
+        $category = $this->registry->registry('current_category');
+        if ($category) {
+            return $category;
+        }
+
+        $categoryId = (int) $this->getRequest()->getParam('cat');
+        return $categoryId ? $this->categoryRepository->get($categoryId) : null;
+    }
+}
