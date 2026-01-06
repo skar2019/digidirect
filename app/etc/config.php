5ee20ac4a721596af3c47d5697dd9281f030276e
lakshyami <lakshyami@i4tlabs.io>
2026-01-06 12:43:49 +0530

    Optimizations - minify js and move script to bottom configuration changes reverting

--- app/etc/config.php
+++ app/etc/config.php
@@ -104,8 +104,8 @@ return [
                 'js' => [
                     'merge_files' => '0',
                     'enable_js_bundling' => '0',
-                    'minify_files' => '1',
-                    'move_script_to_bottom' => '1',
+                    'minify_files' => '0',
+                    'move_script_to_bottom' => '0',
                     'translate_strategy' => 'dictionary',
                     'session_storage_logging' => '0',
                     'minify_exclude' => [
