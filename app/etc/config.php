423b3beb73cd305b87a864c153d6972c5db4336c
lakshyami <lakshyami@i4tlabs.io>
2026-01-23 17:26:02 +0530

    DGU-295 => disabling merge css/js, enable move script to bottom as config file replaced by autogeneration

--- app/etc/config.php
+++ app/etc/config.php
@@ -105,7 +105,7 @@ return [
                     'merge_files' => '0',
                     'enable_js_bundling' => '0',
                     'minify_files' => '1',
-                    'move_script_to_bottom' => '0',
+                    'move_script_to_bottom' => '1',
                     'translate_strategy' => 'dictionary',
                     'session_storage_logging' => '0',
                     'minify_exclude' => [
@@ -115,7 +115,7 @@ return [
                     ]
                 ],
                 'css' => [
-                    'merge_css_files' => '1',
+                    'merge_css_files' => '0',
                     'minify_files' => '1',
                     'use_css_critical_path' => '0',
                     'minify_exclude' => [
