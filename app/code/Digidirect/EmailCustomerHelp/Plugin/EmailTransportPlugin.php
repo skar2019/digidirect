a7fbc8eb7ced54c6b103ce63cb6add22541cc43c
lakshyami <lakshyami@i4tlabs.io>
2026-01-27 13:29:07 +0530

    DGU-305 => shipping confirmation tracking number added

---
+++ app/code/Digidirect/EmailCustomerHelp/Plugin/EmailTransportPlugin.php
@@ -0,0 +1,46 @@
+<?php
+namespace Digidirect\EmailCustomerHelp\Plugin;
+
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Psr\Log\LoggerInterface;
+
+class EmailTransportPlugin
+{
+    private TimezoneInterface $timezone;
+    private LoggerInterface $logger;
+    public function __construct(TimezoneInterface $timezone, LoggerInterface $logger)
+    {
+        $this->timezone = $timezone;
+        $this->logger = $logger;
+    }
+
+    public function beforeSetTemplateVars(
+        \Magento\Framework\Mail\Template\TransportBuilder $subject,
+        array $templateVars
+    ) {
+        if (isset($templateVars['invoice'])) {
+            $templateVars['email_invoice_created_at_formatted'] =
+                $this->timezone->formatDate(
+                    $templateVars['invoice']->getCreatedAt(),
+                    'EEEE, MMMM d, y'
+                );
+        }
+
+        if (isset($templateVars['shipment']) && is_object($templateVars['shipment'])) {
+            $shipment = $templateVars['shipment'];
+            $tracksHtml = '';
+            try {
+                $tracks = method_exists($shipment, 'getAllTracks') ? $shipment->getAllTracks() : [];
+                foreach ($tracks as $track) {
+                    $tracksHtml = $track->getTrackNumber() ?: '';
+                }
+            } catch (\Throwable $e) {
+                $this->logger->debug('Error building tracks_html: ' . $e->getMessage());
+            }
+            $templateVars['tracks_html'] = $tracksHtml;
+        }
+
+        return [$templateVars];
+    }
+
+}
