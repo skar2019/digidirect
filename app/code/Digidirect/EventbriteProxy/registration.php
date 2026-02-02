150784bae2647fb361acba43504e01273c2cea75
Jireh <jireh.c@digidirect.com.au>
2026-02-02 12:05:43 +0800

    modify EventbriteProxy

--- app/code/Digidirect/EventbriteProxy/registration.php
+++ app/code/Digidirect/EventbriteProxy/registration.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * Place at: app/code/DigiDirect/EventbriteProxy/registration.php
- */
-
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'DigiDirect_EventbriteProxy',
+    'Digidirect_EventbriteProxy',
     __DIR__
 );
