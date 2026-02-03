0067f2728be0637ba0ef6a5c104df43159a561c4
clintmercado <clint@kayweb.com.au>
2026-02-03 10:53:24 +0800

    debugging

--- app/code/Digidirect/Collect/view/frontend/web/js/view/checkout-block.js
+++ app/code/Digidirect/Collect/view/frontend/web/js/view/checkout-block.js
@@ -185,20 +185,23 @@ define([
             $(this.collectBlock).removeClass(this.visibleClass);
             $('[data-collect-type="' + method + '"]').addClass(this.visibleClass);
 
+            console.log('BEFORE clear - shipping method:', quote.shippingMethod());
+
             if (method === 'delivery') {
                 // Clear shipping method when switching to delivery
                 quote.shippingMethod(null);
+                console.log('AFTER clear (delivery) - shipping method:', quote.shippingMethod());
 
                 this.applyDeliveryToAllItems();
                 this.isCollectSelected(false);
                 quote.isCollectSelected = false;
             } else {
+                // Clear shipping method when switching to Click & Collect
                 quote.shippingMethod(null);
+                console.log('AFTER clear (collect) - shipping method:', quote.shippingMethod());
 
                 this.isCollectSelected(true);
                 quote.isCollectSelected = true;
-
-                this.showFormPopUp();
             }
         },
         applyDeliveryToAllItems: function () {
