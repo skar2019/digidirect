e4f1494424381c66644ff4e7e6a2280333c10da2
lakshyami <lakshyami@i4tlabs.io>
2026-02-11 10:52:03 +0530

    Added order place time with the date

--- app/code/Digidirect/EmailCustomerHelp/Plugin/EmailTransportPlugin.php
+++ app/code/Digidirect/EmailCustomerHelp/Plugin/EmailTransportPlugin.php
@@ -22,7 +22,7 @@ class EmailTransportPlugin
             $templateVars['email_order_created_at_formatted'] =
                 $this->timezone->formatDate(
                     $templateVars['order']->getCreatedAt(),
-                    'EEEE, MMMM d, y'
+                    'EEEE, MMMM d, y g:i:s a'
                 );
         }
 
