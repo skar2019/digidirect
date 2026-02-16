3dfc7487caffa4c0709e778c1686e6c35cb17dd7
Jireh <jireh.c@digidirect.com.au>
2026-02-16 10:35:31 +0800

    push email fix for cc info

--- app/code/Digidirect/EmailFix/Plugin/PaymentHelperPlugin.php
+++ app/code/Digidirect/EmailFix/Plugin/PaymentHelperPlugin.php
@@ -10,21 +10,52 @@ class PaymentHelperPlugin
         \Magento\Payment\Model\InfoInterface $info,
         $storeId
     ) {
-        if ($info->getMethod() !== 'braintree') {
+        $method = $info->getMethod();
+
+        // Only modify Braintree methods
+        if (strpos($method, 'braintree') === false) {
             return $result;
         }
 
+        $additionalInfo = $info->getAdditionalInformation();
         $ccType = $info->getCcType();
         $ccLast4 = $info->getCcLast4();
 
-        $html = '<div><strong>Credit Card Test</strong></div>';
+        $html = '';
 
-        if ($ccType) {
-            $html .= '<div><strong>Credit Card Type:</strong> ' . $ccType . '</div>';
+        // ===== Credit Card =====
+        if ($ccLast4) {
+            $html .= '<div><strong>Credit Card</strong></div>';
+
+            if ($ccType) {
+                $html .= '<div><strong>Credit Card Type:</strong> ' . $ccType . '</div>';
+            }
+
+            $html .= '<div><strong>Credit Card Number:</strong> ****-' . $ccLast4 . '</div>';
         }
 
-        if ($ccLast4) {
-            $html .= '<div><strong>Credit Card Number:</strong> ****-' . $ccLast4 . '</div>';
+        // ===== Apple Pay =====
+        elseif (isset($additionalInfo['payment_instrument_type']) 
+            && $additionalInfo['payment_instrument_type'] === 'apple_pay') {
+
+            $html .= '<div><strong>Apple Pay</strong></div>';
+
+            if ($ccType && $ccLast4) {
+                $html .= '<div><strong>Card:</strong> ' . $ccType . ' ****-' . $ccLast4 . '</div>';
+            }
+        }
+
+        // ===== PayPal =====
+        elseif (isset($additionalInfo['paypal_payer_email'])) {
+
+            $html .= '<div><strong>PayPal</strong></div>';
+            $html .= '<div><strong>PayPal Email:</strong> ' 
+                . $additionalInfo['paypal_payer_email'] . '</div>';
+        }
+
+        // Fallback (if something unexpected)
+        else {
+            return $result;
         }
 
         return $html;
