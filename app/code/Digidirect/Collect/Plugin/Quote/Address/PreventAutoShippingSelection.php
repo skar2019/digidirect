94a5eebbdbc8840d54ea9fa621f723b92cc328ce
clintmercado <clint@kayweb.com.au>
2026-02-19 08:52:39 +0800

    shippingMethod null

--- app/code/Digidirect/Collect/Plugin/Quote/Address/PreventAutoShippingSelection.php
+++ app/code/Digidirect/Collect/Plugin/Quote/Address/PreventAutoShippingSelection.php
@@ -7,7 +7,6 @@ use Psr\Log\LoggerInterface;
 class PreventAutoShippingSelection
 {
     protected $logger;
-    private static $allowStandard = false;
 
     public function __construct(LoggerInterface $logger)
     {
@@ -18,23 +17,43 @@ class PreventAutoShippingSelection
         Address $subject,
                 $method
     ) {
-        // Allow standard_standard if it's being set with explicit rates available
-        if ($method === 'standard_standard') {
-            // Check if there are shipping rates available (means user is selecting)
-            $rates = $subject->getAllShippingRates();
+        // Allow null - we're explicitly clearing the method
+        if ($method === null) {
+            return [$method];
+        }
 
-            // If we have rates, it means the user is on the shipping method selection step
-            if (!empty($rates)) {
-                $this->logger->info('ALLOWING standard_standard - user selection');
-                self::$allowStandard = true;
+        // Block standard_standard if no method is currently selected
+        // This prevents auto-selection when switching from C&C to Delivery
+        if ($method === 'standard_standard') {
+            $currentMethod = $subject->getShippingMethod();
+
+            // If there's no current method, this is auto-selection - block it
+            if (empty($currentMethod)) {
+                $this->logger->info('BLOCKED standard_standard - no current method (auto-selection)');
+                return [null];
+            }
+
+            // If current method is already standard_standard, allow it (re-save)
+            if ($currentMethod === 'standard_standard') {
+                $this->logger->info('ALLOWING standard_standard - re-save of existing method');
                 return [$method];
             }
 
-            // If no rates, it's auto-selection, block it
-            $this->logger->info('BLOCKED standard_standard - auto selection');
-            return [null];
+            // If switching FROM another method TO standard, this should be allowed
+            // (user explicitly selected it)
+            $this->logger->info('ALLOWING standard_standard - switching from ' . $currentMethod);
+            return [$method];
         }
 
         return [$method];
+    }
+
+    public function afterSetShippingMethod(
+        Address $subject,
+                $result,
+                $method
+    ) {
+        $this->logger->info('FINAL shipping method set: ' . var_export($subject->getShippingMethod(), true));
+        return $result;
     }
 }
