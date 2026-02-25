bd3e55f5f23a69d97f199d0a58df2413a4fb1983
clintmercado <clint@kayweb.com.au>
2026-02-25 21:36:15 +0800

    REMOVED: Auto-selection of shipping method

--- app/design/frontend/Digidirect/digi-glow-up/Digidirect_Collect/web/js/view/block-mixin.js
+++ app/design/frontend/Digidirect/digi-glow-up/Digidirect_Collect/web/js/view/block-mixin.js
@@ -35,14 +35,13 @@ define([
             },
             onSuccessDelivery: function () {
                 this._super();
-                this.toggleToDeliveryShippingMethod();
+                // REMOVED: Auto-selection of shipping method
+                // this.toggleToDeliveryShippingMethod();
             },
             toggleToDeliveryShippingMethod: function () {
-                var closestRadioButtons = $('.row.collect').siblings().find('input');
-
-                if (!closestRadioButtons.filter(':checked').length) {
-                    closestRadioButtons.first().trigger('click');
-                }
+                // DISABLED: This was auto-selecting standard shipping
+                // Keep function for compatibility but don't auto-click
+                console.log('toggleToDeliveryShippingMethod called but auto-selection disabled');
             },
             setCollectAbstractEntityFields: function () {
                 var self = this,
